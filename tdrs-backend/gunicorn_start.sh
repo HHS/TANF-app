129d1577c306e80756c9d93f3582d1860f814a60
raftergit <patrickgoraft@MacBook-Pro.local>
2020-07-31 02:57:48 -0400

    draft of the manifest.yml and multi Dockerfile support

--- tdrs-backend/gunicorn_start.sh
+++ tdrs-backend/gunicorn_start.sh
@@ -3,7 +3,4 @@
 if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ] ; then
     (python manage.py createsuperuser --no-input)
 fi
-(exec gunicorn tdpservice.wsgi:application --bind 0.0.0.0:8090 --timeout 240 --workers 3 --log-file=- --log-level debug) 
-
-
-#CMD exec gunicorn tdpservice.wsgi:application --bind 0.0.0.0:8090 --workers 3
+(exec gunicorn tdpservice.wsgi:application --bind 0.0.0.0:8000 --timeout 10 --workers 3 --log-file=- --log-level debug) 
