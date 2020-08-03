56d2f0bbda2e813d3478833e77cff29870f10d9f
raftergit <patrickgoraft@MacBook-Pro.local>
2020-08-03 08:59:24 -0400

    removing debugging logic

--- tdrs-backend/tdpservice/users/api.py
+++ tdrs-backend/tdpservice/users/api.py
@@ -11,14 +11,13 @@ class ValidateOIDCBearerToken(ObtainAuthToken):
  
 
     def post(self, request, *args, **kwargs):
-
         # include step to decrypt in tandem with UIs logic to encrypt token
         temp_token=request.data['temp_token']
         if temp_token is None:
             print('NO VALID AUTH TOKEN?!')
             return Response({"error": "Received a poor authentication token"}, status=status.HTTP_400_BAD_REQUEST)
         # keep track of how many times we've retried
-        '''Check the auth token against the userinfo endpoint to authenticate'''
+        '''Check the auth token against the oidc userinfo endpoint to authenticate'''
         r = requests.post(os.environ['OIDC_OP_USER_ENDPOINT'],
                               headers={'Authorization': 'Bearer '+temp_token})
         if r.status_code == 200:
