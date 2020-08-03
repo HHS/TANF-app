d2105a49246211e61be03a7dc0ac7340d44d8cf8
raftergit <patrickgoraft@MacBook-Pro.local>
2020-08-03 09:06:53 -0400

    removing old oidc lib from urls.py

--- tdrs-backend/tdpservice/urls.py
+++ tdrs-backend/tdpservice/urls.py
@@ -17,7 +17,6 @@ urlpatterns = [
     path('api/v1/', include(router.urls)),
     #  path('api-token-auth/', views.obtain_auth_token),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path(r'oidc/', include('mozilla_django_oidc.urls')),
     path(r'api-token-auth/', ValidateOIDCBearerToken.as_view()),
 
     # the 'api-root' from django rest-frameworks default router
