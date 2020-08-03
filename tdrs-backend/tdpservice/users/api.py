7f2b98d2dd5351366a570eab5c66a3d54f53e5e1
Carlton Smith <carl@llwebconsulting.com>
2020-08-03 14:38:57 -0400

    check for verified email
    typo fix
    
    latest

--- tdrs-backend/tdpservice/users/api.py
+++ tdrs-backend/tdpservice/users/api.py
@@ -2,11 +2,13 @@ import logging
 import os
 import requests
 import sys
+import json
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
 from rest_framework import status
 from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 
 # consider adding throttling logic
 class ValidateOIDCBearerToken(ObtainAuthToken):
@@ -20,18 +22,23 @@ class ValidateOIDCBearerToken(ObtainAuthToken):
         attempt_num = 0
         # keep track of how many times we've retried
         '''Check the auth token against the userinfo endpoint to authenticate'''
-        r = requests.get(os.environ['OIDC_OP_USER_ENDPOINT'],
+        r = requests.get('https://idp.int.identitysandbox.gov/api/openid_connect/userinfo',
                               headers={'Authorization': 'Bearer '+temp_token})
+        
         if r.status_code == 200:
             data = r.json()
+            User = get_user_model()
             
             # get user from database if they exist. if not, create a new one
-            try:
-                user = User.objects.get(email=data.email)
-            except ObjectDoesNotExist:
-                user = User.objects.create_user(data.email)
-                user.set_unusable_password()
-                user.save()
+            if data["email_verified"] == True:
+                try:
+                    user = User.objects.get(email=data["email"])
+                except User.DoesNotExist:
+                    user = User.objects.create_user(data["email"])
+                    user.set_unusable_password()
+                    user.save()
+            else:
+                return Response({"error": "Unverified email"}, status=status.HTTP_400_BAD_REQUEST)
                 
             session.user_id = user.pk
             
--- tdrs-backend/tdpservice/users/models.py
+++ tdrs-backend/tdpservice/users/models.py
@@ -2,12 +2,14 @@ import uuid
 from django.db import models
 from django.conf import settings
 from django.dispatch import receiver
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import (
+    BaseUserManager, AbstractUser
+)
 from django.db.models.signals import post_save
 from rest_framework.authtoken.models import Token
 class User(AbstractUser):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-
+    
     def __str__(self):
         return self.username
 
@@ -16,3 +18,5 @@ class User(AbstractUser):
 def create_auth_token(sender, instance=None, created=False, **kwargs):
     if created:
         Token.objects.create(user=instance)
+        
+
