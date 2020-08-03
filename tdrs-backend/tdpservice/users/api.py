1f24f803b03f2405743ddf4efe7cf1362af80162
Carlton Smith <carl@llwebconsulting.com>
2020-08-03 18:48:44 -0400

    log in user and return user data to front end

--- tdrs-backend/tdpservice/users/api.py
+++ tdrs-backend/tdpservice/users/api.py
@@ -3,6 +3,7 @@ import os
 import requests
 import sys
 import json
+import uuid
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
@@ -32,21 +33,21 @@ class ValidateOIDCBearerToken(ObtainAuthToken):
             # get user from database if they exist. if not, create a new one
             if data["email_verified"] == True:
                 try:
-                    user = User.objects.get(email=data["email"])
-                    print('existing user logging in ' + user.pk)
+                    user = User.objects.get(username=data["email"])
+                    print('existing user logging in ' + user.username)
                 except User.DoesNotExist:
                     user = User.objects.create_user(data["email"])
                     user.set_unusable_password()
                     user.save()
-                    print('new user created ' + user.pk)
+                    print('new user created ' + user.username)
             else:
                 return Response({"error": "Unverified email"}, status=status.HTTP_400_BAD_REQUEST)
                 
-            session.user_id = user.pk
+            request.session['user_id'] = str(user.pk)
             
             return Response({
                     'user_id': user.pk,
-                    'email': user.email
+                    'email': user.username
                 }, status=status.HTTP_200_OK)
         else:
             return Response({"error": "Request failed"}, status=r.status_code)
