cab4bda5e8e0bbe5e2ee973b45d4988c9c1c8195
raftergit <patrickgoraft@MacBook-Pro.local>
2020-08-06 20:37:46 -0400

    Adding Backend CircleCI Testing
     Adding circleci build jobs for the Django rest framework

--- tdrs-backend/tdpservice/settings/common.py
+++ tdrs-backend/tdpservice/settings/common.py
@@ -40,10 +40,7 @@ class Common(Configuration):
 
     ALLOWED_HOSTS = ["*"]
     ROOT_URLCONF = 'tdpservice.urls'
-    SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
-    PUBLIC_KEY = os.environ['PUBLIC_KEY']
-    JWT_CERT = os.environ['JWT_CERT']
-    OIDC_RP_IDP_SIGN_KEY = os.environ['OIDC_RP_IDP_SIGN_KEY']
+    SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
     WSGI_APPLICATION = 'tdpservice.wsgi.application'
 
     # Email Server
