e5c20fecdd4accb2a3c6bfc8dfe2a8ae49f8b9d3
Carlton Smith <carl@llwebconsulting.com>
2020-08-13 16:05:31 -0400

    login and logout using login.gov and django custom auth

---
+++ tdrs-backend/tdpservice/auth_backend.py
@@ -0,0 +1,22 @@
+from django.contrib.auth.backends import ModelBackend
+from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+
+# requires to define two functions authenticate and get_user
+
+class CustomAuthentication:  
+
+    def authenticate(self, request, username=None):
+        User = get_user_model()
+        try:
+            user = User.objects.get(username=username)
+            return user
+        except User.DoesNotExist:
+            return None
+        
+    def get_user(self, user_id):
+        User = get_user_model()
+        try:
+            return User.objects.get(pk=user_id)
+        except User.DoesNotExist:
+            return None
