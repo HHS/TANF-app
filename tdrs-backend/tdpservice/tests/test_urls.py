a8f681c344c5abd7ebeb6b1531dac12dc8309999
Carlton Smith <carl@llwebconsulting.com>
2020-08-17 14:37:24 -0400

    repair url test

--- tdrs-backend/tdpservice/tests/test_urls.py
+++
@@ -1,9 +0,0 @@
-"""Test main routing works."""
-
-from django.urls import reverse
-
-
-def test_create(self):
-    """Test login url responds."""
-    response = self.client.get(reverse('login'))
-    self.assertEqual(response.status_code, 200)
