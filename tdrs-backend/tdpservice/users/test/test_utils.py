309a315961616f1cd49cd1b1f4c4782a7f8d3cfc
Carlton Smith <carl@llwebconsulting.com>
2020-08-27 13:55:50 -0400

    testing utlities

--- tdrs-backend/tdpservice/users/test/test_utils.py
+++ tdrs-backend/tdpservice/users/test/test_utils.py
@@ -2,22 +2,31 @@
 
 import pytest
 from ..api import utils
+from django.test import TestCase
+from django.test import Client
 
-@pytest.fixture(scope="session", autouse=True)
-def test_add_state_and_nonce_to_session(request):
-    """Test method for adding state and nonce to the session."""
-    state = "teststate"
-    nonce = "testnonce"
-    utils.add_state_and_nonce_to_session(request, state, nonce)
-    assert request.session["openid_authenticity_tracker"][state].exists() is True
-    assert request.session["openid_authenticity_tracker"][state]["nonce"] == nonce
 
-@pytest.fixture(scope="session", autouse=True)
-def test_get_nonce_and_state(request):
-    """Test method for getting the nonce and state."""
-    state = "teststate2"
-    nonce = "testnonce2"
-    utils.add_state_and_nonce_to_session(request, state, nonce)
-    sessionvals = utils.get_nonce_and_state(request)
-    assert sessionvals["state"] == state
-    assert sessionvals["nonce"] == nonce
+class TestUtilities(TestCase):
+    """Test utility methods."""
+
+    @pytest.mark.django_db
+    def test_add_state_and_nonce_to_session(self):
+        """Test method for adding state and nonce to the session."""
+        state = "teststate"
+        nonce = "testnonce"
+        tracker = "openid_authenticity_tracker"
+        request = Client.request
+        utils.add_state_and_nonce_to_session(request, state, nonce)
+        assert self.client.session[tracker][state].exists() is True
+        assert self.client.session[tracker][state]["nonce"] == nonce
+
+    @pytest.mark.django_db
+    def test_get_nonce_and_state(self):
+        """Test method for getting the nonce and state."""
+        state = "teststate2"
+        nonce = "testnonce2"
+        request = Client.request
+        utils.add_state_and_nonce_to_session(request, state, nonce)
+        sessionvals = utils.get_nonce_and_state(request)
+        assert sessionvals["state"] == state
+        assert sessionvals["nonce"] == nonce
