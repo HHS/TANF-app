2e25879b1d8ce62bb096a279bd5b413fed9fde77
Spencer Hilvitz <spencer.hilvitz@colorado.edu>
2020-09-04 12:59:49 -0700

    basic layout done (without comments)

--- tdrs-frontend/src/components/Dashboard/Dashboard.jsx
+++ tdrs-frontend/src/components/Dashboard/Dashboard.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { GridContainer, Button, Grid } from '@trussworks/react-uswds'
-import { useSelector } from 'react-redux'
 import circleImg from '../../matt-wang-eNwx-kYd6fM-unsplash.jpg'
 
 /**
@@ -12,11 +11,6 @@ import circleImg from '../../matt-wang-eNwx-kYd6fM-unsplash.jpg'
  * @param {object} email - a user's email address
  */
 function Dashboard() {
-  const user = useSelector((state) => state.auth.user.email)
-  const handleClick = (event) => {
-    event.preventDefault()
-    window.location.href = `${process.env.REACT_APP_BACKEND_URL}/logout/oidc`
-  }
   return (
     <main id="main-content">
       <section className="usa-hero" aria-label="Introduction">
@@ -73,99 +67,6 @@ function Dashboard() {
           </Grid>
         </Grid>
       </section>
-
-      {/* <section className="usa-graphic-list usa-section usa-section--dark">
-        <GridContainer>
-          <Grid row gap className="usa-graphic-list__row">
-            <Grid tablet={{ col: true }} className="usa-media-block">
-              <img
-                className="usa-media-block__img"
-                src={circleImg}
-                alt="Alt text"
-              />
-              <div className="usa-media-block__body">
-                <h2 className="usa-graphic-list__heading">
-                  Graphic headings can vary.
-                </h2>
-                <p>
-                  Graphic headings can be used a few different ways, depending
-                  on what your landing page is for. Highlight your values,
-                  specific program areas, or results.
-                </p>
-              </div>
-            </Grid>
-            <Grid tablet={{ col: true }} className="usa-media-block">
-              <img
-                className="usa-media-block__img"
-                src={circleImg}
-                alt="Alt text"
-              />
-              <div className="usa-media-block__body">
-                <h2 className="usa-graphic-list__heading">
-                  Stick to 6 or fewer words.
-                </h2>
-                <p>
-                  Keep body text to about 30 words. They can be shorter, but
-                  try to be somewhat balanced across all four. It creates a
-                  clean appearance with good spacing.
-                </p>
-              </div>
-            </Grid>
-          </Grid>
-          <Grid row gap className="usa-graphic-list__row">
-            <Grid tablet={{ col: true }} className="usa-media-block">
-              <img
-                className="usa-media-block__img"
-                src={circleImg}
-                alt="Alt text"
-              />
-              <div className="usa-media-block__body">
-                <h2 className="usa-graphic-list__heading">
-                  Never highlight anything without a goal.
-                </h2>
-                <p>
-                  For anything you want to highlight here, understand what
-                  your users know now, and what activity or impression you
-                  want from them after they see it.
-                </p>
-              </div>
-            </Grid>
-            <Grid tablet={{ col: true }} className="usa-media-block">
-              <img
-                className="usa-media-block__img"
-                src={circleImg}
-                alt="Alt text"
-              />
-              <div className="usa-media-block__body">
-                <h2 className="usa-graphic-list__heading">
-                  Could also have 2 or 6.
-                </h2>
-                <p>
-                  In addition to your goal, find out your users’ goals. What
-                  do they want to know or do that supports your mission? Use
-                  these headings to show these.
-                </p>
-              </div>
-            </Grid>
-          </Grid>
-        </GridContainer>
-      </section>
-
-      <section id="test-section-id" className="usa-section">
-        <GridContainer>
-          <h2 className="font-heading-xl margin-y-0">Section heading</h2>
-          <p className="usa-intro">
-            Everything up to this point should help people understand your
-            agency or project: who you are, your goal or mission, and how you
-            approach it. Use this section to encourage them to act. Describe
-            why they should get in touch here, and use an active verb on the
-            button below. “Get in touch,” “Learn more,” and so on.
-          </p>
-          <a href="#" className="usa-button usa-button--big">
-            Call to action
-          </a>
-        </GridContainer>
-      </section> */}
     </main>
   )
 }
--- tdrs-frontend/src/components/Header/Header.jsx
+++ tdrs-frontend/src/components/Header/Header.jsx
@@ -1,69 +1,12 @@
 import React, { useState } from 'react'
-// // import './Header.scss'
 
 import { 
-  // GridContainer,
-  // Grid, 
-  // FooterNav,
-  // Logo, 
   NavMenuButton, 
   Menu, 
   Header, 
-  // PrimaryNav,
   Title,
   ExtendedNav,
-  // Search,
-  // SocialLinks,
-  // Address,
-  // Footer 
 } from '@trussworks/react-uswds'
-
-// function HeaderComp() {
-//   const [expanded, setExpanded] = useState(false)
-// const onClick = () => setExpanded((prvExpanded) => !prvExpanded)
-// const [isOpen, setIsOpen] = useState([false])
-
-// const testMenuItems = [
-//   <a href="#linkOne" key="one">
-//     Simple link one
-//   </a>,
-//   <a href="#linkTwo" key="two">
-//     Simple link two
-//   </a>,
-// ]
-
-// const testItemsMenu = [
-//   <>
-//     <Menu
-//       key="one"
-//       items={testMenuItems}
-//       isOpen={isOpen[0]}
-//       id="testDropDownOne"
-//     />
-//   </>,
-//   <a href="#two" key="two" className="usa-nav__link">
-//     <span>Welcome</span>
-//   </a>
-// ]
-
-//   return (
-//     <>
-//       <div className={`usa-overlay ${expanded ? 'is-visible' : ''}`}></div>
-//       <Header extended={true}>
-//         <div className="usa-navbar">
-//           <Title>Project Title</Title>
-//           <NavMenuButton onClick={onClick} label="Menu" />
-//         </div>
-//         <ExtendedNav
-//           primaryItems={testItemsMenu}
-//           secondaryItems={[]}
-//           mobileExpanded={expanded}
-//           onToggleMobileNav={onClick}>
-//         </ExtendedNav>
-//       </Header>
-//     </>
-//   )
-// }
 
 function HeaderComp() {
   const [expanded, setExpanded] = useState(false)
