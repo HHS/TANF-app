05b06aa71f28cf2d74c7fe4d14e1a6228dceb21f
raftergit <patrickgoraft@MacBook-Pro.local>
2020-09-08 21:14:51 -0400

    resolving linting after updates to common.py update

--- tdrs-backend/tdpservice/settings/common.py
+++ tdrs-backend/tdpservice/settings/common.py
@@ -199,7 +199,7 @@ class Common(Configuration):
     SESSION_COOKIE_HTTPONLY = True
 
     # The CSRF token Cookie holds no security benefits when confined to HttpOnly.
-    # Setting this to false to allow the frontend to include it in the header 
+    # Setting this to false to allow the frontend to include it in the header
     # of API POST calls to prevent false negative authorization errors.
     # https://docs.djangoproject.com/en/2.2/ref/settings/#csrf-cookie-httponly
     CSRF_COOKIE_HTTPONLY = False
