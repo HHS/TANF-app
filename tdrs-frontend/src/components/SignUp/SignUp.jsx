17461385d2c2bf9b5e00c21ed85c76bb4cb7f4ce
Spencer Hilvitz <spencer.hilvitz@gmail.com>
2020-09-10 13:58:31 -0700

    remove card and fieldset from signup.jsx

--- tdrs-frontend/src/components/SignUp/SignUp.jsx
+++ tdrs-frontend/src/components/SignUp/SignUp.jsx
@@ -1,44 +1,29 @@
 import React from 'react'
 import {
   GridContainer,
-  CardGroup,
-  Card,
   Form,
-  Fieldset,
   Label,
   TextInput,
   Button,
-  CardHeader,
-  CardBody,
 } from '@trussworks/react-uswds'
 
 function SignUp() {
   return (
     <GridContainer>
-      <CardGroup>
-        <Card gridLayout={{ col: 6 }}>
-          <CardHeader>
-            <h1 className="usa-card__heading">Request Access</h1>
-            <p>
-              We need to collect some information before an OFA Admin can grant
-              you access
-            </p>
-          </CardHeader>
-          <CardBody>
-            <Form>
-              <Fieldset legend="Name">
-                <Label htmlFor="first-name">First name</Label>
-                <TextInput id="first-name" name="first-name" type="text" />
-                <Label htmlFor="last-name">Last name</Label>
-                <TextInput id="last-name" name="last-name" type="text" />
-                <Button className="usa-button--big" type="submit">
-                  Request Access
-                </Button>
-              </Fieldset>
-            </Form>
-          </CardBody>
-        </Card>
-      </CardGroup>
+      <h1>Request Access</h1>
+      <p>
+        We need to collect some information before an OFA Admin can grant you
+        access
+      </p>
+      <Form>
+        <Label htmlFor="first-name">First name</Label>
+        <TextInput id="first-name" name="first-name" type="text" />
+        <Label htmlFor="last-name">Last name</Label>
+        <TextInput id="last-name" name="last-name" type="text" />
+        <Button className="usa-button--big" type="submit">
+          Request Access
+        </Button>
+      </Form>
     </GridContainer>
   )
 }
