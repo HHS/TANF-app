88aec966cc70cc50ae5c8104dea262156ba0707a
Carlton Smith <carl@llwebconsulting.com>
2020-09-15 14:20:26 -0400

    added tests to cover different sections of the page

--- tdrs-frontend/cypress/integration/welcome_page_greeting.spec.js
+++ tdrs-frontend/cypress/integration/welcome_page_greeting.spec.js
@@ -11,4 +11,24 @@ describe('welcome page greeting', () => {
       'An official website of the United States government'
     )
   })
+  it('contains a navigation link', () => {
+    cy.visit('http://localhost:3000')
+
+    cy.get('li.usa-nav__primary-item').find('a.usa-nav__link')
+  })
+  it('prompts the user to sign in', () => {
+    cy.visit('http://localhost:3000')
+
+    cy.get('h1.usa-hero__heading').contains('Sign into TANF Data Portal')
+  })
+  it('contains a vision message', () => {
+    cy.visit('http://localhost:3000')
+
+    cy.get('div.usa-hero__callout').contains('Our vision is to build a new')
+  })
+  it('contains a resources section headline', () => {
+    cy.visit('http://localhost:3000')
+
+    cy.get('h2.resources-header').contains('Featured TANF Resources')
+  })
 })
