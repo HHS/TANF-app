9991253a4262775edda20dc2dbb7eb9d515af24e
raftergit <patrickgoraft@MacBook-Pro.local>
2020-09-18 09:04:07 -0400

    proto-type endpoint for getting states /tribes/ territories organized by region

---
+++ tdrs-backend/tdpservice/users/management/commands/populate_stts.py
@@ -0,0 +1,78 @@
+"""`populate_stts` command."""
+
+import csv
+from pathlib import Path
+
+from django.core.management import BaseCommand
+
+from ...models import Region, STT
+
+
+DATA_DIR = BASE_DIR = Path(__file__).resolve().parent / "data"
+
+
+def _populate_regions():
+    with open(DATA_DIR / "regions.csv") as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            Region.objects.get_or_create(id=row["Id"])
+        Region.objects.get_or_create(id=1000)
+
+
+def _get_states():
+    with open(DATA_DIR / "states.csv") as csvfile:
+        reader = csv.DictReader(csvfile)
+        return [
+            STT(
+                code=row["Code"],
+                name=row["Name"],
+                region_id=row["Region"],
+                type=STT.STTType.STATE,
+            )
+            for row in reader
+        ]
+
+
+def _get_territories():
+    with open(DATA_DIR / "territories.csv") as csvfile:
+        reader = csv.DictReader(csvfile)
+        return [
+            STT(
+                code=row["Code"],
+                name=row["Name"],
+                region_id=row["Region"],
+                type=STT.STTType.TERRITORY,
+            )
+            for row in reader
+        ]
+
+
+def _populate_tribes():
+    with open(DATA_DIR / "tribes.csv") as csvfile:
+        reader = csv.DictReader(csvfile)
+        stts = [
+            STT(
+                name=row["Name"],
+                region_id=row["Region"],
+                state=STT.objects.get(code=row["Code"]),
+                type=STT.STTType.TRIBE,
+            )
+            for row in reader
+        ]
+        STT.objects.bulk_create(stts, ignore_conflicts=True)
+
+
+class Command(BaseCommand):
+    """Command class."""
+
+    help = "Populate regions, states, territories, and tribes."
+
+    def handle(self, *args, **options):
+        """Populate the various regions, states, territories, and tribes."""
+        _populate_regions()
+        stts = _get_states()
+        stts.extend(_get_territories())
+        STT.objects.bulk_create(stts, ignore_conflicts=True)
+        _populate_tribes()
+        """Possible logic to include roles in combo box"""
+        STT.objects.get_or_create(id=-1, region_id=1000, name="I work at OFA")
--- tdrs-backend/tdpservice/users/models.py
+++ tdrs-backend/tdpservice/users/models.py
@@ -4,12 +4,55 @@ import uuid
 
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.db.models import constraints
+
+
+class Region(models.Model):
+    """A model representing a US region."""
+
+    id = models.PositiveIntegerField(primary_key=True)
+
+    def __str__(self):
+        """Return the ID."""
+        return str(self.id)
+
+
+class STT(models.Model):
+    """A model representing a US state, tribe or territory."""
+
+    class STTType(models.TextChoices):
+        """Enum representing types of STT."""
+
+        STATE = "state"
+        TERRITORY = "territory"
+        TRIBE = "tribe"
+
+    type = models.CharField(
+        max_length=200, blank=True, null=True, choices=STTType.choices
+    )
+    code = models.CharField(max_length=2, blank=True, null=True)
+    name = models.CharField(max_length=1000)
+    region = models.ForeignKey(Region, on_delete=models.CASCADE, related_name="stts")
+    # Tribes have a state, which we need to store.
+    state = models.ForeignKey("self", on_delete=models.CASCADE, blank=True, null=True)
+
+    class Meta:
+        """Metadata."""
+
+        constraints = [
+            constraints.UniqueConstraint(fields=["name"], name="stt_uniq_name"),
+        ]
+
+    def __str__(self):
+        """Return the STT's name."""
+        return self.name
 
 
 class User(AbstractUser):
     """Define user fields and methods."""
 
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    stt = models.ForeignKey(STT, on_delete=models.CASCADE, blank=True, null=True)
 
     def __str__(self):
         """Return the username as the string representation of the object."""
