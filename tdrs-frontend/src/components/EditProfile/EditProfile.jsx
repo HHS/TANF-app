22abbf5dae3cdcff7d1ccb4845c84934247b0594
Spencer Hilvitz <spencer.hilvitz@gmail.com>
2020-10-05 09:50:50 -0700

    trying to use uswds example

--- tdrs-frontend/src/components/EditProfile/EditProfile.jsx
+++ tdrs-frontend/src/components/EditProfile/EditProfile.jsx
@@ -1,10 +1,7 @@
 import React, { useEffect } from 'react'
-import { Select } from 'antd'
 import { useDispatch, useSelector } from 'react-redux'
 import { fetchStts } from '../../actions/stts'
 import Button from '../Button'
-
-const { Option } = Select
 
 function EditProfile() {
   const sttsLoading = useSelector((state) => state.stts.loading)
@@ -14,22 +11,6 @@ function EditProfile() {
   useEffect(() => {
     dispatch(fetchStts())
   }, [dispatch])
-
-  function onChange(value) {
-    console.log(`selected ${value}`)
-  }
-
-  function onBlur() {
-    console.log('blur')
-  }
-
-  function onFocus() {
-    console.log('focus')
-  }
-
-  function onSearch(val) {
-    console.log('search:', val)
-  }
 
   return (
     <div className="grid-container">
@@ -68,30 +49,81 @@ function EditProfile() {
         {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
         <label className="usa-label" htmlFor="stt">
           Associated State, Tribe, or Territory
-          <Select
-            showSearch
-            allowClear
-            style={{ width: 320 }}
-            placeholder="Select a person"
-            onChange={onChange}
-            onFocus={onFocus}
-            onBlur={onBlur}
-            onSearch={onSearch}
-            loading={sttsLoading}
-            filterOption={(input, option) =>
-              option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
-            }
-          >
-            {stts.map((stt) => (
-              <Option
-                key={stt.id}
-                label={stt.name}
-                value={stt.name.toLowerCase()}
-              >
-                {stt.name}
-              </Option>
-            ))}
-          </Select>
+          <div className="usa-combo-box">
+            <select
+              className="usa-select usa-combo-box__select"
+              name="stt"
+              id="fruit"
+            >
+              <option value>Select a fruit</option>
+              <option value="apple">Apple</option>
+              <option value="apricot">Apricot</option>
+              <option value="avocado">Avocado</option>
+              <option value="banana">Banana</option>
+              <option value="blackberry">Blackberry</option>
+              <option value="blood orange">Blood orange</option>
+              <option value="blueberry">Blueberry</option>
+              <option value="boysenberry">Boysenberry</option>
+              <option value="breadfruit">Breadfruit</option>
+              <option value="buddhas hand citron">
+                Buddha&apos;s hand citron
+              </option>
+              <option value="cantaloupe">Cantaloupe</option>
+              <option value="clementine">Clementine</option>
+              <option value="crab apple">Crab apple</option>
+              <option value="currant">Currant</option>
+              <option value="cherry">Cherry</option>
+              <option value="custard apple">Custard apple</option>
+              <option value="coconut">Coconut</option>
+              <option value="cranberry">Cranberry</option>
+              <option value="date">Date</option>
+              <option value="dragonfruit">Dragonfruit</option>
+              <option value="durian">Durian</option>
+              <option value="elderberry">Elderberry</option>
+              <option value="fig">Fig</option>
+              <option value="gooseberry">Gooseberry</option>
+              <option value="grape">Grape</option>
+              <option value="grapefruit">Grapefruit</option>
+              <option value="guava">Guava</option>
+              <option value="honeydew melon">Honeydew melon</option>
+              <option value="jackfruit">Jackfruit</option>
+              <option value="kiwifruit">Kiwifruit</option>
+              <option value="kumquat">Kumquat</option>
+              <option value="lemon">Lemon</option>
+              <option value="lime">Lime</option>
+              <option value="lychee">Lychee</option>
+              <option value="mandarine">Mandarine</option>
+              <option value="mango">Mango</option>
+              <option value="mangosteen">Mangosteen</option>
+              <option value="marionberry">Marionberry</option>
+              <option value="nectarine">Nectarine</option>
+              <option value="orange">Orange</option>
+              <option value="papaya">Papaya</option>
+              <option value="passionfruit">Passionfruit</option>
+              <option value="peach">Peach</option>
+              <option value="pear">Pear</option>
+              <option value="persimmon">Persimmon</option>
+              <option value="plantain">Plantain</option>
+              <option value="plum">Plum</option>
+              <option value="pineapple">Pineapple</option>
+              <option value="pluot">Pluot</option>
+              <option value="pomegranate">Pomegranate</option>
+              <option value="pomelo">Pomelo</option>
+              <option value="quince">Quince</option>
+              <option value="raspberry">Raspberry</option>
+              <option value="rambutan">Rambutan</option>
+              <option value="soursop">Soursop</option>
+              <option value="starfruit">Starfruit</option>
+              <option value="strawberry">Strawberry</option>
+              <option value="tamarind">Tamarind</option>
+              <option value="tangelo">Tangelo</option>
+              <option value="tangerine">Tangerine</option>
+              <option value="ugli fruit">Ugli fruit</option>
+              <option value="watermelon">Watermelon</option>
+              <option value="white currant">White currant</option>
+              <option value="yuzu">Yuzu</option>
+            </select>
+          </div>
         </label>
         <Button
           type="submit"
