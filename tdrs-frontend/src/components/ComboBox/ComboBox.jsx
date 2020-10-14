fd1de601a0c698fdc1b4396e8b15f0926ba7eb13
Spencer Hilvitz <spencer.hilvitz@gmail.com>
2020-10-14 10:58:53 -0700

    Working with no outstanding issues

--- tdrs-frontend/src/components/ComboBox/ComboBox.jsx
+++ tdrs-frontend/src/components/ComboBox/ComboBox.jsx
@@ -1,24 +1,18 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useRef } from 'react'
 import PropTypes from 'prop-types'
 import comboBox from 'uswds/src/js/components/combo-box'
 
 const ComboBox = ({ sttList }) => {
-  const comboBoxRef = useRef()
-  let select
+  const selectRef = useRef()
   useEffect(() => {
     comboBox.init()
-    if (comboBoxRef.current) {
-      comboBox.on(comboBoxRef.current)
-    }
-    select = document.querySelector('.usa-select')
-    console.log('SELECT', select.value)
-  }, [comboBoxRef])
-
+    selectRef.current.value = ''
+  })
   return (
-    <label className="usa-label" htmlFor="stt">
+    <label className="usa-label" htmlFor="sttList">
       Associated State, Tribe, or Territory
-      <div className="usa-combo-box" ref={comboBoxRef}>
-        <select className="usa-select" name="stt" id="stt">
+      <div className="usa-combo-box">
+        <select className="usa-select" name="stt" id="stt" ref={selectRef}>
           {sttList.map((stt) => (
             <option
               className="stt-option"
