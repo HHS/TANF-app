b57273483a5a5d532140e6a350f4bb4b6390833d
Aaron Beavers <foamy125@gmail.com>
2020-10-22 18:20:21 -0500

    Add title heading element to display h1 content in page tab title

--- tdrs-frontend/src/components/EditProfile/EditProfile.test.js
+++ tdrs-frontend/src/components/EditProfile/EditProfile.test.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import thunk from 'redux-thunk'
 import { mount } from 'enzyme'
+import {Helmet} from 'react-helmet'
 import { Provider } from 'react-redux'
 import configureStore from 'redux-mock-store'
 
@@ -21,6 +22,12 @@ describe('EditProfile', () => {
 
     expect(h1).toExist()
     expect(h1.text()).toEqual('Request Access')
+  })
+
+  it('should have a page title with Request Access in it',() => {
+    const wrapper = mount( <EditProfile />)
+
+    expect(Helmet.peek().title.join("")).toEqual('Request Access - TDP - TANF Data Portal')
   })
 
   it('should have a first name input', () => {
