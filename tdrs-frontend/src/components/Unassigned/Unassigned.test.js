629b809468db0a94cec0cd9bb395a681b009febf
Aaron Beavers <foamy125@gmail.com>
2020-11-04 13:16:00 -0600

    Use header from private route update

--- tdrs-frontend/src/components/Unassigned/Unassigned.test.js
+++ tdrs-frontend/src/components/Unassigned/Unassigned.test.js
@@ -5,15 +5,6 @@ import Unassigned from '.'
 import Button from '../Button'
 
 describe('Unassigned', () => {
-  it('should have an h1 with "Request Submitted"', () => {
-    const wrapper = shallow(<Unassigned />)
-
-    const h1 = wrapper.find('h1')
-
-    expect(h1).toExist()
-    expect(h1.text()).toEqual('Request Submitted')
-  })
-
   it('should have a subheading with "An administrator will be in touch soon to confirm your access!"', () => {
     const wrapper = shallow(<Unassigned />)
 
