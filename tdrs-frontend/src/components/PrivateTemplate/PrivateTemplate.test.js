5865603986ab968bbc58ebe2c1f2814b54d519c9
Aaron Beavers <foamy125@gmail.com>
2020-11-04 14:41:10 -0600

    Tests passing

--- tdrs-frontend/src/components/PrivateTemplate/PrivateTemplate.test.js
+++ tdrs-frontend/src/components/PrivateTemplate/PrivateTemplate.test.js
@@ -1,25 +1,41 @@
 import React from 'react'
+import thunk from 'redux-thunk'
 import { shallow } from 'enzyme'
+
+import { fireEvent, render } from '@testing-library/react'
+import configureStore from 'redux-mock-store'
 
 import PrivateTemplate from '.'
 
+
 describe('PrivateTemplate', () => {
-  const title = 'Test'
-  it('should have an h1 with the title contents', () => {
-    const wrapper = shallow(
-      <PrivateTemplate title={title}>
-        <div>Test</div>
-      </PrivateTemplate>
-    )
+    const title = 'Test'
 
-    const h1 = wrapper.find('h1')
 
-    expect(h1).toExist()
-    expect(h1.text()).toEqual('Request Submitted')
-  })
-  it('should have a page title with title contents in it', () => {
-    const store = mockStore(initialState)
-    const { container } = render()
-    expect(document.title).toEqual('Request Access - TDP - TANF Data Portal')
-  })
+    const mockStore = configureStore([thunk])
+    it('should have an h1 with the title contents', () => {
+
+
+        const wrapper = shallow(
+                <PrivateTemplate title={title}>
+                    <div>Test</div>
+                </PrivateTemplate>
+        )
+
+        const h1 = wrapper.find('h1')
+
+        expect(h1).toExist()
+        expect(h1.text()).toEqual('Test')
+    })
+
+    it('should have a page title with title contents in it', () => {
+
+        const { container } = render(
+            <PrivateTemplate title={title}>
+                <div>Test</div>
+            </PrivateTemplate>
+        )
+
+        expect(document.title).toEqual('Test - TDP - TANF Data Portal')
+    })
 })
