3d5ffb440bdc9b1410ce6de81ff4f1333dcbfec2
Aaron Beavers <foamy125@gmail.com>
2020-11-10 12:50:57 -0600

    Move time logic inside middleware

--- tdrs-backend/tdpservice/users/api/middleware.py
+++ tdrs-backend/tdpservice/users/api/middleware.py
@@ -1,8 +1,6 @@
 """Handle authorization calls globally."""
 import datetime
 
-now = datetime.datetime.now()
-timeout = now + datetime.timedelta(minutes=10)
 
 class AuthUpdateMiddleware:
     """Update authorization cookie with new requests."""
@@ -13,6 +11,8 @@ class AuthUpdateMiddleware:
     def __call__(self, request):
         """Update cookie."""
         response = self.get_response(request)
+        now = datetime.datetime.now()
+        timeout = now + datetime.timedelta(minutes=30)
 
         # if there is no user, the user is currently
         # in the authentication process so we can't
