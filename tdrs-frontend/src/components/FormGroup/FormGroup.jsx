97ba1bfa6957886bc2f7356927104eedad4bb53c
Spencer Hilvitz <spencer.hilvitz@gmail.com>
2020-11-12 12:35:36 -0800

    Add documentation to FormGroup

--- tdrs-frontend/src/components/FormGroup/FormGroup.jsx
+++ tdrs-frontend/src/components/FormGroup/FormGroup.jsx
@@ -1,6 +1,24 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+/**
+ * FormGroup renders a group of elements for a form.
+ * This includes a form group, label, error span, and an input.
+ * The span will only be rendered if there is an error, and some
+ * classes will be added to match USWDS form styling.
+ * @param {string} error - if there is an error from an input being empty,
+ * it will be passed here.
+ * @param {string} label - this will be the string value used for the label
+ * of the input and is required.
+ * @param {string} name - this string will be used for the id's and class
+ * names of the input to ensure that they are unique and is required.
+ * @param {string} inputValue - this value is used to make the input a
+ * controlled component.
+ * @param {function} handleChange - this is a function to update the state
+ * with the value of the input.
+ * @param {function} handleBlur - this is a function that is passed in
+ * to handle the blur event of the input.
+ */
 function FormGroup({
   error,
   label,
