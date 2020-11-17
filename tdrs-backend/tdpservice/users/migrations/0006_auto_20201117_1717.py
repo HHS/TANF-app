429eb7fdcfe8d7535a0570cbddb05a741ad87897
Carlton Smith <carl@llwebconsulting.com>
2020-11-17 12:32:42 -0500

    add groups

---
+++ tdrs-backend/tdpservice/users/migrations/0006_auto_20201117_1717.py
@@ -0,0 +1,25 @@
+from django.contrib.auth.models import Group
+from django.db import migrations
+
+
+def add_groups(apps, schema_editor):
+    groups = [
+        Group(name="OFA Admin"),
+        Group(name="Data Prepper"),
+        Group(name="OFA Analyst")
+    ]
+    Group.objects.bulk_create(groups)
+
+def remove_groups(apps, schema_editor):
+    Group.objects.filter(name__in={"OFA Admin", "Data Prepper", "OFA Analyst"}).delete()
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('users', '0005_auto_20201022_1423'),
+    ]
+
+    operations = [
+        migrations.RunPython(add_groups, remove_groups),
+    ]
