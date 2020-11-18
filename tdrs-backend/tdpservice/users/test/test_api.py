6e11b63f44d0785ade392db2fcd0d49ef8354fe2
Aaron Beavers <foamy125@gmail.com>
2020-11-18 15:14:16 -0600

    Add get groupless users to user views

--- tdrs-backend/tdpservice/users/test/test_api.py
+++ tdrs-backend/tdpservice/users/test/test_api.py
@@ -379,7 +379,7 @@ def test_permission_delete(api_client, generate_groups):
 def test_user_no_roles_list(api_client):
     """Test endpoint listing users with no roles."""
     api_client.login(username="test__admin", password="test_password")
-    response = api_client.get('/v1/users/no-roles')
+    response = api_client.get('/v1/users/no_roles')
     groupless_users = User.objects.filter(groups=None).map(lambda u: u.id).sort()
     response_users = response.data.map(lambda u: u.id).sort()
     all(map(lambda groupless_id, response_id: groupless_id == response_id,
--- tdrs-backend/tdpservice/users/views.py
+++ tdrs-backend/tdpservice/users/views.py
@@ -47,6 +47,12 @@ class UserViewSet(
             "set_profile": SetUserProfileSerializer,
         }.get(self.action, UserSerializer)
 
+    @action(methods=["GET"],detail = False)
+    def no_roles(self,request, pk = None):
+        """Get a list of all users that do not belong to a group"""
+        serializer = self.get_serializer(data=User.get_groupless(),many=True)
+        return Response(serializer.data)
+
     @action(methods=["POST"], detail=False)
     def set_profile(self, request, pk=None):
         """Set a user's profile data."""
