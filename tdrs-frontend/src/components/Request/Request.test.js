c5511f2ce083b3617684643563b99309577c9cae
Aaron Beavers <foamy125@gmail.com>
2020-11-30 09:57:43 -0600

    removed currently unneeded test

--- tdrs-frontend/src/components/Request/Request.test.js
+++ tdrs-frontend/src/components/Request/Request.test.js
@@ -5,14 +5,6 @@ import Request from '.'
 import Button from '../Button'
 
 describe('Request', () => {
-  it('should have an h1 with "Request Submitted"', () => {
-    const wrapper = shallow(<Request />)
-
-    const h1 = wrapper.find('h1')
-
-    expect(h1).toExist()
-    expect(h1.text()).toEqual('Request Submitted')
-  })
 
   it('should have a subheading with "An administrator will be in touch soon to confirm your access!"', () => {
     const wrapper = shallow(<Request />)
