6d00410f2c50fa98cdaa5ddac2a08cd8ca4ab698
Carlton Smith <carl@llwebconsulting.com>
2020-12-31 16:10:14 -0500

    customize admin user form

--- tdrs-backend/tdpservice/users/admin.py
+++ tdrs-backend/tdpservice/users/admin.py
@@ -1,9 +1,24 @@
 """Add users to Django Admin."""
 
+from django import forms
 from django.contrib import admin
 from .models import User
 from rest_framework.authtoken.models import TokenProxy
 
 
-admin.site.register(User)
+class UserForm(forms.ModelForm):
+
+    class Meta:
+        model = User
+        exclude = ['password', 'is_staff', 'is_superuser', 'user_permissions']
+        readonly_fields = ['last_login', 'date_joined']
+
+
+class UserAdmin(admin.ModelAdmin):
+    exclude = ['password', 'is_staff', 'is_superuser', 'user_permissions']
+    readonly_fields = ['last_login', 'date_joined']
+    form = UserForm
+
+
+admin.site.register(User, UserAdmin)
 admin.site.unregister(TokenProxy)
