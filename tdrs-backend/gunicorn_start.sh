8c4d649ec34f6b62cc8787e65840b0b6e7de5fb4
Carlton Smith <carl@llwebconsulting.com>
2021-01-07 15:15:02 -0500

    add comment

--- tdrs-backend/gunicorn_start.sh
+++ tdrs-backend/gunicorn_start.sh
@@ -4,6 +4,7 @@ echo "Applying database migrations"
 python manage.py makemigrations
 python manage.py migrate
 python manage.py populate_stts
+echo "Collect Static Files"
 python manage.py collectstatic --noinput
 echo "Starting Gunicorn"
 exec gunicorn tdpservice.wsgi:application --bind 0.0.0.0:8080 --timeout 10 --workers 3 --log-file=- --log-level debug 
