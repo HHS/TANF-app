da2a2a7754511e86b1b3a45a4ef43695fc71ffd0
Aaron Beavers <foamy125@gmail.com>
2021-01-20 16:26:30 -0500

    Add new fixtures for users with specific groups

--- tdrs-backend/tdpservice/conftest.py
+++ tdrs-backend/tdpservice/conftest.py
@@ -4,6 +4,7 @@ from rest_framework.test import APIClient
 
 from tdpservice.users.test.factories import UserFactory
 from tdpservice.stts.test.factories import STTFactory, RegionFactory
+from django.contrib.auth.models import Group
 
 
 @pytest.fixture(scope="function")
@@ -16,6 +17,24 @@ def api_client():
 def user():
     """Return a basic, non-admin user."""
     return UserFactory.create()
+
+
+@pytest.fixture
+def ofa_admin():
+    """Return an ofa admin user."""
+    return UserFactory.create(groups=(Group.objects.get(name="OFA Admin"),))
+
+
+@pytest.fixture
+def ofa_analyst():
+    """Return an OFA Analyst user."""
+    return UserFactory.create(groups=(Group.objects.get(name="OFA Analyst"),))
+
+
+@pytest.fixture
+def data_prepper():
+    """Return a data prepper user."""
+    return UserFactory.create(groups=(Group.objects.get(name="Data Prepper"),))
 
 
 @pytest.fixture
