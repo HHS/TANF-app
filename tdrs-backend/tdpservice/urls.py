86fa24d6e7ed99ad447fa567b8703db50adb3ec9
John Willis <jtwillis92@gmail.com>
2021-01-26 18:08:51 -0500

    Fix linter error

--- tdrs-backend/tdpservice/urls.py
+++ tdrs-backend/tdpservice/urls.py
@@ -50,8 +50,16 @@ schema_view = get_schema_view(
 )
 
 doc_patterns = [
-    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(
+        r'^swagger(?P<format>\.json|\.yaml)$',
+        schema_view.without_ui(cache_timeout=0),
+        name='schema-json'
+    ),
+    path(
+        'swagger/',
+        schema_view.with_ui('swagger', cache_timeout=0),
+        name='schema-swagger-ui'
+    ),
     path('docs/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 
