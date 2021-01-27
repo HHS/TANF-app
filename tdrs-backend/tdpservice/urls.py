7abfe1910bc5551c5945f1e0a2e7e42c48c2cb36
John Willis <jtwillis92@gmail.com>
2021-01-27 14:54:47 -0500

    PR review feedback: use /redocs for path instead of /docs

--- tdrs-backend/tdpservice/urls.py
+++ tdrs-backend/tdpservice/urls.py
@@ -60,7 +60,7 @@ doc_patterns = [
         schema_view.with_ui('swagger', cache_timeout=0),
         name='schema-swagger-ui'
     ),
-    path('docs/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('redocs/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 
 urlpatterns.extend(doc_patterns)
