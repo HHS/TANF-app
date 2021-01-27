5cc97b1c5e60cf38659c8a679dd1b1f6587cf23a
Aaron Beavers <foamy125@gmail.com>
2021-01-27 15:51:29 -0500

    Fix typo

--- tdrs-backend/tdpservice/conftest.py
+++ tdrs-backend/tdpservice/conftest.py
@@ -4,7 +4,7 @@ from rest_framework.test import APIClient
 
 from tdpservice.users.test.factories import UserFactory
 from tdpservice.stts.test.factories import STTFactory, RegionFactory
-from tdpservice.reports.test.factoriesw import ReportFileFactory
+from tdpservice.reports.test.factories import ReportFileFactory
 
 @pytest.fixture(scope="function")
 def api_client():
