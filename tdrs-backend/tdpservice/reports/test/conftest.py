42cbc85e686804ca8d41e94189558a56336f11f9
Aaron Beavers <foamy125@gmail.com>
2021-01-27 16:42:51 -0500

    Add serializer tests

--- tdrs-backend/tdpservice/reports/test/conftest.py
+++ tdrs-backend/tdpservice/reports/test/conftest.py
@@ -4,16 +4,18 @@ import uuid
 import pytest
 
 @pytest.fixture
-def report_data():
+def report_data(user):
     """Return report creation data."""
+    print("creating report data fixture")
+    print("using user")
+    print(user.id)
     return {
-        "id": uuid.uuid4(),
         "original_filename": "report.txt",
         "slug": "report-txt-slug",
         "extension": "txt",
         "section": "Active Case Data",
+        "user": str(user.id),
         "quarter": "Q1",
         "year": "2020",
-        "version": 1,
-        "stt": "New Jersey"
+        "stt": "Michigan"
     }
--- tdrs-backend/tdpservice/reports/test/test_serializers.py
+++ tdrs-backend/tdpservice/reports/test/test_serializers.py
@@ -3,18 +3,23 @@
 import pytest
 from ..serializers import ReportFileSerializer
 
-def test_serializer_with_valid_date(report_data):
+@pytest.mark.django_db
+def test_serializer_with_valid_date(report):
     """If a serializer has valid data it will return a valid object."""
-    serializer = ReportFileSerializer(data=report_data)
-    assert serializer.is_valid() is True
+    get_serializer = ReportFileSerializer(report)
+    create_serializer = ReportFileSerializer(data=get_serializer.data)
+    assert create_serializer.is_valid() is True
 
-def test_serializer_increment_create(report_data):
+@pytest.mark.django_db
+def test_serializer_increment_create(report):
     """Test serializer produces reports with correct version"""
-    serializer_1 = ReportFileSerializer(data=report_data)
+
+    get_serializer = ReportFileSerializer(report)
+    serializer_1 = ReportFileSerializer(data=get_serializer.data)
     assert serializer_1.is_valid() is True
     report_1 = serializer_1.save()
 
-    serializer_2 = ReportFileSerializer(data=report_data)
+    serializer_2 = ReportFileSerializer(data=get_serializer.data)
     assert serializer_2.is_valid() is True
     report_2 = serializer_2.save()
 
