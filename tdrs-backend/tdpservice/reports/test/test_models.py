e420fac387bc22455117708293b4839183426939
Aaron Beavers <foamy125@gmail.com>
2021-02-08 18:05:29 -0500

    Add tests for report model versioning helpers

--- tdrs-backend/tdpservice/reports/test/test_models.py
+++ tdrs-backend/tdpservice/reports/test/test_models.py
@@ -16,3 +16,46 @@ def test_create_new_report_version(report):
         "user": report.user,
     })
     assert new_version.version == report.version + 1
+
+@pytest.mark.django_db
+def test_find_latest_version(report):
+    new_report = ReportFile.create_new_version({
+        "year": report.year,
+        "quarter": report.quarter,
+        "section": report.section,
+        "stt": report.stt,
+        "original_filename": report.original_filename,
+        "slug": report.slug,
+        "extension": report.extension,
+        "user": report.user,
+    })
+    latest_report = ReportFile.find_latest_version(
+        year=report.year,
+        quarter=report.quarter,
+        section=report.section,
+        stt=report.stt.id
+    )
+    assert latest_report.version == new_report.version
+
+
+@pytest.mark.django_db
+def test_find_latest_version_number(report):
+
+    new_report = ReportFile.create_new_version({
+        "year": report.year,
+        "quarter": report.quarter,
+        "section": report.section,
+        "stt": report.stt,
+        "original_filename": report.original_filename,
+        "slug": report.slug,
+        "extension": report.extension,
+        "user": report.user,
+    })
+    latest_version = ReportFile.find_latest_version_number(
+        year=report.year,
+        quarter=report.quarter,
+        section=report.section,
+        stt=report.stt.id
+    )
+    assert latest_version == new_report.version
+
