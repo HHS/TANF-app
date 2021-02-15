0f76c0f32ed04c4820ed5b9d99146f239bf52bed
Carlton Smith <carl@llwebconsulting.com>
2021-02-15 16:09:20 -0500

    add test to make sure endpoing is gone

--- tdrs-backend/tdpservice/users/test/test_api/test_basic.py
+++ tdrs-backend/tdpservice/users/test/test_api/test_basic.py
@@ -29,3 +29,10 @@ def test_cannot_update_user_anonymously(api_client, user):
     """Test an unauthenticated user cannot update a user."""
     response = api_client.patch(f"/v1/users/{user.pk}/", {"first_name": "Jane"})
     assert response.status_code == status.HTTP_403_FORBIDDEN
+
+
+@pytest.mark.django_db
+def test_create_user_endpoint_not_present(api_client, user_data):
+    """Test removed endpoint is no longer there."""
+    response = api_client.post("/v1/users/", user_data)
+    assert response.status_code == status.HTTP_404_NOT_FOUND
