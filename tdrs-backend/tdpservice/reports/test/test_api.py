0b306b782731ca2e49290ee50d7f10a82ba55cc1
Aaron Beavers <foamy125@gmail.com>
2021-02-17 12:07:25 -0500

    Update tests

--- tdrs-backend/tdpservice/reports/test/test_api.py
+++ tdrs-backend/tdpservice/reports/test/test_api.py
@@ -38,7 +38,7 @@ def test_create_report_file_entry(api_client, ofa_admin):
 
 @pytest.mark.django_db
 def test_report_file_version_increment(api_client, ofa_admin):
-    """Test that report file version numbers increment."""
+    """Test that report file version numbers incremented."""
     user = ofa_admin
     api_client.login(username=user.username, password="test_password")
     data1 = {
@@ -63,11 +63,16 @@ def test_report_file_version_increment(api_client, ofa_admin):
     response1 = api_client.post("/v1/reports/", data1)
     response2 = api_client.post("/v1/reports/", data2)
 
+    print(response2.data)
+    print(response1.data)
+
     assert response1.status_code == status.HTTP_201_CREATED
     assert response1.data["slug"] == data1["slug"]
 
     assert response2.status_code == status.HTTP_201_CREATED
     assert response2.data["slug"] == data2["slug"]
+
+    assert response1.data["version"] + 1 == response2.data["version"]
 
 
 @pytest.mark.django_db
@@ -86,7 +91,6 @@ def test_reports_data_prepper_permission(api_client, data_prepper):
     }
 
     response = api_client.post("/v1/reports/", data)
-    # response = api_client.post("/v1/reports/", data)
     assert response.status_code == status.HTTP_201_CREATED
 
 
@@ -106,5 +110,4 @@ def test_reports_data_prepper_not_allowed(api_client, data_prepper):
     }
 
     response = api_client.post("/v1/reports/", data)
-    # response = api_client.post("/v1/reports/", data)
     assert response.status_code == status.HTTP_403_FORBIDDEN
