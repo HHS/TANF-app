aff448e3425e8ac3723a304fa6b0cccf93c2b23b
Aaron Beavers <foamy125@gmail.com>
2021-02-19 15:48:06 -0500

    Update migration so it does not use `create_superuser`

--- tdrs-backend/tdpservice/users/migrations/0012_add_initial_super_user.py
+++ tdrs-backend/tdpservice/users/migrations/0012_add_initial_super_user.py
@@ -1,5 +1,7 @@
 import os
+from django.contrib.auth.hashers import make_password
 from django.db import migrations
+from django.utils import timezone
 
 class Migration(migrations.Migration):
     dependencies = [
@@ -7,18 +9,35 @@ class Migration(migrations.Migration):
     ]
 
     def generate_superuser(apps, schema_editor):
+        # Use the historical model to prevent this from failing on clean
+        # builds if the User model changes in the future
         User = apps.get_model('users', 'User')
 
         # set the environment variable to the username of the
         # initial superuser
         su_username = os.environ.get('DJANGO_SU_NAME', 'admin')
 
-        superuser = User.objects.create_superuser(
-            username=su_username,
-            email=su_username)
-        superuser.set_unusable_password()
+        # Get current time for date_joined
+        now = timezone.now()
 
-        superuser.save()
+        # Sets a password the user won't be able to log in with
+        # Needed because we defer to Login.gov for authentication
+        # and users mustn't be allowed to login directly.
+        unusable_password = make_password(None)
+
+        # Note we manually call `create` instead of `create_superuser`
+        # This allows us to operate on Historical Models, which
+        # do not have access to arbitrary model methods.
+        # https://docs.djangoproject.com/en/3.1/topics/migrations/#historical-models  noqa
+        User.objects.create(
+            username=su_username,
+            email=su_username,
+            date_joined=now,
+            is_active=True,
+            is_staff=True,
+            is_superuser=True,
+            password=unusable_password
+        )
 
     operations = [
         migrations.RunPython(generate_superuser),
