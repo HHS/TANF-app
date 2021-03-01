3f1f3d13494a72d0bb704d232406156a0653af31
Aaron Beavers <foamy125@gmail.com>
2021-03-01 15:25:23 -0500

    Add new routes

--- tdrs-backend/tdpservice/reports/urls.py
+++ tdrs-backend/tdpservice/reports/urls.py
@@ -2,11 +2,23 @@
 
 from rest_framework.routers import DefaultRouter
 from . import views
+from django.urls import path
 
 router = DefaultRouter()
 
 router.register("", views.ReportFileViewSet)
 
-urlpatterns = []
+urlpatterns = [
+    path(
+        "<str:year>/<str:quarter>/<str:section>",
+        views.GetReport.as_view(),
+        name="get-report",
+    ),
+    path(
+        "years/<int:stt>",
+        views.GetList.as_view(),
+        name="get-report",
+    )
+]
 
 urlpatterns += router.urls
