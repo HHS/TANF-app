a9e62ad8f779050715c6661c9f808524789086af
Jorge Gonzalez <jorgegonzalez@users.noreply.github.com>
2021-03-23 10:36:08 -0400

    add more tests

--- tdrs-frontend/src/reducers/reports.test.js
+++ tdrs-frontend/src/reducers/reports.test.js
@@ -1,7 +1,7 @@
 import reducer from './reports'
-import { SET_SELECTED_YEAR } from '../actions/reports'
+import { SET_SELECTED_STT, SET_SELECTED_YEAR } from '../actions/reports'
 
-describe('reducers/upload', () => {
+describe('reducers/reports', () => {
   it('should return the initial state', () => {
     expect(reducer(undefined, {})).toEqual({
       year: 2020,
@@ -20,6 +20,20 @@ describe('reducers/upload', () => {
     ).toEqual({
       year: 2021,
       stt: '',
+    })
+  })
+
+  it('should handle "SET_SELECTED_STT"', () => {
+    expect(
+      reducer(undefined, {
+        type: SET_SELECTED_STT,
+        payload: {
+          stt: 'florida',
+        },
+      })
+    ).toEqual({
+      year: 2020, // 2020 is in the default initial state
+      stt: 'florida',
     })
   })
 })
