bd3b43ca5b35cdd8e5fbe444f47b287624b20cea
Jorge Gonzalez <jorgegonzalez@users.noreply.github.com>
2021-04-09 10:43:58 -0400

    Fix linter

--- tdrs-backend/tdpservice/core/views.py
+++ tdrs-backend/tdpservice/core/views.py
@@ -1,3 +1,4 @@
+"""Define core, generic views of the app."""
 import logging
 
 from rest_framework.response import Response
@@ -5,8 +6,13 @@ from rest_framework.decorators import api_view
 
 logger = logging.getLogger()
 
+
 @api_view(['POST'])
 def write_logs(request):
+    """Pass request bodies to the system logger.
+
+    Mainly used to log client-side alerts and errors.
+    """
     logger.info(request.data)
 
     return Response('Success')
