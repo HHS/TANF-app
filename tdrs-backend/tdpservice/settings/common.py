617e9581c7842d39647ee673a1221c3911dd3193
Aaron Beavers <foamy125@gmail.com>
2021-04-13 11:51:25 -0400

    Add urls to CSRF_TRUSTED_ORIGINS

--- tdrs-backend/tdpservice/settings/common.py
+++ tdrs-backend/tdpservice/settings/common.py
@@ -273,6 +273,7 @@ class Common(Configuration):
     # of API POST calls to prevent false negative authorization errors.
     # https://docs.djangoproject.com/en/2.2/ref/settings/#csrf-cookie-httponly
     CSRF_COOKIE_HTTPONLY = False
+    CSRF_TRUSTED_ORIGINS = ['.app.cloud.gov','.ach.hhs.gov']
 
     SESSION_COOKIE_PATH = "/;HttpOnly"
 
