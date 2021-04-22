c94761e53b7b6555869edfa692afa192262fd0e7
John Willis <jtwillis92@gmail.com>
2021-04-22 11:21:53 -0400

    Update zap-scanner.sh

--- tdrs-frontend/zap-scanner.sh
+++ tdrs-frontend/zap-scanner.sh
@@ -6,29 +6,32 @@ elif command -v shasum >/dev/null ; then
 	SHASUM=shasum
 fi
 
-
-
 docker-compose down
 docker-compose up -d --build
 
-	# do an OWASP ZAP scan
- 	export ZAP_CONFIG=" \
-	  -config globalexcludeurl.url_list.url\(0\).regex='.*/robots\.txt.*' \
-	  -config globalexcludeurl.url_list.url\(0\).description='Exclude robots.txt' \
-	  -config globalexcludeurl.url_list.url\(0\).enabled=true \
-	  -config spider.postform=true"
+# do an OWASP ZAP scan
+export ZAP_CONFIG=" \
+  -config globalexcludeurl.url_list.url\(0\).regex='.*/robots\.txt.*' \
+  -config globalexcludeurl.url_list.url\(0\).description='Exclude robots.txt' \
+  -config globalexcludeurl.url_list.url\(0\).enabled=true \
+  -config spider.postform=true"
 
-	echo "================== OWASP ZAP tests =================="
-	chmod 777 $(pwd)/reports
-  docker-compose run zaproxy zap-full-scan.py \
-    -t http://tdp-frontend \
-    -m 5 \
-    -z "${ZAP_CONFIG}" \
-    -r owasp_report.html | tee /dev/tty | grep -q "FAIL-NEW: 0"
-  ZAPEXIT=$?
+echo "================== OWASP ZAP tests =================="
+# Ensure the reports directory can be written to
+chmod 777 $(pwd)/reports
 
- docker-compose down --remove-orphan
+# Run the ZAP scan and generate an HTML report
+docker-compose run zaproxy zap-full-scan.py \
+	-t http://tdp-frontend \
+	-m 5 \
+	-z "${ZAP_CONFIG}" \
+	-r owasp_report.html | tee /dev/tty | grep -q "FAIL-NEW: 0"
 
+# The `grep -q` piped to the end of the previous command will return a
+# 0 exit code if the term is found and 1 otherwise.
+ZAPEXIT=$?
+
+docker-compose down --remove-orphan
 
 EXIT=0
 
