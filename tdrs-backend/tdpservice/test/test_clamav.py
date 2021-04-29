fc0729ac0d5065c338d37b92d9be90ba5645580f
John Willis <jtwillis92@gmail.com>
2021-04-29 00:50:13 -0400

    Utilize raw string for EICAR test file

--- tdrs-backend/tdpservice/test/test_clamav.py
+++ tdrs-backend/tdpservice/test/test_clamav.py
@@ -34,7 +34,7 @@ def infected_file():
     https://en.wikipedia.org/wiki/EICAR_test_file
     """
     return StringIO(
-        'X5O!P%@AP[4\\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H*'
+        r'X5O!P%@AP[4\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H*'
     )
 
 
