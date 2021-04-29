83d4f31a13c056288c2a803b4b5819c50b79c0d5
Jorge Gonzalez <jorgegonzalez@users.noreply.github.com>
2021-04-29 10:57:22 -0400

    Tweak permissions

--- tdrs-backend/tdpservice/core/views.py
+++ tdrs-backend/tdpservice/core/views.py
@@ -3,10 +3,10 @@ import logging
 
 from django.contrib.admin.models import LogEntry, CHANGE
 from django.contrib.contenttypes.models import ContentType
-from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 
+from ..users.permissions import IsUser
 from ..users.models import User
 from ..reports.models import ReportFile
 
@@ -14,7 +14,7 @@ logger = logging.getLogger()
 
 
 @api_view(['POST'])
-@permission_classes([IsAuthenticated])
+@permission_classes([IsUser])
 def write_logs(request):
     """Pass request bodies to the system logger.
 
@@ -29,7 +29,7 @@ def write_logs(request):
     if 'files' in data:
         for file in data['files']:
             LogEntry.objects.log_action(
-                user_id=User.objects.get(username=data['user']),
+                user_id=User.objects.get(username=data['user']).pk,
                 content_type_id=ContentType.objects.get_for_model(ReportFile).pk,
                 object_id=file,
                 object_repr=str(data),
