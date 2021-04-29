d80806f34dd68feed18ca8673e99460b45391fdf
Jorge Gonzalez <jorgegonzalez@users.noreply.github.com>
2021-04-29 14:14:55 -0400

    Lint

--- tdrs-backend/tdpservice/core/admin.py
+++ tdrs-backend/tdpservice/core/admin.py
@@ -1,3 +1,4 @@
+"""Admin class for LogEntry objects."""
 from django.contrib import admin
 from django.contrib.admin.models import LogEntry, DELETION
 from django.utils.html import escape
@@ -9,7 +10,7 @@ admin.site.unregister(LogEntry)
 
 @admin.register(LogEntry)
 class LogEntryAdmin(admin.ModelAdmin):
-    """Customize and restrict the LogEntry table in Django Admin"""
+    """Customize and restrict the LogEntry table in Django Admin."""
 
     date_hierarchy = 'action_time'
 
@@ -33,29 +34,30 @@ class LogEntryAdmin(admin.ModelAdmin):
     ]
 
     def has_add_permission(self, request):
-        """Deny the user permission to create Report Files in Django Admin"""
+        """Deny the user permission to create Report Files in Django Admin."""
         return False
 
     def has_change_permission(self, request, obj=None):
-        """Deny the user permission to update Report Files in Django Admin"""
+        """Deny the user permission to update Report Files in Django Admin."""
         return False
 
     def has_delete_permission(self, request, obj=None):
-        """Deny the user permission to delete Report Files in Django Admin"""
+        """Deny the user permission to delete Report Files in Django Admin."""
         return False
 
     def has_view_permission(self, request, obj=None):
-        """Only allow superusers to be able to view Report Files"""
+        """Only allow superusers to be able to view Report Files."""
         return request.user.is_superuser
 
     def object_link(self, obj):
-        """Create a link to to corresponding objects for a given LogEntry"""
+        """Create a link to to corresponding objects for a given LogEntry."""
         if obj.action_flag == DELETION:
             link = escape(obj.object_repr)
         else:
             ct = obj.content_type
             link = '<a href="%s">%s</a>' % (
-                reverse('admin:%s_%s_change' % (ct.app_label, ct.model), args=[obj.object_id]),
+                reverse('admin:%s_%s_change' % (ct.app_label, ct.model),
+                        args=[obj.object_id]),
                 escape(obj.object_repr),
             )
         return mark_safe(link)
--- tdrs-backend/tdpservice/reports/admin.py
+++ tdrs-backend/tdpservice/reports/admin.py
@@ -1,22 +1,24 @@
+"""Admin class for ReportFile objects."""
 from django.contrib import admin
 from .models import ReportFile
 
 
 @admin.register(ReportFile)
 class ReportFileAdmin(admin.ModelAdmin):
-    """Enforce read-only on the Report File admin form"""
+    """Enforce read-only on the Report File admin form."""
+
     def has_add_permission(self, request):
-        """Deny the user permission to create Report Files in Django Admin"""
+        """Deny the user permission to create Report Files in Django Admin."""
         return False
 
     def has_change_permission(self, request, obj=None):
-        """Deny the user permission to update Report Files in Django Admin"""
+        """Deny the user permission to update Report Files in Django Admin."""
         return False
 
     def has_delete_permission(self, request, obj=None):
-        """Deny the user permission to delete Report Files in Django Admin"""
+        """Deny the user permission to delete Report Files in Django Admin."""
         return False
 
     def has_view_permission(self, request, obj=None):
-        """Only allow superusers to be able to view Report Files"""
+        """Only allow superusers to be able to view Report Files."""
         return request.user.is_superuser
