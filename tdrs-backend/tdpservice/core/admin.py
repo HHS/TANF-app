395e10f9586d8c241b80ee6d25134f9679383d40
Jorge Gonzalez <jorgegonzalez@users.noreply.github.com>
2021-04-29 12:26:19 -0400

    Add docstrings

--- tdrs-backend/tdpservice/core/admin.py
+++ tdrs-backend/tdpservice/core/admin.py
@@ -9,6 +9,8 @@ admin.site.unregister(LogEntry)
 
 @admin.register(LogEntry)
 class LogEntryAdmin(admin.ModelAdmin):
+    """Customize and restrict the LogEntry table in Django Admin"""
+
     date_hierarchy = 'action_time'
 
     list_filter = [
@@ -31,18 +33,23 @@ class LogEntryAdmin(admin.ModelAdmin):
     ]
 
     def has_add_permission(self, request):
+        """Deny the user permission to create Report Files in Django Admin"""
         return False
 
     def has_change_permission(self, request, obj=None):
+        """Deny the user permission to update Report Files in Django Admin"""
         return False
 
     def has_delete_permission(self, request, obj=None):
+        """Deny the user permission to delete Report Files in Django Admin"""
         return False
 
     def has_view_permission(self, request, obj=None):
+        """Only allow superusers to be able to view Report Files"""
         return request.user.is_superuser
 
     def object_link(self, obj):
+        """Create a link to to corresponding objects for a given LogEntry"""
         if obj.action_flag == DELETION:
             link = escape(obj.object_repr)
         else:
--- tdrs-backend/tdpservice/reports/admin.py
+++ tdrs-backend/tdpservice/reports/admin.py
@@ -4,14 +4,19 @@ from .models import ReportFile
 
 @admin.register(ReportFile)
 class ReportFileAdmin(admin.ModelAdmin):
+    """Enforce read-only on the Report File admin form"""
     def has_add_permission(self, request):
+        """Deny the user permission to create Report Files in Django Admin"""
         return False
 
     def has_change_permission(self, request, obj=None):
+        """Deny the user permission to update Report Files in Django Admin"""
         return False
 
     def has_delete_permission(self, request, obj=None):
+        """Deny the user permission to delete Report Files in Django Admin"""
         return False
 
     def has_view_permission(self, request, obj=None):
+        """Only allow superusers to be able to view Report Files"""
         return request.user.is_superuser
