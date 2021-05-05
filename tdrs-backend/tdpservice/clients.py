53005a4b33b5b05023f59c8a9a3fbae18b93cce4
John Willis <jtwillis92@gmail.com>
2021-05-05 16:39:16 -0400

    Issue 818 (PR #1): Implement ClamAV Client for use with uploads

--- tdrs-backend/tdpservice/clients.py
+++ tdrs-backend/tdpservice/clients.py
@@ -1,11 +1,76 @@
 """Globally available external client services."""
+from requests.adapters import HTTPAdapter
+from requests.exceptions import ConnectionError
+from requests.packages.urllib3.util.retry import Retry
+from requests.sessions import Session
+import logging
+
 from boto3 import client
 from django.conf import settings
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+
+class ClamAVClient:
+    class ServiceUnavailable(Exception):
+        pass
+
+    # https://github.com/raft-tech/clamav-rest#status-codes
+    SCAN_CODES = {
+        'CLEAN': [200],
+        'INFECTED': [406],
+        'ERROR': [400, 412, 429, 500, 501]
+    }
+
+    def __init__(self, endpoint_url=None):
+        if not endpoint_url:
+            endpoint_url = settings.AV_SCAN_URL
+
+        self.endpoint_url = endpoint_url
+        self.session = self.init_session()
+
+    def init_session(self):
+        session = Session()
+        retries = Retry(
+            backoff_factor=settings.AV_SCAN_BACKOFF_FACTOR,
+            status_forcelist=self.SCAN_CODES['ERROR'],
+            total=settings.AV_SCAN_MAX_RETRIES
+        )
+        session.mount(self.endpoint_url, HTTPAdapter(max_retries=retries))
+        return session
+
+    def scan_file(self, file, file_name) -> bool:
+        logger.debug(f'Initiating virus scan for file: {file_name}')
+        try:
+            scan_response = self.session.post(
+                self.endpoint_url,
+                data={'name': file_name},
+                files={'file': file},
+                timeout=settings.AV_SCAN_TIMEOUT
+            )
+        except ConnectionError as err:
+            logger.debug(f'Encountered error scanning file: {err}')
+            raise self.ServiceUnavailable()
+
+        if scan_response.status_code in self.SCAN_CODES['CLEAN']:
+            logger.debug(
+                f'File scan marked as CLEAN for file: {file_name}'
+            )
+            return True
+
+        if scan_response.status_code in self.SCAN_CODES['INFECTED']:
+            logger.debug(
+                f'File scan marked as INFECTED for file: {file_name}'
+            )
+            return False
+
+        logger.debug(f'Unable to scan file with name: {file_name}')
+        return False
 
 
 def get_s3_client():
     """Return an S3 client that points to localstack or AWS based on settings.
-
     Intended to be used in place of a direct boto3 client.
     """
     region_name = settings.AWS_REGION_NAME
