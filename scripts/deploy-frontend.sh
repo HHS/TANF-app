2124ac361e903d2a82d796d47f02bce739885353
John Willis <jtwillis92@gmail.com>
2021-05-06 16:54:11 -0400

    Install nodeJS and Yarn for frontend deploy

--- scripts/deploy-frontend.sh
+++ scripts/deploy-frontend.sh
@@ -12,20 +12,17 @@ DEPLOY_ENV=${2}
 CGHOSTNAME_FRONTEND=${3}
 CGHOSTNAME_BACKEND=${4}
 
-
 update_frontend()
 {
-
-
-    echo DEPLOY_STRATEGY: $DEPLOY_STRATEGY
-    echo DEPLOY_ENV: $DEPLOY_ENV
-    echo FRONTEND_HOST: $CGHOSTNAME_FRONTEND
-    echo BACKEND_HOST: $CGHOSTNAME_BACKEND
-    cd tdrs-frontend
+    echo DEPLOY_STRATEGY: "$DEPLOY_STRATEGY"
+    echo DEPLOY_ENV: "$DEPLOY_ENV"
+    echo FRONTEND_HOST: "$CGHOSTNAME_FRONTEND"
+    echo BACKEND_HOST: "$CGHOSTNAME_BACKEND"
+    cd tdrs-frontend || exit
 
     echo "REACT_APP_BACKEND_URL=https://$CGHOSTNAME_BACKEND.app.cloud.gov/v1" >> .env.production
     echo "REACT_APP_BACKEND_HOST=https://$CGHOSTNAME_BACKEND.app.cloud.gov" >> .env.production
-    npm run build
+    yarn build:production
     unlink .env.production
     mkdir deployment
 
@@ -34,26 +31,25 @@ update_frontend()
     cp  nginx/mime.types deployment/mime.types
 
     cp manifest.buildpack.yml deployment/manifest.buildpack.yml
+    cd deployment || exit
 
-    cd deployment
-	  if [ "$1" = "rolling" ] ; then
-		    # Do a zero downtime deploy.  This requires enough memory for
-		    # two apps to exist in the org/space at one time.
-		    cf push $CGHOSTNAME_FRONTEND --no-route -f manifest.buildpack.yml --strategy rolling || exit 1
-	  else
-		    cf push $CGHOSTNAME_FRONTEND --no-route -f manifest.buildpack.yml
-	  fi
-	  cf map-route $CGHOSTNAME_FRONTEND app.cloud.gov --hostname "${CGHOSTNAME_FRONTEND}"
+    if [ "$1" = "rolling" ] ; then
+        # Do a zero downtime deploy.  This requires enough memory for
+        # two apps to exist in the org/space at one time.
+        cf push "$CGHOSTNAME_FRONTEND" --no-route -f manifest.buildpack.yml --strategy rolling || exit 1
+    else
+        cf push "$CGHOSTNAME_FRONTEND" --no-route -f manifest.buildpack.yml
+    fi
+
+    cf map-route "$CGHOSTNAME_FRONTEND" app.cloud.gov --hostname "${CGHOSTNAME_FRONTEND}"
     cd ../..
     rm -r tdrs-frontend/deployment
 }
 
-
-# perform a rolling update for the backend and frontend deployments if specifed,
-# otherwise perform a normal deployment 
-if [ $DEPLOY_STRATEGY = "rolling" ] ; then
-
-	  update_frontend 'rolling'
+# perform a rolling update for the backend and frontend deployments if
+# specified, otherwise perform a normal deployment
+if [ "$DEPLOY_STRATEGY" = "rolling" ] ; then
+    update_frontend 'rolling'
 else
-	  update_frontend
+    update_frontend
 fi
