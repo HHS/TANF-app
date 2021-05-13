db71f7ebac7c3eb34ea89aa2b0304df61cd405f8
Aaron Beavers <foamy125@gmail.com>
2021-05-13 12:47:43 -0400

    initial work on file download

--- tdrs-backend/tdpservice/reports/views.py
+++ tdrs-backend/tdpservice/reports/views.py
@@ -3,6 +3,7 @@ import logging
 
 from rest_framework.parsers import MultiPartParser
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.decorators import action
 
 from tdpservice.reports.models import ReportFile
 from tdpservice.reports.serializers import ReportFileSerializer
@@ -19,3 +20,8 @@ class ReportFileViewSet(ModelViewSet):
     permission_classes = [CanUploadReport]
     serializer_class = ReportFileSerializer
     queryset = ReportFile.objects.all()
+
+    @action(methods=["get"], detail=True)
+    def download(self, request, pk=None):
+        """Retrieve a file from s3 then stream it to the client."""
+        serializer = self.get_serializer(self.request.user, request.data)
