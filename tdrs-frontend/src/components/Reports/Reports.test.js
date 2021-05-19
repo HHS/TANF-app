f9d465f86c77de8bcef28e1ac72331fad8a697c0
Jorge Gonzalez <jorgegonzalez@users.noreply.github.com>
2021-05-19 15:10:00 -0400

    Fix tests

--- tdrs-frontend/src/components/Reports/Reports.test.js
+++ tdrs-frontend/src/components/Reports/Reports.test.js
@@ -164,7 +164,7 @@ describe('Reports', () => {
     expect(getByText('2021', { selector: 'option' }).selected).toBe(true)
   })
 
-  it('should render the UploadReports form when a year is selected and Search button is clicked', () => {
+  it('should render the UploadReports form when a year is selected and Search button is clicked', async () => {
     const store = mockStore({
       ...initialState,
       reports: {
@@ -184,14 +184,15 @@ describe('Reports', () => {
     expect(queryByText('Section 1 - Active Case Data')).not.toBeInTheDocument()
 
     fireEvent.click(getByText(/Search/, { selector: 'button' }))
-
-    expect(getByText('Section 1 - Active Case Data')).toBeInTheDocument()
-    expect(getByText('Section 2 - Closed Case Data')).toBeInTheDocument()
-    expect(getByText('Section 3 - Aggregate Data')).toBeInTheDocument()
-    expect(getByText('Section 4 - Stratum Data')).toBeInTheDocument()
+    await waitFor(() => {
+      expect(getByText('Section 1 - Active Case Data')).toBeInTheDocument()
+      expect(getByText('Section 2 - Closed Case Data')).toBeInTheDocument()
+      expect(getByText('Section 3 - Aggregate Data')).toBeInTheDocument()
+      expect(getByText('Section 4 - Stratum Data')).toBeInTheDocument()
+    })
   })
 
-  it('should not de-render the UploadReports form after it has been toggled but the year is changed', () => {
+  it('should not de-render the UploadReports form after it has been toggled but the year is changed', async () => {
     const store = mockStore({
       ...initialState,
       reports: {
@@ -212,7 +213,9 @@ describe('Reports', () => {
 
     fireEvent.click(getByText(/Search/, { selector: 'button' }))
 
-    expect(queryByText('Section 1 - Active Case Data')).toBeInTheDocument()
+    await waitFor(() => {
+      expect(queryByText('Section 1 - Active Case Data')).toBeInTheDocument()
+    })
 
     const select = getByLabelText(/Fiscal Year/)
 
@@ -225,7 +228,7 @@ describe('Reports', () => {
     expect(queryByText('Section 1 - Active Case Data')).toBeInTheDocument()
   })
 
-  it('should de-render when Cancel is clicked', () => {
+  it('should de-render when Cancel is clicked', async () => {
     const store = mockStore({
       ...initialState,
       reports: {
@@ -243,8 +246,9 @@ describe('Reports', () => {
     )
 
     fireEvent.click(getByText(/Search/, { selector: 'button' }))
-
-    expect(getByText('Section 1 - Active Case Data')).toBeInTheDocument()
+    await waitFor(() => {
+      expect(getByText('Section 1 - Active Case Data')).toBeInTheDocument()
+    })
 
     fireEvent.click(getByText(/Cancel/))
 
