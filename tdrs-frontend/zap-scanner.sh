ba442b158e3b3bf7d36e1e11c1ce4ba32306695b
Aaron Beavers <foamy125@gmail.com>
2021-05-20 15:27:44 -0400

    Update frontend zap scanner to only run in CI

--- tdrs-frontend/zap-scanner.sh
+++ tdrs-frontend/zap-scanner.sh
@@ -20,13 +20,22 @@ echo "================== OWASP ZAP tests =================="
 # Ensure the reports directory can be written to
 chmod 777 $(pwd)/reports
 
-# Run the ZAP scan and generate an HTML report
-docker-compose run zaproxy zap-full-scan.py \
-	-t http://tdp-frontend \
-	-m 5 \
-	-z "${ZAP_CONFIG}" \
-  -c "zap.conf" \
-	-r owasp_report.html | tee /dev/tty | grep -q "FAIL-NEW: 0"
+# check if running in circle CI
+
+if [ -z "${CIRCLE_BRANCH+x}"]; then
+    docker-compose run zaproxy zap-full-scan.py \
+                   -t http://web:8080/ \
+                   -m 5 \
+                   -z "${ZAP_CONFIG}" \
+                   -c "zap.conf" \
+                   -r owasp_report.html | tee /dev/tty | grep -q "FAIL-NEW: 0"
+else
+    docker-compose run zaproxy zap-full-scan.py \
+                   -t http://web:8080/ \
+                   -m 5 \
+                   -z "${ZAP_CONFIG}" \
+                   -c "zap.conf" \
+                   -r owasp_report.html | tee /dev/tty | grep -q "FAIL-NEW: 0"
 
 # The `grep -q` piped to the end of the previous command will return a
 # 0 exit code if the term is found and 1 otherwise.
