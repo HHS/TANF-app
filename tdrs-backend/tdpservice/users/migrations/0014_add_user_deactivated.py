9fab304119e2e393062a9954367cef561ed441e2
Jorge Gonzalez <jorgegonzalez@users.noreply.github.com>
2021-06-03 11:32:41 -0400

    Fix migration

--- tdrs-backend/tdpservice/users/migrations/0014_add_user_deactivated.py
+++ tdrs-backend/tdpservice/users/migrations/0014_add_user_deactivated.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('users', '0012_add_initial_super_user'),
+        ('users', '0013_user_sub'),
     ]
 
     operations = [
