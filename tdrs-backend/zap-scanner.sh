d1eb8d165b8ac76910c7cdac1b17145a3e1ab2fc
John Willis <jtwillis92@gmail.com>
2021-06-03 17:42:40 -0400

    Remove unnecessary down/build/up steps in zap-scanner to account for increased web startup time

--- tdrs-backend/zap-scanner.sh
+++ tdrs-backend/zap-scanner.sh
@@ -1,28 +1,21 @@
 #!/bin/sh
 
-if command -v sha256sum >/dev/null ; then
-	SHASUM=sha256sum
-elif command -v shasum >/dev/null ; then
-	SHASUM=shasum
-fi
+# do an OWASP ZAP scan
+export ZAP_CONFIG=" \
+  -config globalexcludeurl.url_list.url\(0\).regex='.*/robots\.txt.*' \
+  -config globalexcludeurl.url_list.url\(0\).description='Exclude robots.txt' \
+  -config globalexcludeurl.url_list.url\(0\).enabled=true \
+  -config spider.postform=true"
 
-docker-compose down
-docker-compose up -d --build
-    # do an OWASP ZAP scan
-     export ZAP_CONFIG=" \
-      -config globalexcludeurl.url_list.url\(0\).regex='.*/robots\.txt.*' \
-      -config globalexcludeurl.url_list.url\(0\).description='Exclude robots.txt' \
-      -config globalexcludeurl.url_list.url\(0\).enabled=true \
-      -config spider.postform=true"
+echo "================== OWASP ZAP tests =================="
+chmod 777 $(pwd)/reports
+docker-compose run zaproxy zap-full-scan.py \
+    -t http://web:8080/ \
+    -m 5 \
+    -z "${ZAP_CONFIG}" \
+    -r owasp_report.html | tee /dev/tty | grep -q "FAIL-NEW: 0"
 
-    echo "================== OWASP ZAP tests =================="
-    chmod 777 $(pwd)/reports
-    docker-compose run zaproxy zap-full-scan.py \
-        -t http://web:8080/ \
-        -m 5 \
-        -z "${ZAP_CONFIG}" \
-        -r owasp_report.html | tee /dev/tty | grep -q "FAIL-NEW: 0"
-    ZAPEXIT=$?
+ZAPEXIT=$?
 
 EXIT=0
 
@@ -30,7 +23,5 @@ if [ "$ZAPEXIT" = 1 ] ; then
     echo "OWASP ZAP scan failed"
     EXIT=1
 fi
-
-docker-compose down --remove-orphan
 
 exit $EXIT
