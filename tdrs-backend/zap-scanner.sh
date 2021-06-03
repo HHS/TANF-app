16656ea715c89eb3ad8397bee1cc0f979657650a
John Willis <jtwillis92@gmail.com>
2021-06-03 17:56:39 -0400

    Revert zap changes that didn't do anything

--- tdrs-backend/zap-scanner.sh
+++ tdrs-backend/zap-scanner.sh
@@ -1,21 +1,28 @@
 #!/bin/sh
 
-# do an OWASP ZAP scan
-export ZAP_CONFIG=" \
-  -config globalexcludeurl.url_list.url\(0\).regex='.*/robots\.txt.*' \
-  -config globalexcludeurl.url_list.url\(0\).description='Exclude robots.txt' \
-  -config globalexcludeurl.url_list.url\(0\).enabled=true \
-  -config spider.postform=true"
+if command -v sha256sum >/dev/null ; then
+	SHASUM=sha256sum
+elif command -v shasum >/dev/null ; then
+	SHASUM=shasum
+fi
 
-echo "================== OWASP ZAP tests =================="
-chmod 777 $(pwd)/reports
-docker-compose run zaproxy zap-full-scan.py \
-    -t http://web:8080/ \
-    -m 5 \
-    -z "${ZAP_CONFIG}" \
-    -r owasp_report.html | tee /dev/tty | grep -q "FAIL-NEW: 0"
+docker-compose down
+docker-compose up -d --build
+    # do an OWASP ZAP scan
+     export ZAP_CONFIG=" \
+      -config globalexcludeurl.url_list.url\(0\).regex='.*/robots\.txt.*' \
+      -config globalexcludeurl.url_list.url\(0\).description='Exclude robots.txt' \
+      -config globalexcludeurl.url_list.url\(0\).enabled=true \
+      -config spider.postform=true"
 
-ZAPEXIT=$?
+    echo "================== OWASP ZAP tests =================="
+    chmod 777 $(pwd)/reports
+    docker-compose run zaproxy zap-full-scan.py \
+        -t http://web:8080/ \
+        -m 5 \
+        -z "${ZAP_CONFIG}" \
+        -r owasp_report.html | tee /dev/tty | grep -q "FAIL-NEW: 0"
+    ZAPEXIT=$?
 
 EXIT=0
 
@@ -23,5 +30,7 @@ if [ "$ZAPEXIT" = 1 ] ; then
     echo "OWASP ZAP scan failed"
     EXIT=1
 fi
+
+docker-compose down --remove-orphan
 
 exit $EXIT
