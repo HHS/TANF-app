d3bd8060020bccd576e1a807a75fd46bffff0982
John Willis <jtwillis92@gmail.com>
2021-06-10 15:40:53 -0400

    Move S3 bucket check from CI to wait_for_services

--- tdrs-backend/wait_for_services.sh
+++ tdrs-backend/wait_for_services.sh
@@ -5,5 +5,14 @@ wait-for-it \
     --timeout 60 \
     -- echo "ClamAV and Localstack are ready!"
 
+# Wait for Localstack to create the necessary S3 bucket
+echo "Waiting 60 seconds for S3 bucket creation"
+# shellcheck disable=SC2091
+until $(awslocal s3api head-bucket --bucket "$AWS_BUCKET" > /dev/null 2>&1); do
+    ((c++)) && ((c==10)) && echo "S3 bucket not found" && exit 1;
+    sleep 6;
+done
+echo "S3 Bucket is ready after $((c*6)) seconds"
+
 # NOTE: Consider deprecating this script and using the above command instead.
 python wait_for_postgres.py
