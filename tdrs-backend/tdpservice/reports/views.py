15f443c29cbcc6161d611286c94c3dce52a74dc6
Aaron Beavers <foamy125@gmail.com>
2021-06-14 13:39:48 -0400

    Tests pass

--- tdrs-backend/tdpservice/reports/views.py
+++ tdrs-backend/tdpservice/reports/views.py
@@ -32,10 +32,11 @@ class ReportFileViewSet(ModelViewSet):
     @action(methods=["get"], detail=True)
     def download(self, request, pk=None):
         """Retrieve a file from s3 then stream it to the client."""
-        record = DownloadReportFileSerializer(id=pk)
+        record = DownloadReportFileSerializer(ReportFile.objects.get(id=pk))
 
-        response = StreamingHttpResponse(streaming_content=record.file)
-        response['Content-Disposition'] = f'attachement; filename="{record.file_name}"'
+        response = StreamingHttpResponse(streaming_content=record.data['file'])
+        file_name = record.data['original_filename']
+        response['Content-Disposition'] = f'attachement; filename="{file_name}"'
         return response
 
 
