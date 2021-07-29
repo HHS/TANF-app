1481606cec91d02b08e31f3a0a6c12f70db722ba
abottoms-coder <abottoms@goraft.tech>
2021-07-29 09:56:40 -0400

    Testing again

--- terraform/create_backend_vars.sh
+++ terraform/create_backend_vars.sh
@@ -11,7 +11,6 @@ fi
 ACCESS_KEY=$(echo "${S3_CREDENTIALS}" | jq -r '.access_key_id')
 SECRET_KEY=$(echo "${S3_CREDENTIALS}" | jq -r '.secret_access_key')
 REGION=$(echo "${S3_CREDENTIALS}" | jq -r '.region')
-
 {
   echo "access_key = \"$ACCESS_KEY\""
   echo "secret_key = \"$SECRET_KEY\""
