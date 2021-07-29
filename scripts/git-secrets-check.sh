8f695ae49d6eb74eb9d0ec33ab08318657098575
abottoms-coder <abottoms@goraft.tech>
2021-07-29 14:32:08 -0400

    After digging, it's just a shellscript, no make necessary? Also trying to move it to tmp

--- scripts/git-secrets-check.sh
+++ scripts/git-secrets-check.sh
@@ -1,26 +1,14 @@
 #!/bin/bash
 set -e
 
-if command -v git-secrets /dev/null 2>&1; then
-    echo The command git secrets is available
+if [ -d /tmp/git-secrets ]; then
+    echo The command git-secrets is available
 else
-    echo The command git secrets is not available, installing...
-    oldpwd=$PWD
-    cd /tmp/
-    git clone git@github.com:awslabs/git-secrets.git
-    cd git-secrets
-    if command -v make /dev/null 2>&1; then
-        echo The command make is available
-    else
-        echo The command make is not available, installing...
-        apt-get update && apt-get install -y make
-    fi
-    make install
-    cd $oldpwd
-    #rm -rf /tmp/git-secrets
+    echo The command git-secrets is not available, installing...
+    git clone git@github.com:awslabs/git-secrets.git /tmp/
 fi
 
 # ensure we have correct configs in place
 [ -f ../.gitconfig ]
 cat ../.gitconfig >> .git/config 
-git-secrets --scan -r ../
+/tmp/git-secrets/git-secrets --scan -r ../
