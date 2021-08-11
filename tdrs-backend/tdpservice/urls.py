2579001386e5bac0256a4400a2c33571e6a03d97
Aaron Beavers <foamy125@gmail.com>
2021-08-11 10:52:52 -0400

    remove unused import

--- tdrs-backend/tdpservice/urls.py
+++ tdrs-backend/tdpservice/urls.py
@@ -2,7 +2,6 @@
 
 import os
 
-from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path, re_path, reverse_lazy
