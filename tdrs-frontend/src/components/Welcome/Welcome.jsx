7009bb70e1db939999ed64fc67bb4c45518041de
Jorge Gonzalez <jorgegonzalez@users.noreply.github.com>
2021-08-23 14:29:52 -0400

    feat: Implement welcome page for logged-in users

---
+++ tdrs-frontend/src/components/Welcome/Welcome.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import Button from '../Button'
+import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { useSelector } from 'react-redux'
+
+function Welcome() {
+  const logOut = () => {
+    window.location = `${process.env.REACT_APP_BACKEND_URL}/logout/oidc`
+  }
+  const user = useSelector((state) => state.auth.user)
+  const role = user?.roles
+  const hasRole = Boolean(role?.length > 0)
+
+  return (
+    <div className="margin-top-5">
+      {!hasRole ? (
+        <p className="margin-top-1 margin-bottom-4">
+          Your request for access is currently being reviewed by an OFA Admin.
+          We'll send you an email when it's been approved.
+        </p>
+      ) : (
+        <>
+          <p className="margin-top-1 margin-bottom-4">
+            You've been approved as a/an {role?.[0].name}. You'll be able to do
+            the following in TDP:
+          </p>
+          <ul>
+            {role?.[0]?.permissions?.map((permission) => (
+              <li key={permission.id} id={permission.id}>
+                {permission.name}
+              </li>
+            ))}
+          </ul>
+        </>
+      )}
+      <Button type="button" className="width-tablet" onClick={logOut}>
+        <FontAwesomeIcon className="margin-right-1" icon={faSignOutAlt} />
+        Sign Out
+      </Button>
+    </div>
+  )
+}
+
+export default Welcome
---
+++ tdrs-frontend/src/components/Welcome/Welcome.test.jsx
@@ -0,0 +1,152 @@
+import React from 'react'
+import { fireEvent, render, screen, waitFor } from '@testing-library/react'
+
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+import configureStore from 'redux-mock-store'
+import Welcome from './Welcome'
+
+describe('Welcome', () => {
+  const initialState = {
+    auth: {
+      authenticated: true,
+      user: {
+        email: 'hi@bye.com',
+        roles: [],
+      },
+    },
+  }
+  const mockStore = configureStore([thunk])
+
+  it('should render the welcome page with the request access subheader', () => {
+    const store = mockStore(initialState)
+    const { getByText } = render(
+      <Provider store={store}>
+        <Welcome />
+      </Provider>
+    )
+
+    const header = getByText(
+      `Your request for access is currently being reviewed by an OFA Admin. We'll send you an email when it's been approved.`
+    )
+    expect(header).toBeInTheDocument()
+  })
+
+  it("should render the welcome page with the user's current role", () => {
+    const store = mockStore({
+      auth: {
+        authenticated: true,
+        user: {
+          email: 'hi@bye.com',
+          roles: [{ id: 1, name: 'OFA Admin', permission: [] }],
+        },
+      },
+    })
+
+    const { getByText } = render(
+      <Provider store={store}>
+        <Welcome />
+      </Provider>
+    )
+
+    const header = getByText(
+      `You've been approved as a/an OFA Admin. You'll be able to do the following in TDP:`
+    )
+    expect(header).toBeInTheDocument()
+  })
+
+  it("should render the welcome page with the user's OFA admin role", () => {
+    const store = mockStore({
+      auth: {
+        authenticated: true,
+        user: {
+          email: 'hi@bye.com',
+          roles: [{ id: 1, name: 'OFA Admin', permission: [] }],
+        },
+      },
+    })
+
+    const { getByText } = render(
+      <Provider store={store}>
+        <Welcome />
+      </Provider>
+    )
+
+    const header = getByText(
+      `You've been approved as a/an OFA Admin. You'll be able to do the following in TDP:`
+    )
+    expect(header).toBeInTheDocument()
+  })
+
+  it("should render the welcome page with the user's Data Analyst role and permissions", () => {
+    const store = mockStore({
+      auth: {
+        authenticated: true,
+        user: {
+          email: 'hi@bye.com',
+          roles: [
+            {
+              id: 1,
+              name: 'OFA Admin',
+              permissions: [
+                {
+                  id: 8,
+                  codename: 'view_logentry',
+                  name: 'Can view log entry',
+                },
+                {
+                  id: 49,
+                  codename: 'add_datafile',
+                  name: 'Can add data file',
+                },
+                {
+                  id: 52,
+                  codename: 'view_datafile',
+                  name: 'Can view data file',
+                },
+                {
+                  id: 37,
+                  codename: 'add_user',
+                  name: 'Can add user',
+                },
+                {
+                  id: 38,
+                  codename: 'change_user',
+                  name: 'Can change user',
+                },
+                {
+                  id: 40,
+                  codename: 'view_user',
+                  name: 'Can view user',
+                },
+              ],
+            },
+          ],
+        },
+      },
+    })
+
+    const { getByText } = render(
+      <Provider store={store}>
+        <Welcome />
+      </Provider>
+    )
+
+    expect(getByText('Can view log entry')).toBeInTheDocument()
+    expect(getByText('Can change user')).toBeInTheDocument()
+    expect(getByText('Can add user')).toBeInTheDocument()
+    expect(getByText('Can view log entry')).toBeInTheDocument()
+    expect(getByText('Can add data file')).toBeInTheDocument()
+  })
+
+  it('should redirect the page when a user clicks the logout button', async () => {
+    const store = mockStore(initialState)
+    render(
+      <Provider store={store}>
+        <Welcome />
+      </Provider>
+    )
+
+    fireEvent.click(screen.getByText('Sign Out'))
+  })
+})
