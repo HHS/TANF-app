fc22e464a65bca70ec4c8575a5f68e1a09ddd3e9
Jorge Gonzalez <jorgegonzalez@users.noreply.github.com>
2021-09-07 09:40:37 -0400

    hotfix: fix trufflehog scans in upstream merges

--- scripts/trufflehog-check.sh
+++ scripts/trufflehog-check.sh
@@ -18,16 +18,8 @@ echo "trufflehog-check.sh: Scanning repo ..."
 # $1 - The first argument to this script, the current git branch name
 CIRCLE_BRANCH=$1
 
-# If the current PR branch includes `raft-tdp-main`, it a merge against HHS:main,
-# so we update the target branch to scan against.
-if [ "$CIRCLE_BRANCH" == "raft-tdp-main" ] ; then
-  TARGET=main
-else
-  TARGET=raft-tdp-main
-fi
-
 # Get the hash of the latest commit in the target branch.
-last_merge=$(git log --format=format:"%H" -n 1 $TARGET)
+last_merge=$(git log --format=format:"%H" -n 1 "$CIRCLE_BRANCH")
 
 # --since_commit - Look at all commits since the last merge into raft-tdp-main
 # --entropy=True - Entropy checks on large git diffs
