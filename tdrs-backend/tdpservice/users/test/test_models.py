eb9df18f96e26a25f1fcc8064c118952ffb7baec
Aaron Beavers <foamy125@gmail.com>
2021-09-24 12:15:53 -0400

    add test for regional user stt assignment exception

--- tdrs-backend/tdpservice/users/test/test_models.py
+++ tdrs-backend/tdpservice/users/test/test_models.py
@@ -1,8 +1,16 @@
 """Module for testing the user model."""
 import pytest
+from django.core.exceptions import ValidationError
 
 
 @pytest.mark.django_db
 def test_user_string_representation(user):
     """Test user string representation."""
     assert str(user) == user.username
+
+@pytest.mark.django_db
+def test_regional_user_cannot_have_stt(regional_user, stt):
+    try:
+        regional_user.stt = stt
+    except ValidationError:
+        assert True
