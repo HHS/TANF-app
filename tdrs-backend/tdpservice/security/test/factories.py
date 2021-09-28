85b04efd3da89207632e7f1e4f19469f8d7d7412
John Willis <jtwillis92@gmail.com>
2021-09-28 15:50:59 -0400

    Code coverage improvements

---
+++ tdrs-backend/tdpservice/security/test/factories.py
@@ -0,0 +1,28 @@
+from faker import Factory as FakerFactory
+import factory
+
+from tdpservice.security.models import OwaspZapScan
+
+faker = FakerFactory.create()
+
+
+class OwaspZapScanFactory(factory.django.DjangoModelFactory):
+    """Factory for OwaspZapScan instances."""
+
+    class Meta:
+        """Meta options tying the factory to the model."""
+        model = OwaspZapScan
+
+    app_target = factory.Iterator(OwaspZapScan.AppTarget.choices)
+    html_report = factory.django.FileField(filename='owasp_report.html')
+    scanned_at = factory.LazyAttribute(lambda x: faker.date_time())
+
+    fail_count = factory.LazyAttribute(
+        lambda x: faker.random_int(min=0, max=256)
+    )
+    pass_count = factory.LazyAttribute(
+        lambda x: faker.random_int(min=0, max=256)
+    )
+    warn_count = factory.LazyAttribute(
+        lambda x: faker.random_int(min=0, max=256)
+    )
