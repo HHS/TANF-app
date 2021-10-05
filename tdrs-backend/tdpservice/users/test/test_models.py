37081a93917dbde72bf715a031a7eb31e318680c
Aaron Beavers <foamy125@gmail.com>
2021-10-05 13:29:04 -0400

    Update tests

--- tdrs-backend/tdpservice/users/test/test_models.py
+++ tdrs-backend/tdpservice/users/test/test_models.py
@@ -13,9 +13,21 @@ def test_regional_user_cannot_have_stt(regional_user, stt):
     """Test that an error will be thrown if an stt is set on a regional user."""
     with pytest.raises(ValidationError):
         regional_user.stt = stt
+        regional_user.save()
 
 @pytest.mark.django_db
-def test_data_analyst_cannot_have_region(user, region):
+def test_data_analyst_cannot_have_region(data_analyst, region):
     """Test that an error will be thrown if a region is set on a data analyst user."""
     with pytest.raises(ValidationError):
-        user.region = region
+        data_analyst.region = region
+        data_analyst.save()
+
+@pytest.mark.django_db
+def test_is_data_analyst_property(data_analyst, regional_user):
+    assert data_analyst.is_data_analyst is True
+    assert regional_user.is_data_analyst is False
+
+@pytest.mark.django_db
+def test_is_regional_user_property(data_analyst, regional_user):
+    assert data_analyst.is_regional_staff is False
+    assert regional_user.is_regional_staff is True
