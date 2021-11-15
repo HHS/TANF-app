1fe8f9c0045713e1c55ba7aee234588b7a785e41
Aaron Beavers <foamy125@gmail.com>
2021-11-15 13:22:04 -0500

    Formatting and trailing whitespace

--- scripts/docker-compose-check.sh
+++ scripts/docker-compose-check.sh
@@ -6,5 +6,5 @@ else
 echo The command docker-compose is not available installing...
 set -x
 curl -L https://github.com/docker/compose/releases/download/1.25.3/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose     
-fi   
+chmod +x /usr/local/bin/docker-compose
+fi
