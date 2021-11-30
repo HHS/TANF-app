8e0046e5908839f572db46994cbc481ddc99e1a5
abottoms-coder <abottoms@goraft.tech>
2021-11-30 14:34:07 -0500

    Reverting superfluous changes that don't fix bug

--- scripts/git-secrets-check.sh
+++ scripts/git-secrets-check.sh
@@ -5,7 +5,7 @@ if [ -d /tmp/git-secrets ]; then
     echo The command git-secrets is available
 else
     echo The command git-secrets is not available, cloning...
-    git clone https://github.com/awslabs/git-secrets.git /tmp/git-secrets
+    git clone git@github.com:awslabs/git-secrets.git /tmp/git-secrets/
 fi
 
 # ensure we have correct configs in place
@@ -17,7 +17,7 @@ grep -A10 secrets .git/config
 
 PATH="$PATH:/tmp/git-secrets"
 echo "git-secrets-check.sh: Scanning repo ..."
-bash /tmp/git-secrets/git-secrets --scan -r ../
+/tmp/git-secrets/git-secrets --scan -r ../
 
 # if there are issues, they will be listed then script will abort here
 if [[ $? -eq 0 ]]; then
