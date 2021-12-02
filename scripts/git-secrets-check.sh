bd2c80a7fa5c00a4c02ffdb02f46695bbdb395c7
abottoms-coder <abottoms@goraft.tech>
2021-12-02 09:46:36 -0500

    try 2

--- scripts/git-secrets-check.sh
+++ scripts/git-secrets-check.sh
@@ -6,6 +6,11 @@ if [ -d /tmp/git-secrets ]; then
 else
     echo The command git-secrets is not available, cloning...
     git clone git@github.com:awslabs/git-secrets.git /tmp/git-secrets/
+    if [ -f /tmp/git-secrets/git-secrets ]; then
+        sudo cp ./tmp/git-secrets/git-secrets /usr/sbin/
+    else
+        git clone https://github.com/awslabs/git-secrets.git /tmp/git-secrets
+    fi
 fi
 
 # ensure we have correct configs in place
@@ -16,10 +21,15 @@ grep -A10 secrets .git/config
 # grep will return non-zero code if nothing found, failing the build
 
 #PATH="$PATH:/tmp/git-secrets" What if we just copy into somewhere in the PATH?
-sudo cp ./tmp/git-secrets/git-secrets /usr/sbin 
+if [ -d /tmp/git-secrets ]; then
+	sudo cp ./tmp/git-secrets/git-secrets /usr/sbin 
+else
+	git clone https://github.com/awslabs/git-secrets.git /tmp/git-secrets
+fi
+
 echo "git-secrets-check.sh: Scanning repo ..."
 #/tmp/git-secrets/
-git-secrets --scan -r ../
+git secrets --scan -r ../
 
 # if there are issues, they will be listed then script will abort here
 if [[ $? -eq 0 ]]; then
