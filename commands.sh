80aff15d1052a8da499055846208be9cb7843f8b
Aaron Beavers <foamy125@gmail.com>
2022-01-06 14:20:36 -0500

    resolve conflict with comments

--- commands.sh
+++ commands.sh
@@ -1,25 +1,41 @@
-#!/bin/sh
+#!/usr/bin/sh
 # You will need to set this variable to match your local directory structure
 # TDRS_HOME="$HOME/Where/Ever/You/Want/TANF-app"
 
-TDRS_HOME="$HOME/GoRaft/470/TANF-app"
+TDRS_HOME="$HOME/devel/goraft.tech/tdrs/instances/TANF-app"
 
+# navigate terminal to tdrs home if $TDRS_HOME is set
 alias cd-tdrs='cd "$TDRS_HOME"'
+
+# navigate terminal to tdrs frontend if $TDRS_HOME is set
 alias cd-tdrs-frontend='cd "$TDRS_HOME/tdrs-frontend"'
+
+# navigate terminal to tdrs backend if $TDRS_HOME is set
 alias cd-tdrs-backend='cd "$TDRS_HOME/tdrs-backend"'
+
+# shortcut for applying all relavent compose files for local development
 alias tdrs-compose-local='docker-compose -f docker-compose.yml -f docker-compose.local.yml'
 
+# Stop tdrs backend entirely, then start it up again
 alias tdrs-backend-hard-restart='tdrs-backend-down && tdrs-backend-up'
-alias tdrs-backend-rebuild='tdrs-backend-compose down && tdrs-backend-compose up --build -d web'
+
+# rebuild backend docker containers
+alias tdrs-backend-rebuild='tdrs-backend-down && tdrs-backend-compose up --build -d web'
+
+# run flake8 in backend container
 alias tdrs-backend-lint='tdrs-backend-compose run --rm web bash -c "flake8 ."'
+
+# shortcut for running bash commands in backend container
 alias tdrs-backend-exec='tdrs-backend-compose exec web /bin/bash'
+
+# Open shell_plus for django backend inside of container
 alias tdrs-shell='tdrs-backend-compose run --rm web bash -c "python manage.py shell_plus"'
 
 # TDRS Frontend aliases
-# alias cd-tdrs-frontend='cd "$TDRS_HOME/tdrs-frontend"'
-alias tdrs-frontend-compose='docker-compose -f docker-compose.yml -f docker-compose.local.yml'
+
 alias tdrs-frontend-down='tdrs-frontend-compose down'
 alias tdrs-frontend-up='tdrs-frontend-compose up -d tdp-frontend'
+
 alias tdrs-frontend-restart='tdrs-frontend-compose restart tdp-frontend'
 alias tdrs-frontend-hard-restart='tdrs-frontend-down && tdrs-frontend-up'
 alias tdrs-frontend-rebuild='tdrs-frontend-compose down && tdrs-frontend-compose up --build -d tdp-frontend'
@@ -33,6 +49,7 @@ tdrs-start-backend() {
     cd tdrs-backend && tdrs-compose-local up
     cd ..
 }
+
 tdrs-start-frontend() {
     cd-tdrs
     cd tdrs-frontend && tdrs-compose-local up -d
