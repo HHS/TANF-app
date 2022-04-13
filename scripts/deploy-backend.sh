0aac714108317d34a66910add320190efbd6e33d
raftmsohani <msohani@goraft.tech>
2022-04-13 12:18:39 -0700

    1631: Removed the APT package

--- scripts/deploy-backend.sh
+++ scripts/deploy-backend.sh
@@ -32,9 +32,9 @@ update_backend()
     if [ "$1" = "rolling" ] ; then
         # Do a zero downtime deploy.  This requires enough memory for
         # two apps to exist in the org/space at one time.
-        cf push "$CGHOSTNAME_BACKEND" --no-route  -f manifest.buildpack.yml -b https://github.com/cloudfoundry/apt-buildpack  --strategy rolling || exit 1
+        cf push "$CGHOSTNAME_BACKEND" --no-route  -f manifest.buildpack.yml  --strategy rolling || exit 1
     else
-        cf push "$CGHOSTNAME_BACKEND" --no-route  -f manifest.buildpack.yml -b https://github.com/cloudfoundry/apt-buildpack
+        cf push "$CGHOSTNAME_BACKEND" --no-route  -f manifest.buildpack.yml
         # set up JWT key if needed
         if cf e "$CGHOSTNAME_BACKEND" | grep -q JWT_KEY ; then
             echo jwt cert already created
