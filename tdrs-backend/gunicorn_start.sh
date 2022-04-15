859f68e8d727df43ce0d007b33933570802cc606
abottoms-coder <abottoms@goraft.tech>
2022-04-15 16:48:39 -0400

    playing with log level as cloud.gov is misbehaving

--- tdrs-backend/gunicorn_start.sh
+++ tdrs-backend/gunicorn_start.sh
@@ -8,5 +8,5 @@ python manage.py populate_stts
 python manage.py collectstatic --noinput
 echo "Starting Gunicorn"
 
-exec gunicorn tdpservice.wsgi:application --bind 0.0.0.0:8080 --timeout 10 --workers 3 --log-file=- --log-level $LOGGING_LEVEL
+exec gunicorn tdpservice.wsgi:application --bind 0.0.0.0:8080 --timeout 10 --workers 3 --log-file=- --log-level debug #$LOGGING_LEVEL
  #--error-logfile gunicorn.log --access-logfile gunicorn.log --capture-output
