1889fdea08abbb508ade5102417a6faaa5612b37
Aaron Beavers <foamy125@gmail.com>
2022-05-25 14:15:54 -0400

    removed fun

--- sync.sh
+++ sync.sh
@@ -5,7 +5,6 @@ APP_GUID=$(cf curl /v3/apps/$(cf app $APP_NAME --guid)/processes | jq --raw-outp
 echo "APP NAME:$APP_NAME"
 echo "APP GUID:$APP_GUID"
 
-echo "Your ssh config file is currently, but don't worry, we're gonna fix that."
 cat <<EOF > ~/.ssh/config
 Host $APP_NAME
     HostName ssh.fr.cloud.gov
@@ -13,11 +12,6 @@ Host $APP_NAME
     Port 2222
 EOF
 
-echo "Your ssh config file is now different, your welcome:"
 cat ~/.ssh/config
 
-# echo "TRYIN TO HACK THE SYSTEM"
-# sudo pacman -S sshpass
-
 sshpass -p $(cf ssh-code) rsync -r ./tdrs-backend/tdpservice/ $APP_NAME:/app/tdpservice
-echo "I'm in."
