e3d241ce0379a041ed7e012bc624a157c6fdeecf
Aaron Beavers <foamy125@gmail.com>
2022-06-08 16:34:07 -0400

    remove excessive test

--- tdrs-backend/tdpservice/stts/test/test_models.py
+++ tdrs-backend/tdpservice/stts/test/test_models.py
@@ -20,16 +20,3 @@ def test_stt_string_representation(stts):
     """Test STT string representation."""
     first_stt = STT.objects.filter(type=STT.EntityType.STATE).first()
     assert str(first_stt) == first_stt.name
-
-
-@pytest.mark.django_db
-def test_each_stt_has_file_name(stts):
-    """Check that each of the stts have a set of filenames associated with them."""
-    DATA_DIR = Path(__file__).resolve().parent.parent / "management/commands/data"
-    stt_types = ["tribes", "territories", "tribes"]
-    for stt_type in stt_types:
-        with open(DATA_DIR / f"{stt_type}.csv") as csvfile:
-            reader = csv.DictReader(csvfile)
-            for row in reader:
-                stt = STT.objects.get(name=row["Name"])
-                assert json.loads(row["filenames"].replace('\'', '"')) == stt.filenames
