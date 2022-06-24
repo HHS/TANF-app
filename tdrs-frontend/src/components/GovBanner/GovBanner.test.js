e5cba92adc8164bf0e473d647986f24499ce6a7b
Aaron Beavers <foamy125@gmail.com>
2022-06-24 16:18:51 -0400

    lint

--- tdrs-frontend/src/components/GovBanner/GovBanner.test.js
+++ tdrs-frontend/src/components/GovBanner/GovBanner.test.js
@@ -13,57 +13,78 @@ describe('GovBanner', () => {
     const OLD_ENV = process.env
     beforeEach(() => {
       jest.resetModules()
-      process.env = {...OLD_ENV}
+      process.env = { ...OLD_ENV }
     })
     afterAll(() => {
       process.env = OLD_ENV
     })
-    test("displays correct behavior in development",() => {
+    test('displays correct behavior in development', () => {
       process.env.NODE_ENV = 'development'
 
       const wrapper = shallow(<GovBanner />)
-      expect(wrapper.text().includes("A Development Demo website of the United States government")).toBe(true)
+      expect(
+        wrapper
+          .text()
+          .includes(
+            'A Development Demo website of the United States government'
+          )
+      ).toBe(true)
     })
 
-    test("displays correct behavior in test",() => {
+    test('displays correct behavior in test', () => {
       process.env.NODE_ENV = 'test'
 
       const wrapper = shallow(<GovBanner />)
-      expect(wrapper.text().includes("A Test Demo website of the United States government")).toBe(true)
+      expect(
+        wrapper
+          .text()
+          .includes('A Test Demo website of the United States government')
+      ).toBe(true)
     })
 
-    test("displays correct behavior in dev",() => {
+    test('displays correct behavior in dev', () => {
       process.env.NODE_ENV = 'production'
       process.env.REACT_APP_CF_SPACE = 'dev'
 
       const wrapper = shallow(<GovBanner />)
-      expect(wrapper.text().includes("A Dev Demo website of the United States government")).toBe(true)
-
+      expect(
+        wrapper
+          .text()
+          .includes('A Dev Demo website of the United States government')
+      ).toBe(true)
     })
 
-    test("displays correct behavior in staging",() => {
+    test('displays correct behavior in staging', () => {
       process.env.NODE_ENV = 'production'
       process.env.REACT_APP_CF_SPACE = 'staging'
 
       const wrapper = shallow(<GovBanner />)
-      expect(wrapper.text().includes("A Staging Demo website of the United States government")).toBe(true)
-
+      expect(
+        wrapper
+          .text()
+          .includes('A Staging Demo website of the United States government')
+      ).toBe(true)
     })
-    test("displays correct behavior in prod",() => {
+    test('displays correct behavior in prod', () => {
       process.env.NODE_ENV = 'production'
       process.env.REACT_APP_CF_SPACE = 'prod'
       const wrapper = shallow(<GovBanner />)
-      expect(wrapper.text().includes("An Official website of the United States government")).toBe(true)
-
+      expect(
+        wrapper
+          .text()
+          .includes('An Official website of the United States government')
+      ).toBe(true)
     })
 
-    test("displays correct behavior in some other hypothetical yet to exist environment",() => {
-
+    test('displays correct behavior in some other hypothetical yet to exist environment', () => {
       process.env.NODE_ENV = 'space'
       process.env.REACT_APP_CF_SPACE = 'super-prod'
       const wrapper = shallow(<GovBanner />)
-      expect(wrapper.text().includes("A Demo website of the United States government")).toBe(true)
-
+      expect(
+        wrapper
+          .text()
+          .includes('A Demo website of the United States government')
+      ).toBe(true)
     })
   })
 })
