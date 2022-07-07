f849ceb6aa84c8659442afdc3e5b649b6661c3f0
Cameron Smart <smartcameron@gmail.com>
2022-07-07 11:39:05 -0700

    change required to *

--- tdrs-frontend/src/components/STTComboBox/STTComboBox.jsx
+++ tdrs-frontend/src/components/STTComboBox/STTComboBox.jsx
@@ -25,7 +25,7 @@ function STTComboBox({ selectStt, selectedStt, handleBlur, error }) {
   return (
     <ComboBox
       name="stt"
-      label="Associated State, Tribe, or Territory"
+      label="Associated State, Tribe, or Territory*"
       error={error ? 'A state, tribe, or territory is required' : undefined}
       handleSelect={selectStt}
       selected={selectedStt}
