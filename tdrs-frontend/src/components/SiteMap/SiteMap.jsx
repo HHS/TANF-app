50ea005dc085401cbbf13d0516c0143622c645bf
Aaron Beavers <foamy125@gmail.com>
2022-07-12 14:02:07 -0400

    update admin element for authorized users only

--- tdrs-frontend/src/components/SiteMap/SiteMap.jsx
+++ tdrs-frontend/src/components/SiteMap/SiteMap.jsx
@@ -1,6 +1,17 @@
 import React, { useRef, useState } from 'react'
+import { useSelector } from 'react-redux'
 
-const SiteMap = () => (
+
+const isMemberOfOne = (user, ...groupNames) =>
+      !!user?.roles?.some((role) => groupNames.includes(role.name))
+
+const userAccessRequestApproved = (user) => !!(!user?.['access_request']) && user.roles.length > 0
+
+const canViewAdmin = (user) =>
+      userAccessRequestApproved(user) &&
+      isMemberOfOne(user,'Developer', 'OFA System Admin', 'ACF OCIO')
+
+const SiteMap = ({ user }) => (
   <div className="margin-top-5">
     <SiteMap.Link text="Home" link="/home" />
     <SiteMap.Link
@@ -9,10 +20,13 @@ const SiteMap = () => (
     />
     <SiteMap.Link text="Data Files" link="/data-files" />
     <SiteMap.Link text="Profile" link="/profile" />
-    <SiteMap.Link
-      text="Admin"
-      link={`${process.env.REACT_APP_BACKEND_HOST}/admin/`}
-    />
+
+    {canViewAdmin(user) && (
+      <SiteMap.Link
+        text="Admin"
+        link={`${process.env.REACT_APP_BACKEND_HOST}/admin/`}
+      />
+    )}
   </div>
 )
 
