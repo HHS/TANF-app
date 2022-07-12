c24df751b950acf397ac85274e092d7a226a019c
Aaron Beavers <foamy125@gmail.com>
2022-07-12 14:03:04 -0400

    Update test to include initial state

--- tdrs-frontend/src/components/SiteMap/SiteMap.test.js
+++ tdrs-frontend/src/components/SiteMap/SiteMap.test.js
@@ -4,6 +4,10 @@ import { render } from '@testing-library/react'
 import SiteMap from './SiteMap'
 
 describe('SiteMap', () => {
+
+  const initialState = {
+    auth: { authenticated: false, inactive: false },
+  }
   const locations = ['Home', 'Privacy Policy', 'Data Files', 'Profile', 'Admin']
   for (let location of locations) {
     it(`renders the $(location) link`, () => {
