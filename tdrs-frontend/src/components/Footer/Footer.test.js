06d2cab8804a55ee6d641fe24f35fc41d225ba30
Aaron Beavers <foamy125@gmail.com>
2022-07-14 14:26:22 -0400

    Update tests

--- tdrs-frontend/src/components/Footer/Footer.test.js
+++ tdrs-frontend/src/components/Footer/Footer.test.js
@@ -1,16 +1,81 @@
 import React from 'react'
 import { render } from '@testing-library/react'
+import configureStore from 'redux-mock-store'
+import thunk from 'redux-thunk'
+import { Provider } from 'react-redux'
 
 import Footer from './Footer'
+import { permissions } from '../Header/developer_permissions'
 
 describe('Footer', () => {
+
+  const unauthenticatedInitialState = {
+    router: { location: { pathname: '/profile' } },
+    auth: {
+      authenticated: false,
+    },
+  }
+  const adminInitialState = {
+    router: { location: { pathname: '/profile' } },
+    auth: {
+      authenticated: true,
+      roles: [{ id: 1, name: 'Developer', permissions }],
+    },
+  }
+
+  const basicAuthenticatedInitialState = {
+    router: { location: { pathname: '/profile' } },
+    auth: {authenticated: true},
+  }
+  const mockStore = configureStore([thunk])
   it('renders the children & families logo', () => {
-    const { container } = render(<Footer />)
+    const store = mockStore(unauthenticatedInitialState)
+    const { container } = render(
+      <Provider store={store}>
+        <Footer />
+      </Provider>
+    )
     expect(container.querySelector('img')).toBeInTheDocument()
   })
 
-  it('renders the privacy policy link', () => {
-    const { getByText } = render(<Footer />)
+  it('renders the privacy policy link as an unauthenticated user', () => {
+    const store = mockStore(unauthenticatedInitialState)
+    const { getByText } = render(
+      <Provider store={store}>
+        <Footer />
+      </Provider>
+    )
     expect(getByText('Privacy policy')).toBeInTheDocument()
   })
+
+  it('renders the privacy policy link as an authenticated user', () => {
+    const store = mockStore(basicAuthenticatedInitialState)
+    const { getByText } = render(
+      <Provider store={store}>
+        <Footer />
+      </Provider>
+    )
+    expect(getByText('Privacy policy')).toBeInTheDocument()
+  })
+
+  it('renders the site map link if a user is authenticated', () => {
+    const store = mockStore(basicAuthenticatedInitialState)
+    const { getByText } = render(
+      <Provider store={store}>
+        <Footer />
+      </Provider>
+    )
+    expect(getByText('Site Map')).toBeInTheDocument()
+  })
+
+  it('does not render the sitemap link if a user is not authenticated', () => {
+    const store = mockStore(unauthenticatedInitialState)
+    const { queryByText} = render(
+      <Provider store={store}>
+        <Footer />
+      </Provider>
+    )
+    expect(queryByText('Site Map')).not.toBeInTheDocument()
+  })
+
 })
--- tdrs-frontend/src/components/SiteMap/SiteMap.test.js
+++ tdrs-frontend/src/components/SiteMap/SiteMap.test.js
@@ -5,14 +5,49 @@ import SiteMap from './SiteMap'
 
 describe('SiteMap', () => {
 
-  const initialState = {
-    auth: { authenticated: false, inactive: false },
-  }
-  const locations = ['Home', 'Privacy Policy', 'Data Files', 'Profile', 'Admin']
-  for (let location of locations) {
-    it(`renders the $(location) link`, () => {
-      const { getByText } = render(<SiteMap />)
-      expect(getByText(location)).toBeInTheDocument()
-    })
-  }
+  describe("When an non-admin user visits the sitemap",() =>{
+
+    const initialState = {
+      auth: { authenticated: false, inactive: false },
+      roles:[]
+    }
+    const locations = ['Home', 'Privacy Policy', 'Data Files', 'Profile']
+    for (let location of locations) {
+      it(`renders the $(location) link`, () => {
+        const { getByText } = render(<SiteMap />)
+        expect(getByText(location)).toBeInTheDocument()
+      })
+    }
+
+  })
+  describe("When an admin user visits the sitemap",() => {
+
+    const initialState = {
+      auth: { authenticated: false, inactive: false },
+      roles:[{name:"Developer"}]
+    }
+    const locations = ['Home', 'Privacy Policy', 'Data Files', 'Profile']
+    for (let location of locations) {
+      it(`renders the $(location) link`, () => {
+        const { getByText } = render(<SiteMap />)
+        expect(getByText(location)).toBeInTheDocument()
+      })
+    }
+  })
+
+  describe("When an unapproved user visits the sitemap",() => {
+
+    const initialState = {
+      auth: { authenticated: false, inactive: false },
+      roles:[{name:"Developer"}]
+    }
+    const locations = ['Home', 'Privacy Policy', 'Data Files', 'Profile']
+    for (let location of locations) {
+      it(`renders the $(location) link`, () => {
+        const { getByText } = render(<SiteMap />)
+        expect(getByText(location)).toBeInTheDocument()
+      })
+    }
+  })
+
 })
