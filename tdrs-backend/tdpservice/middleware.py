24acb2bd6264893f87f6886ee91e4b9c96c8540e
Aaron Beavers <foamy125@gmail.com>
2022-07-25 13:56:49 -0400

    Add Session Middleware Patch

--- tdrs-backend/tdpservice/middleware.py
+++ tdrs-backend/tdpservice/middleware.py
@@ -1,4 +1,6 @@
 """Generic middleware for use across the TDP platform."""
+from django.conf import settings
+from django.contrib.sessions.middleware import SessionMiddleware
 from django.utils.cache import add_never_cache_headers
 
 
@@ -12,4 +14,16 @@ class NoCacheMiddleware(object):
         """Add appropriate headers to the response before sending it out."""
         response = self.get_response(request)
         add_never_cache_headers(response)
+        return response
+
+
+class SessionMiddleware(SessionMiddleware):
+    def process_response(self, request, response):
+        response = super(SessionMiddleware, self).process_response(request, response)
+
+        # if settings.SESSION_COOKIE_NAME in response.cookies:
+        #     response.cookies[settings.SESSION_COOKIE_NAME]['samesite'] = 'None'
+        if settings.CSRF_COOKIE_NAME in response.cookies:
+            response.cookies[settings.CSRF_COOKIE_NAME]['samesite'] = 'None'
+            response.cookies[settings.CSRF_COOKIE_NAME]['Secure'] = True
         return response
