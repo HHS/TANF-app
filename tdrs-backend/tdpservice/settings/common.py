d432eb9a872e6b8888155d67beceef89eae53f46
Aaron Beavers <foamy125@gmail.com>
2022-07-25 13:52:07 -0400

    update SESSION_COOKIE_SECURE

--- tdrs-backend/tdpservice/settings/common.py
+++ tdrs-backend/tdpservice/settings/common.py
@@ -250,6 +250,7 @@ class Common(Configuration):
     # Sessions
     SESSION_ENGINE = "django.contrib.sessions.backends.signed_cookies"
     SESSION_COOKIE_HTTPONLY = True
+    SESSION_COOKIE_SECURE = True
     SESSION_TIMEOUT = 30
 
     # The CSRF token Cookie holds no security benefits when confined to HttpOnly.
