08633ff5e2f5ee33cd56cc3510ae226fa50b1fc2
Cameron Smart <smartcameron@gmail.com>
2022-07-26 08:41:19 -0700

    working on tests

--- tdrs-frontend/src/components/SiteMap/SiteMap.test.js
+++ tdrs-frontend/src/components/SiteMap/SiteMap.test.js
@@ -1,129 +1,71 @@
 import React from 'react'
 import thunk from 'redux-thunk'
-import { Provider } from 'react-redux'
-import { mount } from 'enzyme'
 import { render, screen } from '@testing-library/react'
 import configureStore from 'redux-mock-store'
-import SplashPage from '../SplashPage'
 import SiteMap from './SiteMap'
-import { permissions } from '../Header/developer_permissions'
-import PrivateRoute from '../PrivateRoute'
-import { MemoryRouter, Route, Routes } from 'react-router-dom'
+import { Provider } from 'react-redux'
 
 describe('SiteMap', () => {
-  it('When a non authenticated user visits the splash page do not show sitemap', () => {
-    const initialState = {
-      auth: { authenticated: false, inactive: false },
-      roles: [],
-    }
+
+  const initialState = {
+    router: { location: { pathname: '/home' } },
+    auth: {
+      user: {
+        email: 'test@test.com',
+        roles: [{ id: 1, name: 'Developer', permissions: [] }],
+        access_request: true,
+      },
+      authenticated: true,
+    },
+  }
+
+  it('When an authenticated Developer visits the sitemap', () => {
     const mockStore = configureStore([thunk])
-
-    const store = mockStore(initialState)
-
-    const wrapper = mount(
-      <Provider store={store}>
-        <SplashPage />
-      </Provider>
-    )
-
-    const footer = wrapper.find('footer')
-    expect(footer).not.toIncludeText('Site Map')
-  })
-
-  it('When an authenticated developer visits the splash page show sitemap', () => {
-    const initialState = {
-      router: { location: { pathname: '/home' } },
+    const store = mockStore({
+      ...initialState,
       auth: {
+        authenticated: true,
         user: {
-          email: 'test@test.com',
-          roles: [{ id: 1, name: 'Developer', permissions }],
+          email: 'hi@bye.com',
+          roles: [{ id: 1, name: 'Developer', permission: [] }],
           access_request: true,
         },
-        authenticated: true,
       },
-    }
+    })
 
-    const mockStore = configureStore([thunk])
-    const store = mockStore(initialState)
-
-    const wrapper = mount(
+    const { getByText } = render(
       <Provider store={store}>
-        <SplashPage />
-      </Provider>
-    )
-
-    const footer = wrapper.find('footer')
-    expect(footer.find('.usa-footer')).toIncludeText('Site Map')
-  })
-
-  it('When an authenticated admin visits the splash page show sitemap', () => {
-    const initialState = {
-      router: { location: { pathname: '/home' } },
-      auth: {
-        user: {
-          email: 'test@test.com',
-          roles: [{ id: 1, name: 'Admin', permissions }],
-          access_request: true,
-        },
-        authenticated: true,
-      },
-    }
-
-    const mockStore = configureStore([thunk])
-    const store = mockStore(initialState)
-
-    const wrapper = mount(
-      <Provider store={store}>
-        <MemoryRouter initialEntries={['/home', '/']}>
-          <Routes>
-            <Route
-              exact
-              path="/home"
-              element={
-                <PrivateRoute title="Welcome to TDP">
-                  <SplashPage />
-                </PrivateRoute>
-              }
-            />
-          </Routes>
-        </MemoryRouter>
-      </Provider>
-    )
-
-    const footer = wrapper.find('footer')
-    expect(footer.find('.usa-footer')).not.toIncludeText('Site Map')
-  })
-
-  it('When an non-authenticated user visits the sitemap', () => {
-    const initialState = {
-      auth: { authenticated: false, inactive: false },
-      roles: [],
-    }
-    const mockStore = configureStore([thunk])
-    const store = mockStore(initialState)
-    const wrapper = mount(
-      <Provider store={store}>
-        <SplashPage />
+        <SiteMap />
       </Provider>
     )
 
     const locations = ['Home', 'Privacy Policy', 'Data Files', 'Profile']
-    const footer = wrapper.find('footer')
     for (let location of locations) {
       expect(getByText(location)).toBeInTheDocument()
     }
   })
 
-  it('When an authenticated user visits the sitemap', () => {
-    const initialState = {
-      auth: { authenticated: false, inactive: false },
-      roles: [],
-    }
+  it('When an authenticated Admin visits the sitemap', () => {
     const mockStore = configureStore([thunk])
-    const store = mockStore(initialState)
-    const { getByText } = render(<SiteMap store={store} />)
+    const store = mockStore({
+      ...initialState,
+      auth: {
+        authenticated: true,
+        user: {
+          email: 'hi@bye.com',
+          roles: [{ id: 1, name: 'OFA System Admin', permission: [] }],
+          access_request: true,
+        },
+      },
+    })
 
-    const locations = ['Home', 'Privacy Policy', 'Data Files', 'Profile']
+    const { getByText } = render(
+      <Provider store={store}>
+        <SiteMap />
+      </Provider>
+    )
+
+    const locations = ['Home', 'Privacy Policy', 'Data Files', 'Profile', 'Admin']
     for (let location of locations) {
       expect(getByText(location)).toBeInTheDocument()
     }
