624e4c20846db46116dd6f19295ecce2c7bd59d1
Aaron Beavers <foamy125@gmail.com>
2022-07-29 13:58:10 -0400

    use wild cards

--- tdrs-backend/tdpservice/settings/common.py
+++ tdrs-backend/tdpservice/settings/common.py
@@ -330,11 +330,11 @@ class Common(Configuration):
 
     s3_src = "s3-us-gov-west-1.amazonaws.com"
 
-    CSP_DEFAULT_SRC = ("'self'", "*.cloud.gov", "https://tanfdata.acf.hhs.gov/", "https://api-tanfdata.acf.hhs.gov/")
+    CSP_DEFAULT_SRC = ("'self'", "*.cloud.gov",  "*.acf.hhs.gov")
     CSP_SCRIPT_SRC = ("'self'", s3_src)
     CSP_IMG_SRC = ("'self'", "data:", s3_src)
     CSP_FONT_SRC = ("'self'", s3_src)
-    CSP_CONNECT_SRC = ("'self'", "*.cloud.gov", "https://tanfdata.acf.hhs.gov/","https://api-tanfdata.acf.hhs.gov/")
+    CSP_CONNECT_SRC = ("'self'", "*.cloud.gov", "*.acf.hhs.gov")
     CSP_MANIFEST_SRC = ("'self'")
     CSP_OBJECT_SRC = ("'none'")
     CSP_FRAME_ANCESTORS = ("'none'")
