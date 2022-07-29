2f0bc798615e1cf4e6335ed45f919e3f32d73fdd
raftmsohani <msohani@goraft.tech>
2022-07-29 09:19:26 -0400

    1133 - Linting

--- tdrs-backend/tdpservice/scheduling/apps.py
+++ tdrs-backend/tdpservice/scheduling/apps.py
@@ -4,5 +4,6 @@ from django.apps import AppConfig
 
 
 class TasksConfig(AppConfig):
+    """Scheduling task config."""
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'tdpservice.scheduling'
