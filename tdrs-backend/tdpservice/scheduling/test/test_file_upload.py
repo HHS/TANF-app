59566502c3d39bc169cb9dc1a9a12b3d35e1f541
raftmsohani <msohani@goraft.tech>
2022-08-01 12:47:38 -0400

    1133 - Removed the test files

--- tdrs-backend/tdpservice/scheduling/test/test_file_upload.py
+++ tdrs-backend/tdpservice/scheduling/test/test_file_upload.py
@@ -1,17 +1,1 @@
 """Integration test(s) for clamav-rest operations."""
-
-import pytest
-
-from tdpservice.data_files.test.factories import DataFileFactory
-
-
-@pytest.fixture()
-def upload_file():
-    """Fixture for test upload file."""
-    return DataFileFactory()
-
-
-@pytest.mark.django_db
-def test_upload_server(upload_file):
-    """Test upload file."""
-    assert upload_file.original_filename == 'data_file.txt'
