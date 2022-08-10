d6b83ac051e813bda00e1f9ea861ca1718746537
Aaron Beavers <abeavers@goraft.tech>
2022-08-10 09:52:46 -0400

    Add env vars to frontend nginx server

--- scripts/deploy-frontend.sh
+++ scripts/deploy-frontend.sh
@@ -20,18 +20,27 @@ update_frontend()
         echo "REACT_APP_BACKEND_URL=https://api-tanfdata.acf.hhs.gov/v1" >> .env.production
         echo "REACT_APP_BACKEND_HOST=https://api-tanfdata.acf.hhs.gov" >> .env.production
         echo "REACT_APP_CF_SPACE=$CF_SPACE" >> .env.production
+
+        # For nginx to allow cross origin requests from the resources that it serves
+        # NOT a frontend var
+        cf set-env "ALLOWED_ORIGIN" 'https://tanfdata.acf.hhs.gov'
+        cf set-env "CONNECT_SRC" '*.acf.hhs.gov'
     else
         echo "REACT_APP_BACKEND_URL=https://$CGHOSTNAME_BACKEND.app.cloud.gov/v1" >> .env.production
         echo "REACT_APP_BACKEND_HOST=https://$CGHOSTNAME_BACKEND.app.cloud.gov" >> .env.production
+        echo "REACT_APP_CF_SPACE=$CF_SPACE" >> .env.production
+
+        cf set-env "ALLOWED_ORIGIN" "https://$CGHOSTNAME_FRONTEND.app.cloud.gov"
+        cf set-env "CONNECT_SRC" '*.app.cloud.gov'
     fi
     npm run build
     unlink .env.production
     mkdir deployment
 
     cp -r build deployment/public
-    cp  nginx/buildpack.nginx.conf deployment/nginx.conf
+    cp nginx/buildpack.nginx.conf deployment/nginx.conf
     cp nginx/locations.conf deployment/locations.conf
-    cp  nginx/mime.types deployment/mime.types
+    cp nginx/mime.types deployment/mime.types
 
     cp manifest.buildpack.yml deployment/manifest.buildpack.yml
     cd deployment || exit
