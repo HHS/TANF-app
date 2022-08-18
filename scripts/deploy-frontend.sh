cf272d875dfd2d4b3de0cd863cec03cb6c89ec24
Cameron Smart <smartcameron@gmail.com>
2022-08-18 13:07:38 -0700

    coppied deploy scripts from release v2

--- scripts/deploy-frontend.sh
+++ scripts/deploy-frontend.sh
@@ -6,41 +6,37 @@
 DEPLOY_STRATEGY=${1}
 
 #The application name  defined via the manifest yml for the frontend
-CGAPPNAME_FRONTEND=${2}
-CGAPPNAME_BACKEND=${3}
+CGHOSTNAME_FRONTEND=${2}
+CGHOSTNAME_BACKEND=${3}
 CF_SPACE=${4}
+ENVIRONMENT=${5}
 
 update_frontend()
 {
     echo DEPLOY_STRATEGY: "$DEPLOY_STRATEGY"
-    echo FRONTEND_HOST: "$CGAPPNAME_FRONTEND"
-    echo BACKEND_HOST: "$CGAPPNAME_BACKEND"
+    echo FRONTEND_HOST: "$CGHOSTNAME_FRONTEND"
+    echo BACKEND_HOST: "$CGHOSTNAME_BACKEND"
     cd tdrs-frontend || exit
+
     if [ "$CF_SPACE" = "tanf-prod" ]; then
         echo "REACT_APP_BACKEND_URL=https://api-tanfdata.acf.hhs.gov/v1" >> .env.production
         echo "REACT_APP_BACKEND_HOST=https://api-tanfdata.acf.hhs.gov" >> .env.production
-        echo "REACT_APP_CF_SPACE=$CF_SPACE" >> .env.production
-
-        # For nginx to allow cross origin requests from the resources that it serves
-        # NOT a frontend var
-        cf set-env "$CGAPPNAME_FRONTEND" ALLOWED_ORIGIN 'https://tanfdata.acf.hhs.gov'
-        cf set-env "$CGAPPNAME_FRONTEND" CONNECT_SRC '*.acf.hhs.gov'
+        echo "REACT_APP_LOGIN_GOV_URL=https://secure.login.gov/" >> .env.production
     else
-        echo "REACT_APP_BACKEND_URL=https://$CGAPPNAME_BACKEND.app.cloud.gov/v1" >> .env.production
-        echo "REACT_APP_BACKEND_HOST=https://$CGAPPNAME_BACKEND.app.cloud.gov" >> .env.production
-        echo "REACT_APP_CF_SPACE=$CF_SPACE" >> .env.production
-
-        cf set-env "$CGAPPNAME_FRONTEND" ALLOWED_ORIGIN "https://$CGAPPNAME_FRONTEND.app.cloud.gov"
-        cf set-env "$CGAPPNAME_FRONTEND" CONNECT_SRC '*.app.cloud.gov'
+        echo "REACT_APP_BACKEND_URL=https://$CGHOSTNAME_BACKEND.app.cloud.gov/v1" >> .env.production
+        echo "REACT_APP_BACKEND_HOST=https://$CGHOSTNAME_BACKEND.app.cloud.gov" >> .env.production
     fi
-    npm run build
+
+    echo "REACT_APP_CF_SPACE=$CF_SPACE" >> .env.production
+    
+    npm run build:$ENVIRONMENT
     unlink .env.production
     mkdir deployment
 
     cp -r build deployment/public
-    cp nginx/buildpack.nginx.conf deployment/nginx.conf
+    cp  nginx/buildpack.nginx.conf deployment/nginx.conf
     cp nginx/locations.conf deployment/locations.conf
-    cp nginx/mime.types deployment/mime.types
+    cp  nginx/mime.types deployment/mime.types
 
     cp manifest.buildpack.yml deployment/manifest.buildpack.yml
     cd deployment || exit
@@ -48,12 +44,17 @@ update_frontend()
     if [ "$1" = "rolling" ] ; then
         # Do a zero downtime deploy.  This requires enough memory for
         # two apps to exist in the org/space at one time.
-        cf push "$CGAPPNAME_FRONTEND" --no-route -f manifest.buildpack.yml --strategy rolling || exit 1
+        cf push "$CGHOSTNAME_FRONTEND" --no-route -f manifest.buildpack.yml --strategy rolling || exit 1
     else
-        cf push "$CGAPPNAME_FRONTEND" --no-route -f manifest.buildpack.yml
+        cf push "$CGHOSTNAME_FRONTEND" --no-route -f manifest.buildpack.yml
     fi
 
-    cf map-route "$CGAPPNAME_FRONTEND" app.cloud.gov --hostname "${CGAPPNAME_FRONTEND}"
+    if [ "$CF_SPACE" = "tanf-prod" ]; then
+        cf map-route "$CGHOSTNAME_FRONTEND" tanfdata.acf.hhs.gov
+    else
+        cf map-route "$CGHOSTNAME_FRONTEND" app.cloud.gov --hostname "${CGHOSTNAME_FRONTEND}"
+    fi
+    
     cd ../..
     rm -r tdrs-frontend/deployment
 }
