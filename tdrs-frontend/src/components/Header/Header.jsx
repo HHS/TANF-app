794bc08d1e7b547f79e2e3668dead7beca6068b1
Jan Timpe <jtimpe@goraft.tech>
2022-08-19 10:48:09 -0400

    comment for discussion

--- tdrs-frontend/src/components/Header/Header.jsx
+++ tdrs-frontend/src/components/Header/Header.jsx
@@ -123,6 +123,7 @@ function Header() {
               {authenticated && (
                 <>
                   <NavItem pathname={pathname} tabTitle="Home" href="/home" />
+                  {/* potential issue here with assigning a group to an account with a pending status. */}
                   {canViewDataFiles && (
                     <NavItem
                       pathname={pathname}
