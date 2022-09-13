1b622a4a4789479c78a2ab69855e67e25103fe1c
Cameron Smart <smartcameron@gmail.com>
2022-09-13 09:17:34 -0700

    added email host variables

--- tdrs-backend/tdpservice/settings/common.py
+++ tdrs-backend/tdpservice/settings/common.py
@@ -86,7 +86,8 @@ class Common(Configuration):
 
     # Email Server
     EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-
+    EMAIL_HOST = 'smtp.ees.hhs.gov'
+    
     # Whether to use localstack in place of a live AWS S3 environment
     USE_LOCALSTACK = bool(strtobool(os.getenv("USE_LOCALSTACK", "no")))
 
