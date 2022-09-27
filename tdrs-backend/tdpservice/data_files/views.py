d37f9fe3c6d22d57aa1f954870fe2b6f3063451d
Cameron Smart <smartcameron@gmail.com>
2022-09-27 09:13:53 -0700

    test with email server

--- tdrs-backend/tdpservice/data_files/views.py
+++ tdrs-backend/tdpservice/data_files/views.py
@@ -18,6 +18,8 @@ from tdpservice.data_files.serializers import DataFileSerializer
 from tdpservice.data_files.models import DataFile
 from tdpservice.users.permissions import DataFilePermissions
 from tdpservice.scheduling import sftp_task
+from tdpservice.email.email import mail
+from tdpservice.email.email_enums import EmailType
 
 
 class DataFileFilter(filters.FilterSet):
@@ -64,7 +66,18 @@ class DataFileViewSet(ModelViewSet):
                 username=settings.ACFTITAN_USERNAME,
                 port=22
             )
-
+        mail(
+            email_type=EmailType.DATA_SUBMITTED.value(),
+            recipient_email='csmart@goraft.tech',
+            email_context={
+                'subject': 'Foo Data Submitted',
+                'text_message': 'Your foo data has been submitted',
+                'first_name': 'foo-bar',
+                'stt_name': 'foo-stt',
+                'submission_date': 'foo-date',
+                'fiscal_year': 'foo-year',
+            }
+        )
         return response
 
     def filter_queryset(self, queryset):
