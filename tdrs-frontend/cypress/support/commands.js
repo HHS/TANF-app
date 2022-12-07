7dbfdafbe609bb8dd2191df128c92bbd5d6716d7
Jan Timpe <jtimpe@goraft.tech>
2022-12-07 12:23:29 -0500

    commit cypress support

---
+++ tdrs-frontend/cypress/support/commands.js
@@ -0,0 +1,49 @@
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add('login', (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('login', (username) =>
+  cy
+    .request({
+      method: 'POST',
+      url: `${Cypress.env('apiUrl')}/login/cypress`,
+      body: {
+        username,
+        token: Cypress.env('cypressToken'),
+      },
+    })
+    .then((response) => {
+      cy.window()
+        .its('store')
+        .invoke('dispatch', {
+          type: 'SET_AUTH',
+          payload: {
+            user: {
+              email: username,
+            },
+          },
+        })
+    })
+)
