a6f4e71b442aee80ba5c68411c089247d190fd8a
Jan Timpe <jtimpe@goraft.tech>
2022-12-07 12:22:37 -0500

    update test w/ shared steps

--- tdrs-frontend/cypress/e2e/accounts/accounts.feature
+++ tdrs-frontend/cypress/e2e/accounts/accounts.feature
@@ -1,5 +1,5 @@
 Feature: Users can create and manage their accounts
     Scenario: A user can log in and request access
         When I visit the home page
-        And I click the login button
-        Then I get logged in
+        And I log in as a new user
+        Then I see a Request Access form
--- tdrs-frontend/cypress/e2e/accounts/steps.js
+++ tdrs-frontend/cypress/e2e/accounts/steps.js
@@ -4,38 +4,13 @@ import { When, Then } from '@badeball/cypress-cucumber-preprocessor'
 When('I visit the home page', () => {
   cy.visit('/')
   cy.contains('Sign into TANF Data Portal', { timeout: 30000 })
-
-  cy.request({
-    method: 'POST',
-    url: `${Cypress.env('apiUrl')}/login/cypress`,
-    body: {
-      username: 'cypress@goraft.tech',
-      token: Cypress.env('cypressToken'),
-    },
-  }).then((response) => {
-    cy.window()
-      .its('store')
-      .invoke('dispatch', {
-        type: 'SET_AUTH',
-        payload: {
-          user: {
-            email: 'cypress@goraft.tech',
-            stt: {
-              id: 2,
-              type: 'state',
-              code: 'AK',
-              name: 'Alaska',
-            },
-          },
-        },
-      })
-  })
 })
 
-When('I click the login button', () => {
-  cy.contains('for grantees').click()
+When('I log in as a new user', () => {
+  cy.login('new-cypress@goraft.tech')
 })
 
-Then('I get logged in', () => {
+Then('I see a Request Access form', () => {
   cy.contains('Welcome').should('exist')
+  cy.get('button').contains('Request Access').should('exist')
 })
