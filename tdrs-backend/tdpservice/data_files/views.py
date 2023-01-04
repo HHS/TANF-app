1237113f0eb0750849bc42a95309cbc16dff43eb
Cameron Smart <smartcameron@gmail.com>
2023-01-04 10:18:59 -0800

    corrects setting vars

--- tdrs-backend/tdpservice/data_files/views.py
+++ tdrs-backend/tdpservice/data_files/views.py
@@ -44,8 +44,8 @@ class S3Client():
         self.client = boto3.client(
             's3',
             aws_access_key_id=settings.AWS_S3_DATAFILES_ACCESS_KEY,
-            aws_secret_access_key=settings.AWS_S3_DATAFILES_SECRET_ACCESS_KEY,
-            endpoint_url=settings.AWS_S3_DATAFILES_ENDPOINT_URL,
+            aws_secret_access_key=settings.AWS_S3_DATAFILES_SECRET_KEY,
+            endpoint_url=settings.AWS_S3_DATAFILES_ENDPOINT,
             region_name=settings.AWS_S3_DATAFILES_REGION_NAME
         )
 
