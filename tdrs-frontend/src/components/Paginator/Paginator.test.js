43a5e922d820a6882c601d54850b6f4c21aea260
Jan Timpe <jtimpe@goraft.tech>
2023-01-23 12:06:56 -0500

    outline tests

---
+++ tdrs-frontend/src/components/Paginator/Paginator.test.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { render, fireEvent, waitFor } from '@testing-library/react'
+
+describe('Paginator components', () => {
+  describe('PaginatorArrowButton', () => {})
+
+  describe('PaginatorPageNumberButton', () => {})
+
+  describe('Paginator', () => {})
+})
---
+++ tdrs-frontend/src/components/SubmissionHistory/SubmissionHistory.test.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { render, fireEvent, waitFor } from '@testing-library/react'
+
+describe('Submission history components', () => {
+  describe('SubmissionHistoryRow', () => {})
+
+  describe('SectionSubmissionHistory', () => {})
+
+  describe('SubmissionHistory', () => {})
+})
