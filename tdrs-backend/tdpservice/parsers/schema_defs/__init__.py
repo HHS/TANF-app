30132350c8f33d51ad1b4ff58bfbd176c4bf411b
Jan Timpe <jtimpe@goraft.tech>
2023-03-22 11:16:46 -0400

    remove old preparser impl

--- tdrs-backend/tdpservice/parsers/schema_defs/__init__.py
+++ tdrs-backend/tdpservice/parsers/schema_defs/__init__.py
@@ -1,7 +1,5 @@
 from .header import header
 from .trailer import trailer
-from . import universal
 
 header = header
 trailer = trailer
-universal = universal
--- tdrs-backend/tdpservice/scheduling/parser_task.py
+++ tdrs-backend/tdpservice/scheduling/parser_task.py
@@ -3,9 +3,10 @@ from __future__ import absolute_import
 from celery import shared_task
 import logging
 from tdpservice.data_files.models import DataFile
-from tdpservice.parsers.preparser import preparse
+from tdpservice.parsers.parse import parse_datafile
 
 logger = logging.getLogger(__name__)
+
 
 @shared_task
 def parse(data_file_id):
@@ -14,6 +15,5 @@ def parse(data_file_id):
     # to avoid those. I suppose good practice to not store/serializer large file contents in memory when stored in redis
     # for undetermined amount of time.
     data_file = DataFile.objects.get(id=data_file_id)
-    logger.debug("Sending file '%s' of type '%s' and section '%s' to preparser.",
-                 data_file.filename, "TANF", data_file.section)
-    preparse(data_file, "TANF", data_file.section)  # data_file.type_ssp_something, data_file.section)
+    errors = parse_datafile(data_file, "TANF", data_file.section)  # data_file.type_ssp_something, data_file.section)
+    logger.info(f"DataFile parsing finished with {len(errors)} errors {errors}")
