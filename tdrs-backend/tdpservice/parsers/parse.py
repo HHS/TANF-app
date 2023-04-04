223e5f115fa304cfd534790adae18a65c10793ec
jtimpe <111305129+jtimpe@users.noreply.github.com>
2023-04-04 13:50:46 -0400

    2408.2 - refactor preparser (#2462)
    * lift and shift search_indexes.parsers to parsers
    
    * lint
    
    * add helper methods to RowSchema and Field classes
    
    * add generic validator functions
    
    * create new header/trailer schema using RowSchema
    
    * create generic parse function for all schemas
    
    * add validator test, update docstrings
    
    * fix some tests
    
    * remove old preparser impl
    
    * fix parse section, blanks
    
    * rm comments
    
    * update parser task call
    
    * only save if record exists
    
    * reduce parse_datafile complexity
    
    * improve schema mismatch handling
    
    * update comments
    
    * move get_schema_options_call
    
    * rename validate_document to be more descriptive
    
    * add new startswith validator
    
    * use startsWith in header/trailer preparsing validation
    
    * internal method naming
    
    * move trailer parse/validation out of preparsing
    
    * preparse trailer, but skip trailer pre-validation
    
    * ignore trailer parsing errors in preparse
    
    * misspelling
    
    ---------
    
    Co-authored-by: Andrew <84722778+andrew-jameson@users.noreply.github.com>

---
+++ tdrs-backend/tdpservice/parsers/parse.py
@@ -0,0 +1,127 @@
+"""Convert raw uploaded Datafile into a parsed model, and accumulate/return any errors."""
+
+
+import os
+from . import schema_defs
+from . import validators
+
+
+def parse_datafile(datafile):
+    """Parse and validate Datafile header/trailer, then select appropriate schema and parse/validate all lines."""
+    rawfile = datafile.file
+    errors = {}
+
+    document_is_valid, document_error = validators.validate_single_header_trailer(rawfile)
+    if not document_is_valid:
+        errors['document'] = [document_error]
+        return errors
+
+    # get header line
+    rawfile.seek(0)
+    header_line = rawfile.readline().decode().strip()
+
+    # get trailer line
+    rawfile.seek(0)
+    rawfile.seek(-2, os.SEEK_END)
+    while rawfile.read(1) != b'\n':
+        rawfile.seek(-2, os.SEEK_CUR)
+
+    trailer_line = rawfile.readline().decode().strip('\n')
+
+    # parse header, trailer
+    header, header_is_valid, header_errors = schema_defs.header.parse_and_validate(header_line)
+    if not header_is_valid:
+        errors['header'] = header_errors
+        return errors
+
+    trailer, trailer_is_valid, trailer_errors = schema_defs.trailer.parse_and_validate(trailer_line)
+    if not trailer_is_valid:
+        errors['trailer'] = trailer_errors
+
+    # ensure file section matches upload section
+    section_names = {
+        'A': 'Active Case Data',
+        'C': 'Closed Case Data',
+        'G': 'Aggregate Data',
+        'S': 'Stratum Data',
+    }
+
+    section = header['type']
+
+    if datafile.section.find(section_names[section]) == -1:
+        errors['document'] = ['Section does not match.']
+        return errors
+
+    # parse line with appropriate schema
+    rawfile.seek(0)
+    line_number = 0
+    schema_options = get_schema_options(header['program_type'])
+
+    for rawline in rawfile:
+        line_number += 1
+        line = rawline.decode().strip('\r\n')
+
+        if line.startswith('HEADER') or line.startswith('TRAILER'):
+            continue
+
+        schema = get_schema(line, section, schema_options)
+        record_is_valid, record_errors = parse_datafile_line(line, schema)
+
+        if not record_is_valid:
+            errors[line_number] = record_errors
+
+    return errors
+
+
+def parse_datafile_line(line, schema):
+    """Parse and validate a datafile line and save any errors to the model."""
+    if schema:
+        record, record_is_valid, record_errors = schema.parse_and_validate(line)
+
+        if record:
+            record.errors = record_errors
+            record.save()
+
+        return record_is_valid, record_errors
+
+    return (False, ['No schema selected.'])
+
+
+def get_schema_options(program_type):
+    """Return the allowed schema options."""
+    match program_type:
+        case 'TAN':
+            # schema_options = schema_defs.tanf
+            return None
+        case 'SSP':
+            # schema_options = schema_defs.ssp
+            return None
+        # case tribal?
+    return None
+
+
+def get_schema(line, section, schema_options):
+    """Return the appropriate schema for the line."""
+    if section == 'A' and line.startswith('T1'):
+        return None
+        # return schema_options.t1
+    elif section == 'A' and line.startswith('T2'):
+        return None
+        # return schema_options.t2
+    elif section == 'A' and line.startswith('T3'):
+        return None
+        # return schema_options.t3
+    elif section == 'C' and line.startswith('T4'):
+        return None
+        # return schema_options.t4
+    elif section == 'C' and line.startswith('T5'):
+        return None
+        # return schema_options.t5
+    elif section == 'G' and line.startswith('T6'):
+        return None
+        # return schema_options.t6
+    elif section == 'S' and line.startswith('T7'):
+        return None
+        # return schema_options.t7
+    else:
+        return None
---
+++ tdrs-backend/tdpservice/parsers/schema_defs/__init__.py
@@ -0,0 +1,5 @@
+from .header import header
+from .trailer import trailer
+
+header = header
+trailer = trailer
