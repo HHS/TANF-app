781438e4218be54923d6daaf8d7f243fcb9cbee8
Jan Timpe <jtimpe@goraft.tech>
2023-04-10 10:45:41 -0400

    rm comments

--- tdrs-backend/tdpservice/search_indexes/models/__init__.py
+++ tdrs-backend/tdpservice/search_indexes/models/__init__.py
@@ -1,12 +1,3 @@
-# from .tanf import TANF_T1, TANF_T2, TANF_T3, TANF_T4, TANF_T5, TANF_T6, TANF_T7
 from . import tanf
 
 tanf = tanf
-
-# TANF_T1 = TANF_T1
-# TANF_T2 = TANF_T2
-# TANF_T3 = TANF_T3
-# TANF_T4 = TANF_T4
-# TANF_T5 = TANF_T5
-# TANF_T6 = TANF_T6
-# TANF_T7 = TANF_T7
