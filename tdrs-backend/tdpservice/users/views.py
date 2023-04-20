5bfaa5b5720aadea06809b21b7f1b13876850732
elipe17 <elipe@teamraft.com>
2023-04-20 13:33:50 -0600

    - Fixed remaining tests

--- tdrs-backend/tdpservice/users/views.py
+++ tdrs-backend/tdpservice/users/views.py
@@ -45,9 +45,15 @@ class UserViewSet(
 
     def get_queryset(self):
         """Return the queryset based on user's group status."""
-        queryset = self.queryset
-        if not self.request.user.groups.filter(name="OFA System Admin").exists():
+        queryset = None
+        # This is not a great way to make sure regional users can access what they need. This should be revisited.
+        is_admin = self.request.user.groups.filter(name="OFA System Admin").exists()
+        is_regional = self.request.user.groups.filter(name="OFA Regional Staff").exists()
+        if is_admin or is_regional:
+            queryset = self.queryset
+        else:
             queryset = self.queryset.filter(id=self.request.user.id)
+        print(queryset)
         return queryset
 
     def retrieve(self, request, pk=None):
