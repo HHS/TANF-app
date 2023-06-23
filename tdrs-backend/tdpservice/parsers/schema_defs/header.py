f7dcaf659ef58a99b51e8364bb67112d697a4709
Eric Lipe <125676261+elipe17@users.noreply.github.com>
2023-06-23 14:24:42 -0600

    Item Number Mismatch (#2578)
    * - Updated schemas and models to reflect correct item numbers of fields
    
    * - Revert migration
    
    * - Updated header/trailer item numbers
    
    * - Fixed item numbers off by one errors
    
    ---------
    
    Co-authored-by: Andrew <84722778+andrew-jameson@users.noreply.github.com>

--- tdrs-backend/tdpservice/parsers/schema_defs/header.py
+++ tdrs-backend/tdpservice/parsers/schema_defs/header.py
@@ -16,34 +16,34 @@ header = RowSchema(
     ],
     postparsing_validators=[],
     fields=[
-        Field(item=1, name='title', type='string', startIndex=0, endIndex=6, required=True, validators=[
+        Field(item="2", name='title', type='string', startIndex=0, endIndex=6, required=True, validators=[
             validators.matches('HEADER'),
         ]),
-        Field(item=2, name='year', type='number', startIndex=6, endIndex=10, required=True, validators=[
+        Field(item="4", name='year', type='number', startIndex=6, endIndex=10, required=True, validators=[
             validators.between(2000, 2099)
         ]),
-        Field(item=3, name='quarter', type='string', startIndex=10, endIndex=11, required=True, validators=[
+        Field(item="5", name='quarter', type='string', startIndex=10, endIndex=11, required=True, validators=[
             validators.oneOf(['1', '2', '3', '4'])
         ]),
-        Field(item=4, name='type', type='string', startIndex=11, endIndex=12, required=True, validators=[
+        Field(item="6", name='type', type='string', startIndex=11, endIndex=12, required=True, validators=[
             validators.oneOf(['A', 'C', 'G', 'S'])
         ]),
-        Field(item=5, name='state_fips', type='string', startIndex=12, endIndex=14, required=True, validators=[
+        Field(item="1", name='state_fips', type='string', startIndex=12, endIndex=14, required=True, validators=[
             validators.between(0, 99)
         ]),
-        Field(item=6, name='tribe_code', type='string', startIndex=14, endIndex=17, required=False, validators=[
+        Field(item="3", name='tribe_code', type='string', startIndex=14, endIndex=17, required=False, validators=[
             validators.between(0, 999)
         ]),
-        Field(item=7, name='program_type', type='string', startIndex=17, endIndex=20, required=True, validators=[
+        Field(item="7", name='program_type', type='string', startIndex=17, endIndex=20, required=True, validators=[
             validators.oneOf(['TAN', 'SSP'])
         ]),
-        Field(item=8, name='edit', type='string', startIndex=20, endIndex=21, required=True, validators=[
+        Field(item="8", name='edit', type='string', startIndex=20, endIndex=21, required=True, validators=[
             validators.oneOf(['1', '2'])
         ]),
-        Field(item=9, name='encryption', type='string', startIndex=21, endIndex=22, required=False, validators=[
+        Field(item="9", name='encryption', type='string', startIndex=21, endIndex=22, required=False, validators=[
             validators.matches('E')
         ]),
-        Field(item=10, name='update', type='string', startIndex=22, endIndex=23, required=True, validators=[
+        Field(item="10", name='update', type='string', startIndex=22, endIndex=23, required=True, validators=[
             validators.oneOf(['N', 'D', 'U'])
         ]),
     ],
--- tdrs-backend/tdpservice/parsers/test/factories.py
+++ tdrs-backend/tdpservice/parsers/test/factories.py
@@ -13,7 +13,7 @@ class ParserErrorFactory(factory.django.DjangoModelFactory):
     file = factory.SubFactory(DataFileFactory)
     row_number = 1
     column_number = 1
-    item_number = 1
+    item_number = "1"
     field_name = "test field name"
     case_number = '1'
     rpt_month_year = 202001
