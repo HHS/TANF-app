f9850692b12c45b994d91c940a87008306433b8e
mo sohani <msohani@goraft.tech>
2023-07-18 12:52:28 -0400

    removed one print

--- tdrs-backend/temp_debug_script.py
+++ tdrs-backend/temp_debug_script.py
@@ -5,7 +5,6 @@ from tdpservice.users.models import User
 logger = logging.getLogger()
 
 
-print('__________ see if print works __________')
 print('_____ start debugging _____')
 print('_____ settings.DATABASES:: ' + str(settings.DATABASES))
 user_ = User.objects.all()
