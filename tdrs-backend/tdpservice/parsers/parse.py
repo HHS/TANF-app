8dae7da4e0235b5c64d42ab1e1a6147aa9fca21a
andrew-jameson <ajameson@teamraft.com>
2023-08-09 15:10:23 -0400

    Adopting latest manager, removing old error style.

--- tdrs-backend/tdpservice/parsers/parse.py
+++ tdrs-backend/tdpservice/parsers/parse.py
@@ -160,22 +160,6 @@ def parse_datafile_lines(datafile, program_type, section, is_encrypted):
             prev_sum = header_count + trailer_count
             continue
 
-        schema = util.get_schema(line, section, program_type)
-        if schema is None:  #TODO: this pushes complexity >10, should be handled by the schema manager
-            err_obj = util.generate_parser_error(
-                datafile=datafile,
-                line_number=line_number,
-                schema=None,
-                error_category=ParserErrorCategoryChoices.PRE_CHECK,
-                error_message="Unknown Record_Type was found.",
-                record=None,
-                field="Record_Type",
-            )
-            preparse_error = {line_number: [err_obj]}
-            errors[line_number] = [err_obj]
-            unsaved_parser_errors.update(preparse_error)
-            continue
-
         schema_manager = get_schema_manager(line, section, schema_manager_options)
 
         schema_manager.update_encrypted_fields(is_encrypted)
@@ -234,6 +218,16 @@ def manager_parse_line(line, schema_manager, generate_error):
     if schema_manager.schemas:
         records = schema_manager.parse_and_validate(line, generate_error)
         return records
+    else:
+        return [(None, False, [
+            generate_error(
+                schema=None,
+                error_category=ParserErrorCategoryChoices.PRE_CHECK,
+                error_message="Unknown Record_Type was found.",
+                record=None,
+                field="Record_Type",
+            )
+        ])]
 
 
 def get_schema_manager_options(program_type):
