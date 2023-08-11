d8b7672182aad4f8ee73d6a1bdfb5d76c960e359
andrew-jameson <ajameson@teamraft.com>
2023-08-11 11:38:54 -0400

    syntax mistake with isinstance

--- tdrs-backend/tdpservice/parsers/test/test_parse.py
+++ tdrs-backend/tdpservice/parsers/test/test_parse.py
@@ -770,7 +770,7 @@ def test_get_schema_options(dfs):
     # get section str
     # get ref section
 
-    assert parse.get_schema_manager('T1xx', 'A', 'TAN').isinstance(util.SchemaManager)
+    assert isinstance(parse.get_schema_manager('T1xx', 'A', 'TAN'), util.SchemaManager)
 
 
 @pytest.fixture
