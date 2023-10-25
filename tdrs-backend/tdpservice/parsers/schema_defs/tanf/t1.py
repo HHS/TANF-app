0269728d64d83bf3bc67b22892729c06e418fe44
Mo Sohani <msohani@goraft.tech>
2023-10-25 14:32:22 -0400

    added friendly names to T1 and T2

--- tdrs-backend/tdpservice/parsers/schema_defs/tanf/t1.py
+++ tdrs-backend/tdpservice/parsers/schema_defs/tanf/t1.py
@@ -78,186 +78,187 @@ t1 = SchemaManager(schemas=[
                                           "TRANSP_AMOUNT", "TRANSITION_SERVICES_AMOUNT", "OTHER_AMOUNT"), 0)
             ],
             fields=[
-                  Field(item="0", name='RecordType', type='string', startIndex=0, endIndex=2,
+                # add fields inclding friendly_name (based on name), type, start_index, end_index, required, validators
+                  Field(item="0", name='RecordType', friendly_name='record type', type='string', startIndex=0, endIndex=2,
                         required=True, validators=[]),
-                  Field(item="4", name='RPT_MONTH_YEAR', type='number', startIndex=2, endIndex=8,
+                  Field(item="4", name='RPT_MONTH_YEAR', friendly_name='reporting month year', type='number', startIndex=2, endIndex=8,
                         required=True, validators=[
                               validators.dateYearIsLargerThan(1998),
                               validators.dateMonthIsValid(),
                         ]),
-                  Field(item="6", name='CASE_NUMBER', type='string', startIndex=8, endIndex=19,
+                  Field(item="6", name='CASE_NUMBER', friendly_name='case number', type='string', startIndex=8, endIndex=19,
                         required=True, validators=[validators.isAlphaNumeric()]),
-                  Field(item="2", name='COUNTY_FIPS_CODE', type='string', startIndex=19, endIndex=22,
+                  Field(item="2", name='COUNTY_FIPS_CODE', friendly_name='county fips code', type='string', startIndex=19, endIndex=22,
                         required=True, validators=[
                               validators.isNumber(),
                         ]),
-                  Field(item="5", name='STRATUM', type='string', startIndex=22, endIndex=24,
+                  Field(item="5", name='STRATUM', friendly_name='stratum', type='string', startIndex=22, endIndex=24,
                         required=True, validators=[
                               validators.isInStringRange(0, 99),
                         ]),
-                  Field(item="7", name='ZIP_CODE', type='string', startIndex=24, endIndex=29,
+                  Field(item="7", name='ZIP_CODE', friendly_name='zip code', type='string', startIndex=24, endIndex=29,
                         required=True, validators=[
                               validators.isNumber(),
                         ]),
-                  Field(item="8", name='FUNDING_STREAM', type='number', startIndex=29, endIndex=30,
+                  Field(item="8", name='FUNDING_STREAM', friendly_name='funding stream', type='number', startIndex=29, endIndex=30,
                         required=True, validators=[
                               validators.isInLimits(1, 3),
                         ]),
-                  Field(item="9", name='DISPOSITION', type='number', startIndex=30, endIndex=31,
+                  Field(item="9", name='DISPOSITION', friendly_name='disposition', type='number', startIndex=30, endIndex=31,
                         required=True, validators=[
                               validators.oneOf([1, 2]),
                         ]),
-                  Field(item="10", name='NEW_APPLICANT', type='number', startIndex=31, endIndex=32,
+                  Field(item="10", name='NEW_APPLICANT', friendly_name='new applicant', type='number', startIndex=31, endIndex=32,
                         required=True, validators=[
                               validators.oneOf([1, 2]),
                         ]),
-                  Field(item="11", name='NBR_FAMILY_MEMBERS', type='number', startIndex=32, endIndex=34,
+                  Field(item="11", name='NBR_FAMILY_MEMBERS', friendly_name='number of family members', type='number', startIndex=32, endIndex=34,
                         required=True, validators=[
                               validators.isLargerThan(0),
                         ]),
-                  Field(item="12", name='FAMILY_TYPE', type='number', startIndex=34, endIndex=35,
+                  Field(item="12", name='FAMILY_TYPE', friendly_name='family type', type='number', startIndex=34, endIndex=35,
                         required=True, validators=[
                               validators.isInLimits(1, 3),
                         ]),
-                  Field(item="13", name='RECEIVES_SUB_HOUSING', type='number', startIndex=35, endIndex=36,
+                  Field(item="13", name='RECEIVES_SUB_HOUSING', friendly_name='receives subsidized housing', type='number', startIndex=35, endIndex=36,
                         required=True, validators=[
                               validators.isInLimits(1, 3),
                         ]),
-                  Field(item="14", name='RECEIVES_MED_ASSISTANCE', type='number', startIndex=36, endIndex=37,
+                  Field(item="14", name='RECEIVES_MED_ASSISTANCE', friendly_name='receives medical assisstance', type='number', startIndex=36, endIndex=37,
                         required=True, validators=[
                               validators.isInLimits(1, 2),
                         ]),
-                  Field(item="15", name='RECEIVES_FOOD_STAMPS', type='number', startIndex=37, endIndex=38,
+                  Field(item="15", name='RECEIVES_FOOD_STAMPS', friendly_name='receives food stamps', type='number', startIndex=37, endIndex=38,
                         required=True, validators=[
                               validators.isInLimits(0, 2),
                         ]),
-                  Field(item="16", name='AMT_FOOD_STAMP_ASSISTANCE', type='number', startIndex=38, endIndex=42,
+                  Field(item="16", name='AMT_FOOD_STAMP_ASSISTANCE', friendly_name='amount of food stamp assisstance', type='number', startIndex=38, endIndex=42,
                         required=True, validators=[
                               validators.isLargerThanOrEqualTo(0),
                         ]),
-                  Field(item="17", name='RECEIVES_SUB_CC', type='number', startIndex=42, endIndex=43,
+                  Field(item="17", name='RECEIVES_SUB_CC', friendly_name='receives subsidized child care', type='number', startIndex=42, endIndex=43,
                         required=True, validators=[
                               validators.isInLimits(0, 3),
                         ]),
-                  Field(item="18", name='AMT_SUB_CC', type='number', startIndex=43, endIndex=47,
+                  Field(item="18", name='AMT_SUB_CC', friendly_name='amount of subsidized child care', type='number', startIndex=43, endIndex=47,
                         required=True, validators=[
                               validators.isLargerThanOrEqualTo(0),
                         ]),
-                  Field(item="19", name='CHILD_SUPPORT_AMT', type='number', startIndex=47, endIndex=51,
+                  Field(item="19", name='CHILD_SUPPORT_AMT', friendly_name='child support amount', type='number', startIndex=47, endIndex=51,
                         required=True, validators=[
                               validators.isLargerThanOrEqualTo(0),
                         ]),
-                  Field(item="20", name='FAMILY_CASH_RESOURCES', type='number', startIndex=51, endIndex=55,
+                  Field(item="20", name='FAMILY_CASH_RESOURCES', friendly_name='family cash resources', type='number', startIndex=51, endIndex=55,
                         required=True, validators=[
                               validators.isLargerThanOrEqualTo(0),
                         ]),
-                  Field(item="21A", name='CASH_AMOUNT', type='number', startIndex=55, endIndex=59,
+                  Field(item="21A", name='CASH_AMOUNT', friendly_name='cash amount', type='number', startIndex=55, endIndex=59,
                         required=True, validators=[
                               validators.isLargerThanOrEqualTo(0),
                         ]),
-                  Field(item="21B", name='NBR_MONTHS', type='number', startIndex=59, endIndex=62,
+                  Field(item="21B", name='NBR_MONTHS', friendly_name='number of months', type='number', startIndex=59, endIndex=62,
                         required=True, validators=[
                               validators.isLargerThanOrEqualTo(0),
                         ]),
-                  Field(item="22A", name='CC_AMOUNT', type='number', startIndex=62, endIndex=66,
+                  Field(item="22A", name='CC_AMOUNT', friendly_name='child care amount', type='number', startIndex=62, endIndex=66,
                         required=True, validators=[
                               validators.isLargerThanOrEqualTo(0),
                         ]),
-                  Field(item="22B", name='CHILDREN_COVERED', type='number', startIndex=66, endIndex=68,
+                  Field(item="22B", name='CHILDREN_COVERED', friendly_name='children covered', type='number', startIndex=66, endIndex=68,
                         required=True, validators=[
                               validators.isLargerThanOrEqualTo(0),
                         ]),
-                  Field(item="22C", name='CC_NBR_MONTHS', type='number', startIndex=68, endIndex=71,
+                  Field(item="22C", name='CC_NBR_MONTHS', friendly_name='child care number of months', type='number', startIndex=68, endIndex=71,
                         required=True, validators=[
                               validators.isLargerThanOrEqualTo(0),
                         ]),
-                  Field(item="23A", name='TRANSP_AMOUNT', type='number', startIndex=71, endIndex=75,
+                  Field(item="23A", name='TRANSP_AMOUNT', friendly_name='transportation amount', type='number', startIndex=71, endIndex=75,
                         required=True, validators=[
                               validators.isLargerThanOrEqualTo(0),
                         ]),
-                  Field(item="23B", name='TRANSP_NBR_MONTHS', type='number', startIndex=75, endIndex=78,
+                  Field(item="23B", name='TRANSP_NBR_MONTHS', friendly_name='transportation number of months', type='number', startIndex=75, endIndex=78,
                         required=True, validators=[
                               validators.isLargerThanOrEqualTo(0),
                         ]),
-                  Field(item="24A", name='TRANSITION_SERVICES_AMOUNT', type='number', startIndex=78, endIndex=82,
+                  Field(item="24A", name='TRANSITION_SERVICES_AMOUNT', friendly_name='transition service amount', type='number', startIndex=78, endIndex=82,
                         required=True, validators=[
                               validators.isLargerThanOrEqualTo(0),
                         ]),
-                  Field(item="24B", name='TRANSITION_NBR_MONTHS', type='number', startIndex=82, endIndex=85,
+                  Field(item="24B", name='TRANSITION_NBR_MONTHS', friendly_name='transition number of months', type='number', startIndex=82, endIndex=85,
                         required=True, validators=[
                               validators.isLargerThanOrEqualTo(0),
                         ]),
-                  Field(item="25A", name='OTHER_AMOUNT', type='number', startIndex=85, endIndex=89,
+                  Field(item="25A", name='OTHER_AMOUNT', friendly_name='other amount', type='number', startIndex=85, endIndex=89,
                         required=True, validators=[
                               validators.isLargerThanOrEqualTo(0),
                         ]),
-                  Field(item="25B", name='OTHER_NBR_MONTHS', type='number', startIndex=89, endIndex=92,
+                  Field(item="25B", name='OTHER_NBR_MONTHS', friendly_name='other number of months', type='number', startIndex=89, endIndex=92,
                         required=True, validators=[
                               validators.isLargerThanOrEqualTo(0),
                         ]),
-                  Field(item="26AI", name='SANC_REDUCTION_AMT', type='number', startIndex=92, endIndex=96,
+                  Field(item="26AI", name='SANC_REDUCTION_AMT', friendly_name='sanction reduction amount', type='number', startIndex=92, endIndex=96,
                         required=True, validators=[
                               validators.isLargerThanOrEqualTo(0),
                         ]),
-                  Field(item="26AII", name='WORK_REQ_SANCTION', type='number', startIndex=96, endIndex=97,
+                  Field(item="26AII", name='WORK_REQ_SANCTION', friendly_name='work requirement sanction', type='number', startIndex=96, endIndex=97,
                         required=True, validators=[
                               validators.oneOf([1, 2]),
                         ]),
-                  Field(item="26AIII", name='FAMILY_SANC_ADULT', type='number', startIndex=97, endIndex=98,
+                  Field(item="26AIII", name='FAMILY_SANC_ADULT', friendly_name='family sanction adult', type='number', startIndex=97, endIndex=98,
                         required=True, validators=[
                               validators.oneOf([0, 1, 2]),
                         ]),
-                  Field(item="26AIV", name='SANC_TEEN_PARENT', type='number', startIndex=98, endIndex=99,
+                  Field(item="26AIV", name='SANC_TEEN_PARENT', friendly_name='sanction for teen parent not attending school', type='number', startIndex=98, endIndex=99,
                         required=True, validators=[
                               validators.oneOf([1, 2]),
                         ]),
-                  Field(item="26AV", name='NON_COOPERATION_CSE', type='number', startIndex=99, endIndex=100,
+                  Field(item="26AV", name='NON_COOPERATION_CSE', friendly_name='non cooperation case', type='number', startIndex=99, endIndex=100,
                         required=True, validators=[
                               validators.oneOf([1, 2]),
                         ]),
-                  Field(item="26AVI", name='FAILURE_TO_COMPLY', type='number', startIndex=100, endIndex=101,
+                  Field(item="26AVI", name='FAILURE_TO_COMPLY', friendly_name='failure to comply', type='number', startIndex=100, endIndex=101,
                         required=True, validators=[
                               validators.oneOf([1, 2]),
                         ]),
-                  Field(item="26AVII", name='OTHER_SANCTION', type='number', startIndex=101, endIndex=102,
+                  Field(item="26AVII", name='OTHER_SANCTION', friendly_name='other sanction', type='number', startIndex=101, endIndex=102,
                         required=True, validators=[
                               validators.oneOf([1, 2]),
                         ]),
-                  Field(item="26B", name='RECOUPMENT_PRIOR_OVRPMT', type='number', startIndex=102, endIndex=106,
+                  Field(item="26B", name='RECOUPMENT_PRIOR_OVRPMT', friendly_name='recoupment of prior overpayment', type='number', startIndex=102, endIndex=106,
                         required=True, validators=[
                               validators.isLargerThanOrEqualTo(0),
                         ]),
-                  Field(item="26CI", name='OTHER_TOTAL_REDUCTIONS', type='number', startIndex=106, endIndex=110,
+                  Field(item="26CI", name='OTHER_TOTAL_REDUCTIONS', friendly_name='other total reductions', type='number', startIndex=106, endIndex=110,
                         required=True, validators=[
                               validators.isLargerThanOrEqualTo(0),
                         ]),
-                  Field(item="26CII", name='FAMILY_CAP', type='number', startIndex=110, endIndex=111,
+                  Field(item="26CII", name='FAMILY_CAP', friendly_name='family cap', failure_type='number', startIndex=110, endIndex=111,
                         required=True, validators=[
                               validators.oneOf([1, 2]),
                         ]),
-                  Field(item="26CIII", name='REDUCTIONS_ON_RECEIPTS', type='number', startIndex=111, endIndex=112,
+                  Field(item="26CIII", name='REDUCTIONS_ON_RECEIPTS', friendly_name='reductions on receipts', type='number', startIndex=111, endIndex=112,
                         required=True, validators=[
                               validators.oneOf([1, 2]),
                         ]),
-                  Field(item="26CIV", name='OTHER_NON_SANCTION', type='number', startIndex=112, endIndex=113,
+                  Field(item="26CIV", name='OTHER_NON_SANCTION', friendly_name='other non sanction', type='number', startIndex=112, endIndex=113,
                         required=True, validators=[
                               validators.oneOf([1, 2]),
                         ]),
-                  Field(item="27", name='WAIVER_EVAL_CONTROL_GRPS', type='string', startIndex=113, endIndex=114,
+                  Field(item="27", name='WAIVER_EVAL_CONTROL_GRPS', friendly_name='waiver evaluation experimental and control groups', type='string', startIndex=113, endIndex=114,
                         required=True, validators=[
                               validators.or_validators(validators.matches('9'), validators.isEmpty()),
                               validators.isAlphaNumeric(),
                         ]),
-                  Field(item="28", name='FAMILY_EXEMPT_TIME_LIMITS', type='number', startIndex=114, endIndex=116,
+                  Field(item="28", name='FAMILY_EXEMPT_TIME_LIMITS', friendly_name='is the TANF family exempt from the federal time-limit provisions', type='number', startIndex=114, endIndex=116,
                         required=True, validators=[
                               validators.oneOf([1, 2, 3, 4,
                                                 6, 7, 8, 9])
                         ]),
-                  Field(item="29", name='FAMILY_NEW_CHILD', type='number', startIndex=116, endIndex=117,
+                  Field(item="29", name='FAMILY_NEW_CHILD', friendly_name='family new child', type='number', startIndex=116, endIndex=117,
                         required=True, validators=[
                               validators.oneOf([1, 2]),
                         ]),
-                  Field(item="-1", name='BLANK', type='string', startIndex=117, endIndex=156, required=False,
+                  Field(item="-1", name='BLANK', friendly_name='blank', type='string', startIndex=117, endIndex=156, required=False,
                         validators=[]),
             ],
       )]
