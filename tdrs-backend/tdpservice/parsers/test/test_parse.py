a42ff2b6fcfe409fe9b5ae88635d542757a53e56
Eric Lipe <elipe@teamraft.com>
2024-02-29 13:46:43 -0700

    - Fixed tests

--- tdrs-backend/tdpservice/parsers/test/test_parse.py
+++ tdrs-backend/tdpservice/parsers/test/test_parse.py
@@ -1085,7 +1085,8 @@ def test_parse_ssp_section2_file(ssp_section2_file, dfs):
     dfs.status = dfs.get_status()
     dfs.case_aggregates = aggregates.case_aggregates_by_month(
         dfs.datafile, dfs.status)
-    assert dfs.case_aggregates == {'rejected': 0,
+    print(dfs.case_aggregates)
+    assert dfs.case_aggregates == {'rejected': 8007,
                                    'months': [
                                        {'accepted_without_errors': 0,
                                            'accepted_with_errors': 78, 'month': 'Oct'},
@@ -1095,7 +1096,7 @@ def test_parse_ssp_section2_file(ssp_section2_file, dfs):
                                            'accepted_with_errors': 75, 'month': 'Dec'}
                                    ]}
 
-    assert dfs.get_status() == DataFileSummary.Status.ACCEPTED_WITH_ERRORS
+    assert dfs.get_status() == DataFileSummary.Status.PARTIALLY_ACCEPTED
 
     m4_objs = SSP_M4.objects.all().order_by('id')
     m5_objs = SSP_M5.objects.all().order_by('AMOUNT_EARNED_INCOME')
