9e14abc56e03da42861452a827a87653f64ccfef
Jan Timpe <jtimpe@goraft.tech>
2024-04-03 08:12:34 -0400

    add SESSION_EXPIRE_AT_BROWSER_CLOSE

--- tdrs-backend/tdpservice/settings/common.py
+++ tdrs-backend/tdpservice/settings/common.py
@@ -271,6 +271,7 @@ class Common(Configuration):
     SESSION_ENGINE = "django.contrib.sessions.backends.signed_cookies"
     SESSION_COOKIE_HTTPONLY = True
     SESSION_TIMEOUT = 30
+    SESSION_EXPIRE_AT_BROWSER_CLOSE = True
     # The CSRF token Cookie holds no security benefits when confined to HttpOnly.
     # Setting this to false to allow the frontend to include it in the header
     # of API POST calls to prevent false negative authorization errors.
