389f9e96ea268e90b1e5e58af2a7e2b90c219e68
Eric Lipe <elipe@teamraft.com>
2024-04-12 15:39:46 -0600

    - more logging

--- scripts/cf-runtask.sh
+++ scripts/cf-runtask.sh
@@ -4,18 +4,20 @@
 set -uxo pipefail
 
 PROJECT_SLUG=$1
-CMD_ARGS=$2
-TARGET=$3
-BUILD_NO=$4
-ZAP_BACKEND_PASS_COUNT=$5
-ZAP_BACKEND_WARN_COUNT=$6
-ZAP_BACKEND_FAIL_COUNT=$7
-ZAP_FRONTEND_PASS_COUNT=$8
-ZAP_FRONTEND_WARN_COUNT=$9
-ZAP_FRONTEND_FAIL_COUNT=$10
+TARGET=$2
+BUILD_NO=$3
+ZAP_BACKEND_PASS_COUNT=$4
+ZAP_BACKEND_WARN_COUNT=$5
+ZAP_BACKEND_FAIL_COUNT=$6
+ZAP_FRONTEND_PASS_COUNT=$7
+ZAP_FRONTEND_WARN_COUNT=$8
+ZAP_FRONTEND_FAIL_COUNT=$9
 
 CMD="python manage.py process_owasp_scan $CIRCLE_BUILD_NUM --backend-pass-count ${ZAP_BACKEND_PASS_COUNT:-0} --backend-warn-count ${ZAP_BACKEND_WARN_COUNT:-0} --backend-fail-count ${ZAP_BACKEND_FAIL_COUNT:-0} --frontend-pass-count ${ZAP_FRONTEND_PASS_COUNT:-0} --frontend-warn-count ${ZAP_FRONTEND_WARN_COUNT:-0} --frontend-fail-count ${ZAP_FRONTEND_FAIL_COUNT:-0} --project-slug $PROJECT_SLUG"
-echo $CMD
+echo .
+echo Executing command: $CMD
+echo .
 echo START!
-cf run-task tdp-backend-$TARGET --wait --command "$CMD" --name nightly-owasp-scan
+APP=tdp-backend-$TARGET
+cf run-task "$APP" --wait --command "$CMD" --name "nightly-owasp-scan"
 echo DONE!
