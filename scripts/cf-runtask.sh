239ec16fc6b0e7a2dc373dff023df5fe949170b6
Eric Lipe <elipe@teamraft.com>
2024-04-12 15:24:31 -0600

    - all params

--- scripts/cf-runtask.sh
+++ scripts/cf-runtask.sh
@@ -6,8 +6,15 @@ set -uxo pipefail
 PROJECT_SLUG=$1
 CMD_ARGS=$2
 TARGET=$3
+BUILD_NO=$4
+ZAP_BACKEND_PASS_COUNT=$5
+ZAP_BACKEND_WARN_COUNT=$6
+ZAP_BACKEND_FAIL_COUNT=$7
+ZAP_FRONTEND_PASS_COUNT=$8
+ZAP_FRONTEND_WARN_COUNT=$9
+ZAP_FRONTEND_FAIL_COUNT=$10
 
-CMD="python manage.py process_owasp_scan ${CMD_ARGS[*]}"
+CMD="python manage.py process_owasp_scan $CIRCLE_BUILD_NUM --backend-pass-count ${ZAP_BACKEND_PASS_COUNT:-0} --backend-warn-count ${ZAP_BACKEND_WARN_COUNT:-0} --backend-fail-count ${ZAP_BACKEND_FAIL_COUNT:-0} --frontend-pass-count ${ZAP_FRONTEND_PASS_COUNT:-0} --frontend-warn-count ${ZAP_FRONTEND_WARN_COUNT:-0} --frontend-fail-count ${ZAP_FRONTEND_FAIL_COUNT:-0} --project-slug $PROJECT_SLUG"
 echo $CMD
 echo START!
 cf run-task tdp-backend-$TARGET --wait --command "$CMD" --name nightly-owasp-scan
