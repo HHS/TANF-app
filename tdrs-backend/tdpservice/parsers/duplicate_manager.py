83b9d0b255f88d9290cb892f5d9b910e79873dc3
Eric Lipe <elipe@teamraft.com>
2024-04-18 07:40:40 -0600

    -  Fix lint - Add docstrings

--- tdrs-backend/tdpservice/parsers/duplicate_manager.py
+++ tdrs-backend/tdpservice/parsers/duplicate_manager.py
@@ -1,11 +1,14 @@
+"""Class definition for record duplicate class and helper classes."""
 from .models import ParserErrorCategoryChoices
 
 class ErrorPrecedence:
+    """Data structure to manage error precedence."""
+
     def __init__(self):
         self.max_precedence = None
 
     def has_precedence(self, error_level):
-        """Returns tuple of bools: (has_precidence, is_new_max_precedence)."""
+        """Return tuple of bools: (has_precidence, is_new_max_precedence)."""
         if self.max_precedence is None:
             self.max_precedence = error_level
             return (True, True)
@@ -19,6 +22,8 @@ class ErrorPrecedence:
 
 
 class CaseHashtainer:
+    """Container class to manage hashed values for records of the same CASE_NUMBER and RPT_MONTH_YEAR."""
+
     def __init__(self, my_hash, CASE_NUMBER, RPT_MONTH_YEAR, manager_error_dict, generate_error):
         self.my_hash = my_hash
         self.CASE_NUMBER = CASE_NUMBER
@@ -31,10 +36,11 @@ class CaseHashtainer:
         self.error_precedence = ErrorPrecedence()
 
     def __generate_error(self, err_msg, is_new_max_precedence):
+        """Add an error to the managers error dictionary."""
         if err_msg is not None:
             error = self.generate_error(
                         error_category=ParserErrorCategoryChoices.CASE_CONSISTENCY,
-                        schema=None, ## TODO: Do we need the right schema? Can this be None to avoid so much state?
+                        schema=None,  # TODO: Do we need the right schema? Can this be None to avoid so much state?
                         record=None,
                         field=None,
                         error_message=err_msg,
@@ -45,6 +51,7 @@ class CaseHashtainer:
                 self.manager_error_dict[self.my_hash].append(error)
 
     def add_case_member(self, record, line, line_number):
+        """Add case member and generate errors if needed."""
         self.record_ids.add(record.id)
         line_hash = hash(line)
         partial_hash = None
@@ -52,7 +59,8 @@ class CaseHashtainer:
         if record.RecordType == "T1":
             partial_hash = hash(record.RecordType + str(record.RPT_MONTH_YEAR) + record.CASE_NUMBER)
         else:
-            partial_hash = hash(record.RecordType + str(record.RPT_MONTH_YEAR) + record.CASE_NUMBER + str(record.FAMILY_AFFILIATION) + record.DATE_OF_BIRTH + record.SSN)
+            partial_hash = hash(record.RecordType + str(record.RPT_MONTH_YEAR) + record.CASE_NUMBER +
+                                str(record.FAMILY_AFFILIATION) + record.DATE_OF_BIRTH + record.SSN)
 
         is_exact_dup = False
         err_msg = None
@@ -63,22 +71,23 @@ class CaseHashtainer:
             has_precedence, is_new_max_precedence = self.error_precedence.has_precedence(error_level)
             existing_record_id, existing_record_line_number = self.record_hashes[line_hash]
             err_msg = (f"Duplicate record detected for record id {record.id} with record type {record.RecordType} at "
-                               f"line {line_number}. Record is a duplicate of the record at line number "
-                               f"{existing_record_line_number}, with record id {existing_record_id}")
+                       f"line {line_number}. Record is a duplicate of the record at line number "
+                       f"{existing_record_line_number}, with record id {existing_record_id}")
             is_exact_dup = True
 
         skip_partial = False
-        if  record.RecordType != "T1":
+        if record.RecordType != "T1":
             skip_partial = record.FAMILY_AFFILIATION == 3 or record.FAMILY_AFFILIATION == 5
         if not skip_partial and not is_exact_dup and partial_hash in self.partial_hashes:
             has_precedence, is_new_max_precedence = self.error_precedence.has_precedence(error_level)
-            err_msg = (f"Partial duplicate record detected for record id {record.id} with record type {record.RecordType} at "
-                               f"line {line_number}. Record is a partial duplicate of the record at line number "
-                               f"{self.partial_hashes[partial_hash][1]}, with record id {self.partial_hashes[partial_hash][0]}")
-        
+            err_msg = (f"Partial duplicate record detected for record id {record.id} with record type "
+                       f"{record.RecordType} at line {line_number}. Record is a partial duplicate of the "
+                       f"record at line number {self.partial_hashes[partial_hash][1]}, with record id "
+                       f"{self.partial_hashes[partial_hash][0]}")
+
         if not has_precedence:
             err_msg = None
-        
+
         self.__generate_error(err_msg, is_new_max_precedence)
         if line_hash not in self.record_hashes:
             self.record_hashes[line_hash] = (record.id, line_number)
@@ -87,6 +96,7 @@ class CaseHashtainer:
 
 
 class RecordDuplicateManager:
+    """Manages all CaseHashtainers and their errors."""
 
     def __init__(self, generate_error):
         self.hashtainers = dict()
@@ -94,14 +104,16 @@ class RecordDuplicateManager:
         self.generated_errors = dict()
 
     def add_record(self, record, line, line_number):
+        """Add record to existing CaseHashtainer or create new one."""
         hash_val = hash(str(record.RPT_MONTH_YEAR) + record.CASE_NUMBER)
         if hash_val not in self.hashtainers:
-            hashtainer = CaseHashtainer(hash_val, record.CASE_NUMBER, str(record.RPT_MONTH_YEAR), 
+            hashtainer = CaseHashtainer(hash_val, record.CASE_NUMBER, str(record.RPT_MONTH_YEAR),
                                         self.generated_errors, self.generate_error)
             self.hashtainers[hash_val] = hashtainer
         self.hashtainers[hash_val].add_case_member(record, line, line_number)
 
     def get_generated_errors(self):
+        """Return all errors from all CaseHashtainers."""
         generated_errors = list()
         for errors in self.generated_errors.values():
             generated_errors.extend(errors)
--- tdrs-backend/tdpservice/parsers/parse.py
+++ tdrs-backend/tdpservice/parsers/parse.py
@@ -344,9 +344,9 @@ def parse_datafile_lines(datafile, dfs, program_type, section, is_encrypted, cas
         rollback_records(unsaved_records, datafile)
         bulk_create_errors(unsaved_parser_errors, num_errors, flush=True)
         return errors
-    
+
     validate_case_consistency(case_consistency_validator)
-    
+
     # Add any generated cat4 errors to our error data structure & clear our caches errors list
     duplicate_errors = duplicate_manager.get_generated_errors()
     num_errors += len(duplicate_errors) + len(case_consistency_validator.get_generated_errors())
