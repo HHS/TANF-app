ac2fbc559090b9ec7561348e287b809eaf5dfc3e
Eric Lipe <elipe@teamraft.com>
2024-05-20 09:25:25 -0400

    - fix lint

--- tdrs-backend/tdpservice/parsers/util.py
+++ tdrs-backend/tdpservice/parsers/util.py
@@ -240,10 +240,10 @@ class SortedRecords:
         return False
 
 def generate_t1_t4_hashes(line, record):
-    """Returns the hashes for duplicate and partial duplicate detection for T1 & T4 records."""
+    """Return hashes for duplicate and partial duplicate detection for T1 & T4 records."""
     return hash(line), hash(record.RecordType + str(record.RPT_MONTH_YEAR) + record.CASE_NUMBER)
 
 def generate_t2_t3_t5_hashes(line, record):
-    """Returns the hashes for duplicate and partial duplicate detection for T2 & T3 & T5 records."""
+    """Return hashes for duplicate and partial duplicate detection for T2 & T3 & T5 records."""
     return hash(line), hash(record.RecordType + str(record.RPT_MONTH_YEAR) + record.CASE_NUMBER +
                             str(record.FAMILY_AFFILIATION) + record.DATE_OF_BIRTH + record.SSN)
