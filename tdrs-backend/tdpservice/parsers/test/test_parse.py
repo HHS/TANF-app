8d5def9fd7740fb38def89bb4f10b5ba3dbd0c31
Eric Lipe <elipe@teamraft.com>
2024-05-20 08:35:02 -0400

    - updated test

--- tdrs-backend/tdpservice/parsers/test/test_parse.py
+++ tdrs-backend/tdpservice/parsers/test/test_parse.py
@@ -398,7 +398,7 @@ def test_parse_bad_trailer_file2(bad_trailer_file_2, dfs):
     errors = parse.parse_datafile(bad_trailer_file_2, dfs)
 
     parser_errors = ParserError.objects.filter(file=bad_trailer_file_2)
-    assert parser_errors.count() == 6
+    assert parser_errors.count() == 7
 
     trailer_errors = list(parser_errors.filter(row_number=3).order_by('id'))
 
