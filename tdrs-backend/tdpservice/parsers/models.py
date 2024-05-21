89d62987338247af48bee6ce634d3b461a13c67f
Eric Lipe <elipe@teamraft.com>
2024-05-21 09:16:45 -0400

    - lint

--- tdrs-backend/tdpservice/parsers/models.py
+++ tdrs-backend/tdpservice/parsers/models.py
@@ -112,7 +112,7 @@ class DataFileSummary(models.Model):
         if errors is None:
             return DataFileSummary.Status.PENDING
         elif precheck_errors.count() > 0 or case_consistency_errors.count() > 0 or \
-            self.total_number_of_records_created == 0:
+                self.total_number_of_records_created == 0:
             return DataFileSummary.Status.REJECTED
         elif errors.count() == 0:
             return DataFileSummary.Status.ACCEPTED
