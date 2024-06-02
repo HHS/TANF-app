24bceb9d5e6794a7e6b76f3bcb20790f2f8739f5
Jan Timpe <jtimpe@goraft.tech>
2024-06-02 17:06:10 -0400

    lint

--- tdrs-backend/tdpservice/email/test/test_upcoming_deadline_email.py
+++ tdrs-backend/tdpservice/email/test/test_upcoming_deadline_email.py
@@ -1,10 +1,9 @@
 """Test function for sending upcoming data deadline reminders."""
 import pytest
+from datetime import datetime
 from django.core import mail
 from tdpservice.email.tasks import send_data_submission_reminder
-from datetime import datetime
 from django.contrib.auth.models import Group
-
 from tdpservice.stts.models import STT
 from tdpservice.users.models import User
 from tdpservice.data_files.models import DataFile
@@ -20,7 +19,6 @@ from tdpservice.data_files.models import DataFile
 def test_upcoming_deadline_sends_no_sections_submitted(
         due_date, reporting_period, fiscal_quarter):
     """Test that the send_deactivation_warning_email function runs when no sections have been submitted."""
-
     stt = STT.objects.create(
         name='Arkansas',
         filenames={
@@ -53,7 +51,6 @@ def test_upcoming_deadline_sends_no_sections_submitted(
 def test_upcoming_deadline_sends_some_sections_submitted(
         due_date, reporting_period, fiscal_quarter):
     """Test that the send_deactivation_warning_email function runs when some sections have been submitted."""
-
     stt = STT.objects.create(
         name='Arkansas',
         filenames={
@@ -73,7 +70,7 @@ def test_upcoming_deadline_sends_some_sections_submitted(
     now = datetime.now()
     fiscal_year = now.year - 1 if fiscal_quarter == 'Q1' else now.year
 
-    data_file = DataFile.create_new_version({
+    _ = DataFile.create_new_version({
         "section": 'Active Case Data',
         "quarter": fiscal_quarter,
         "year": fiscal_year,
@@ -87,16 +84,6 @@ def test_upcoming_deadline_sends_some_sections_submitted(
     assert mail.outbox[0].subject == f"Upcoming submission deadline: {due_date}"
 
 
-"""Test function for sending upcoming data deadline reminders."""
-import pytest
-from django.core import mail
-from tdpservice.email.tasks import send_data_submission_reminder
-from django.contrib.auth.models import Group
-
-from tdpservice.stts.models import STT
-from tdpservice.users.models import User
-
-
 @pytest.mark.parametrize('due_date, reporting_period, fiscal_quarter', [
     ('February 14', 'Oct - Dec', 'Q1'),
     ('May 15th', 'Jan - Mar', 'Q2'),
@@ -107,7 +94,6 @@ from tdpservice.users.models import User
 def test_upcoming_deadline_no_send_when_all_sections_complete(
         due_date, reporting_period, fiscal_quarter):
     """Test that the send_deactivation_warning_email function does not run when all sections have been submitted."""
-
     stt = STT.objects.create(
         name='Arkansas',
         filenames={
@@ -127,7 +113,7 @@ def test_upcoming_deadline_no_send_when_all_sections_complete(
     now = datetime.now()
     fiscal_year = now.year - 1 if fiscal_quarter == 'Q1' else now.year
 
-    data_file = DataFile.create_new_version({
+    _ = DataFile.create_new_version({
         "section": 'Active Case Data',
         "quarter": fiscal_quarter,
         "year": fiscal_year,
@@ -135,7 +121,7 @@ def test_upcoming_deadline_no_send_when_all_sections_complete(
         "user": data_analyst,
     })
 
-    data_file2 = DataFile.create_new_version({
+    _ = DataFile.create_new_version({
         "section": 'Closed Case Data',
         "quarter": fiscal_quarter,
         "year": fiscal_year,
