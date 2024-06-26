14ca954cbd24a77da30036f47d9384d5f6884be6
Jan Timpe <jtimpe@goraft.tech>
2024-06-26 13:56:17 -0400

    don't show jurisdiction type for acf users

--- tdrs-frontend/src/components/Home/Home.jsx
+++ tdrs-frontend/src/components/Home/Home.jsx
@@ -177,51 +177,55 @@ function Home() {
             handleChange={handleChange}
             handleBlur={handleBlur}
           />
-          <div className="usa-form-group">
-            <fieldset className="usa-fieldset">
-              <legend className="usa-label text-bold">Jurisdiction Type</legend>
-              <div className="usa-radio">
-                <input
-                  className="usa-radio__input"
-                  id="state"
-                  type="radio"
-                  name="jurisdictionType"
-                  value="state"
-                  defaultChecked
-                  onChange={() => setJurisdictionType('state')}
-                />
-                <label className="usa-radio__label" htmlFor="state">
-                  State
-                </label>
-              </div>
-              <div className="usa-radio">
-                <input
-                  className="usa-radio__input"
-                  id="tribe"
-                  type="radio"
-                  name="jurisdictionType"
-                  value="tribe"
-                  onChange={() => setJurisdictionType('tribe')}
-                />
-                <label className="usa-radio__label" htmlFor="tribe">
-                  Tribe
-                </label>
-              </div>
-              <div className="usa-radio">
-                <input
-                  className="usa-radio__input"
-                  id="territory"
-                  type="radio"
-                  name="jurisdictionType"
-                  value="territory"
-                  onChange={() => setJurisdictionType('territory')}
-                />
-                <label className="usa-radio__label" htmlFor="territory">
-                  Territory
-                </label>
-              </div>
-            </fieldset>
-          </div>
+          {shouldShowSttComboBox && (
+            <div className="usa-form-group">
+              <fieldset className="usa-fieldset">
+                <legend className="usa-label text-bold">
+                  Jurisdiction Type
+                </legend>
+                <div className="usa-radio">
+                  <input
+                    className="usa-radio__input"
+                    id="state"
+                    type="radio"
+                    name="jurisdictionType"
+                    value="state"
+                    defaultChecked
+                    onChange={() => setJurisdictionType('state')}
+                  />
+                  <label className="usa-radio__label" htmlFor="state">
+                    State
+                  </label>
+                </div>
+                <div className="usa-radio">
+                  <input
+                    className="usa-radio__input"
+                    id="tribe"
+                    type="radio"
+                    name="jurisdictionType"
+                    value="tribe"
+                    onChange={() => setJurisdictionType('tribe')}
+                  />
+                  <label className="usa-radio__label" htmlFor="tribe">
+                    Tribe
+                  </label>
+                </div>
+                <div className="usa-radio">
+                  <input
+                    className="usa-radio__input"
+                    id="territory"
+                    type="radio"
+                    name="jurisdictionType"
+                    value="territory"
+                    onChange={() => setJurisdictionType('territory')}
+                  />
+                  <label className="usa-radio__label" htmlFor="territory">
+                    Territory
+                  </label>
+                </div>
+              </fieldset>
+            </div>
+          )}
           {jurisdictionType && shouldShowSttComboBox && (
             <div
               className={`usa-form-group ${
