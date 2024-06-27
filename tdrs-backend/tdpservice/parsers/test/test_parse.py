89c0f2da05baad42c382442485be6efcf41126d3
Jan Timpe <jtimpe@goraft.tech>
2024-06-27 09:15:57 -0400

    fix lint

--- tdrs-backend/tdpservice/parsers/test/test_parse.py
+++ tdrs-backend/tdpservice/parsers/test/test_parse.py
@@ -1646,7 +1646,8 @@ def test_parse_m2_cat2_invalid_37_38_39_file(m2_cat2_invalid_37_38_39_file, dfs)
 
     assert parser_errors.count() == 3
 
-    error_msgs = {"M2 Item 37 (education level): 00 is not in range [1, 16]. or M2 Item 37 (education level): 00 is not in range [98, 99].",
+    error_msgs = {"M2 Item 37 (education level): 00 is not in range [1, 16]. "
+                  "or M2 Item 37 (education level): 00 is not in range [98, 99].",
                   "M2 Item 38 (citizenship status): 0 is not in [1, 2, 3, 9].",
                   "M2 Item 39 (cooperation with child support): 0 is not in [1, 2, 9]."}
     for e in parser_errors:
@@ -1669,7 +1670,8 @@ def test_parse_m3_cat2_invalid_68_69_file(m3_cat2_invalid_68_69_file, dfs):
 
     assert parser_errors.count() == 4
 
-    error_msgs = {"M3 Item 68 (education level): 00 is not in range [1, 16]. or M3 Item 68 (education level): 00 is not in range [98, 99].",
+    error_msgs = {"M3 Item 68 (education level): 00 is not in range [1, 16]. "
+                  "or M3 Item 68 (education level): 00 is not in range [98, 99].",
                   "M3 Item 69 (citizenship status): 0 is not in [1, 2, 3, 9]."}
 
     for e in parser_errors:
