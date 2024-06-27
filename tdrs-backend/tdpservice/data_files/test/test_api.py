8f17e03303e020a83a90d8434323c3a386efd997
Eric Lipe <elipe@teamraft.com>
2024-06-27 09:07:37 -0400

    - Updated tests

--- tdrs-backend/tdpservice/data_files/test/test_api.py
+++ tdrs-backend/tdpservice/data_files/test/test_api.py
@@ -100,8 +100,8 @@ class DataFileAPITestBase:
 
         assert ws.cell(row=1, column=1).value == "Please refer to the most recent versions of the coding " \
             + "instructions (linked below) when looking up items and allowable values during the data revision process"
-        assert ws.cell(row=8, column=COL_ERROR_MESSAGE).value == "Every T1 record should have at least one " + \
-            "corresponding T2 or T3 record with the same RPT_MONTH_YEAR and CASE_NUMBER."
+        assert ws.cell(row=8, column=COL_ERROR_MESSAGE).value == ("if cash amount :873 validator1 passed then number "
+                                                                  "of months T1: 0 is not larger than 0.")
 
     @staticmethod
     def assert_error_report_ssp_file_content_matches_with_friendly_names(response):
@@ -132,9 +132,8 @@ class DataFileAPITestBase:
 
         assert ws.cell(row=1, column=1).value == "Please refer to the most recent versions of the coding " \
             + "instructions (linked below) when looking up items and allowable values during the data revision process"
-        assert ws.cell(row=8, column=COL_ERROR_MESSAGE).value == ("Every T1 record should have at least one "
-                                                                  "corresponding T2 or T3 record with the same "
-                                                                  "RPT_MONTH_YEAR and CASE_NUMBER.")
+        assert ws.cell(row=8, column=COL_ERROR_MESSAGE).value == ("if CASH_AMOUNT :873 validator1 passed then "
+                                                                  "NBR_MONTHS T1: 0 is not larger than 0.")
 
     @staticmethod
     def assert_data_file_exists(data_file_data, version, user):
