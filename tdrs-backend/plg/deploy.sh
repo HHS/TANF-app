c935ce02e629eccfd08951c466f7116ee7d41c60
Eric Lipe <elipe@teamraft.com>
2024-09-18 10:21:56 -0400

    - move data sources to template file

--- tdrs-backend/plg/deploy.sh
+++ tdrs-backend/plg/deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-if [ "$#" -ne 3 ]; then
-    echo "Error, this script expects 3 parameters."
-    echo "I.e: ./deploy.sh env_name db_uri aws_rds_service_name"
-    exit 1
-fi
+# if [ "$#" -ne 3 ]; then
+#     echo "Error, this script expects 3 parameters."
+#     echo "I.e: ./deploy.sh env_name db_uri aws_rds_service_name"
+#     exit 1
+# fi
 
 deploy_pg_exporter() {
     MANIFEST=manifest.$1.yml
@@ -17,9 +17,22 @@ deploy_pg_exporter() {
     yq eval -i ".applications[0].env.DATA_SOURCE_NAME = \"$2\""  $MANIFEST
     yq eval -i ".applications[0].services[0] = \"$3\""  $MANIFEST
 
-    cf push --no-route -f $MANIFEST -t 180 --strategy rolling || exit 1
+    cf push --no-route -f $MANIFEST -t 180
 }
 
+deploy_grafana() {
+    pushd grafana
+    APP_NAME="grafana"
+    DATASOURCES="datasources.yml"
+    cp datasources.template.yml $DATASOURCES
+
+    yq eval -i ".datasources[0].url = \"http://prometheus.apps.internal:9090\""  $DATASOURCES
+    yq eval -i ".datasources[1].url = \"http://loki.apps.internal:3100\""  $DATASOURCES
+
+    # cf push --no-route -f $MANIFEST -t 180
+    popd
+}
+deploy_grafana "test"
 # Commands below for when prometheus is deployed
 # cf map-route "$APP_NAME" apps.internal --hostname "$APP_NAME"
 # cf add-network-policy "$PROMETHEUS" "$APP_NAME" --protocol tcp --port 9187
