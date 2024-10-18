3f0279347bdfab2e67e7690b8f6cc8acf05cb5a8
Eric Lipe <elipe@teamraft.com>
2024-10-18 11:26:14 -0400

    - Updated queries - Ordered based on OneNote

--- tdrs-backend/tdpservice/data_files/views.py
+++ tdrs-backend/tdpservice/data_files/views.py
@@ -150,25 +150,23 @@ class DataFileViewSet(ModelViewSet):
         all_errors = ParserError.objects.filter(file=datafile).order_by('pk')
         filtered_errors = None
         user = self.request.user
-        if not (user.is_ofa_sys_admin or user.is_ofa_admin):
+        is_active = "Active" in datafile.section
+        is_closed = "Closed" in datafile.section
+
+        # We only filter Active and Closed submissions. Aggregate and Stratum return all errors.
+        if not (user.is_ofa_sys_admin or user.is_ofa_admin) and (is_active or is_closed):
             # All cat1/4 errors
             error_type_query = Q(error_type=ParserErrorCategoryChoices.PRE_CHECK) | \
                 Q(error_type=ParserErrorCategoryChoices.CASE_CONSISTENCY)
             filtered_errors = all_errors.filter(error_type_query)
 
-            # All cat2 errors associated with FAMILY_AFFILIATION or CITIZENSHIP_STATUS
-            filtered_errors = filtered_errors.union(all_errors.filter(fields_json__friendly_name__has_any_keys=[
-                "FAMILY_AFFILIATION",
-                "CITIZENSHIP_STATUS"
-                ],
-                error_type=ParserErrorCategoryChoices.FIELD_VALUE))
-
-            # All cat3 errors associated with FAMILY_AFFILIATION and CITIZENSHIP_STATUS
-            filtered_errors = filtered_errors.union(all_errors.filter(fields_json__friendly_name__has_keys=[
-                "FAMILY_AFFILIATION",
-                "CITIZENSHIP_STATUS"
-                ],
-                error_type=ParserErrorCategoryChoices.VALUE_CONSISTENCY))
+            # All cat2 errors associated with FAMILY_AFFILIATION and (CITIZENSHIP_STATUS or CLOSURE_REASON)
+            second_field = "CITIZENSHIP_STATUS" if is_active else "CLOSURE_REASON"
+            field_query = Q(field_name="FAMILY_AFFILIATION") | Q(field_name=second_field)
+            filtered_errors = filtered_errors.union(all_errors.filter(
+                field_query,
+                error_type=ParserErrorCategoryChoices.FIELD_VALUE
+                ))
 
             # All cat3 errors associated with FAMILY_AFFILIATION and SSN
             filtered_errors = filtered_errors.union(all_errors.filter(fields_json__friendly_name__has_keys=[
@@ -177,7 +175,21 @@ class DataFileViewSet(ModelViewSet):
                 ],
                 error_type=ParserErrorCategoryChoices.VALUE_CONSISTENCY))
 
-            if "Active" in datafile.section:
+            # All cat3 errors associated with FAMILY_AFFILIATION and CITIZENSHIP_STATUS
+            filtered_errors = filtered_errors.union(all_errors.filter(fields_json__friendly_name__has_keys=[
+                "FAMILY_AFFILIATION",
+                "CITIZENSHIP_STATUS"
+                ],
+                error_type=ParserErrorCategoryChoices.VALUE_CONSISTENCY))
+
+            if is_active:
+                # All cat3 errors associated with summed fields: AMT_FOOD_STAMP_ASSISTANCE, AMT_SUB_CC, CASH_AMOUNT,
+                # CC_AMOUNT, TRANSP_AMOUNT
+                filtered_errors = filtered_errors.union(all_errors.filter(fields_json__friendly_name__has_keys=[
+                    "AMT_FOOD_STAMP_ASSISTANCE", "AMT_SUB_CC", "CASH_AMOUNT", "CC_AMOUNT", "TRANSP_AMOUNT"
+                    ],
+                    error_type=ParserErrorCategoryChoices.VALUE_CONSISTENCY))
+
                 # All cat3 errors associated with FAMILY_AFFILIATION and SSN and CITIZENSHIP_STATUS
                 filtered_errors = filtered_errors.union(all_errors.filter(fields_json__friendly_name__has_keys=[
                     "FAMILY_AFFILIATION",
@@ -211,13 +223,6 @@ class DataFileViewSet(ModelViewSet):
                 filtered_errors = filtered_errors.union(all_errors.filter(fields_json__friendly_name__has_keys=[
                     "CITIZENSHIP_STATUS",
                     "WORK_ELIGIBLE_INDICATOR",
-                    ],
-                    error_type=ParserErrorCategoryChoices.VALUE_CONSISTENCY))
-
-                # All cat3 errors associated with summed fields: AMT_FOOD_STAMP_ASSISTANCE, AMT_SUB_CC, CASH_AMOUNT,
-                # CC_AMOUNT, TRANSP_AMOUNT
-                filtered_errors = filtered_errors.union(all_errors.filter(fields_json__friendly_name__has_keys=[
-                    "AMT_FOOD_STAMP_ASSISTANCE", "AMT_SUB_CC", "CASH_AMOUNT", "CC_AMOUNT", "TRANSP_AMOUNT"
                     ],
                     error_type=ParserErrorCategoryChoices.VALUE_CONSISTENCY))
         else:
