c8050b15d2ad3aa9f805381a4d5c1a1718c8e5a9
Eric Lipe <elipe@teamraft.com>
2024-11-25 11:41:30 -0500

    - fix errro

--- tdrs-backend/tdpservice/parsers/models.py
+++ tdrs-backend/tdpservice/parsers/models.py
@@ -5,6 +5,7 @@ from django.db import models
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from tdpservice.data_files.models import DataFile
+from tdpservice.data_files.util import ParserErrorCategoryChoices
 
 import logging
 
