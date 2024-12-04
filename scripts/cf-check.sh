1bd1a430280f761417b761bdd6398f24ba315bfd
Andrew <84722778+andrew-jameson@users.noreply.github.com>
2024-12-04 15:18:20 -0500

    Hotfix/cf check (#3339)
    * Standardizing on use of cf-check on older workflow
    
    * Standardizing use of cf-check on older workflows
    
    * terraform executor edge-case
    
    ---------
    
    Co-authored-by: jtimpe <111305129+jtimpe@users.noreply.github.com>

--- scripts/cf-check.sh
+++ scripts/cf-check.sh
@@ -3,8 +3,14 @@ set -e
 if command -v cf /dev/null 2>&1; then
     echo The command cf is available
 else
-    apt-get update
-    apt-get install wget gnupg2 apt-transport-https jq
+    if [[ -f /bin/terraform ]]; then
+        echo "This is our Terraform executor"
+        apk update
+        apk install curl jq
+    else
+        apt-get update
+        apt-get install curl wget gnupg2 apt-transport-https jq
+    fi
 
     NEXUS_ARCHIVE="cf7-cli_7.7.13_linux_x86-64.tgz"
     NEXUS_URL="https://tdp-nexus.dev.raftlabs.tech/repository/tdp-bin/cloudfoundry-cli/$NEXUS_ARCHIVE"
