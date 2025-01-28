e25ca72af867c2d75d198fc68fb29ddae806108c
Jan Timpe <jtimpe@goraft.tech>
2025-01-28 08:42:14 -0500

    temp comment two tests

--- tdrs-frontend/src/components/Reports/FRAReports.test.js
+++ tdrs-frontend/src/components/Reports/FRAReports.test.js
@@ -1,8 +1,9 @@
 import React from 'react'
-import { FRAReports } from '.'
+import * as axios from 'axios'
 import { fireEvent, waitFor, render } from '@testing-library/react'
-import configureStore from '../../configureStore'
 import { Provider } from 'react-redux'
+import { FRAReports } from '.'
+import configureStore from '../../configureStore'
 
 const initialState = {
   auth: {
@@ -30,6 +31,9 @@ const initialState = {
 }
 
 const mockStore = (initial = {}) => configureStore(initial)
+
+const makeTestFile = (name, contents = ['test'], type = 'text/plain') =>
+  new File(contents, name, { type })
 
 describe('FRA Reports Page', () => {
   it('Renders', () => {
@@ -237,12 +241,179 @@ describe('FRA Reports Page', () => {
   })
 
   describe('Upload form', () => {
-    it('Allows text files to be selected and submitted', () => {})
+    const setup = async () => {
+      const state = {
+        ...initialState,
+        auth: {
+          authenticated: true,
+          user: {
+            email: 'hi@bye.com',
+            stt: {
+              id: 2,
+              type: 'state',
+              code: 'AK',
+              name: 'Alaska',
+            },
+            roles: [{ id: 1, name: 'Data Analyst', permission: [] }],
+            account_approval_status: 'Approved',
+          },
+        },
+      }
 
-    it('Shows an error if a non-allowed file type is selected', () => {})
+      const store = mockStore(state)
+      const origDispatch = store.dispatch
+      store.dispatch = jest.fn(origDispatch)
 
-    it('Shows a message if search is clicked with an non-uploaded file', () => {})
+      const component = render(
+        <Provider store={store}>
+          <FRAReports />
+        </Provider>
+      )
 
-    it('Does not show a message if search is clicked after uploading a file', () => {})
+      const { getByLabelText, getByText } = component
+
+      // fill out the form values before clicking search
+      const yearsDropdown = getByLabelText('Fiscal Year (October - September)')
+      fireEvent.change(yearsDropdown, { target: { value: '2021' } })
+
+      const quarterDropdown = getByLabelText('Quarter')
+      fireEvent.change(quarterDropdown, { target: { value: 'Q1' } })
+
+      fireEvent.click(getByText(/Search/, { selector: 'button' }))
+
+      await waitFor(() => {
+        expect(
+          getByText(
+            'Alaska - Work Outcomes for TANF Exiters - Fiscal Year 2021 - Quarter 1 (October - December)'
+          )
+        ).toBeInTheDocument()
+        expect(getByText('Submit Report')).toBeInTheDocument()
+      })
+
+      return { ...component, ...store }
+    }
+
+    // it('Allows text files to be selected and submitted', async () => {
+    //   const { getByText, dispatch, getByRole, container } = await setup()
+
+    //   const uploadForm = container.querySelector('#fra-file-upload')
+    //   await waitFor(() => {
+    //     fireEvent.change(uploadForm, {
+    //       target: { files: [makeTestFile('report.txt')] },
+    //     })
+    //   })
+
+    //   const submitButton = getByText('Submit Report')
+    //   fireEvent.click(submitButton)
+
+    //   // await waitFor(() => getByText('asdfasdf'))
+
+    //   await waitFor(() => getByRole('alert'))
+    //   expect(dispatch).toHaveBeenCalledTimes(1)
+    // })
+
+    it('Shows an error if a non-allowed file type is selected', async () => {
+      const { getByText, dispatch, getByRole, container } = await setup()
+
+      const uploadForm = container.querySelector('#fra-file-upload')
+      fireEvent.change(uploadForm, {
+        target: { files: [makeTestFile('report.png', ['png'], 'img/png')] },
+      })
+      await waitFor(() => {
+        expect(
+          getByText(
+            'Invalid extension. Accepted file types are: .txt, .ms##, .ts##, or .ts###.'
+          )
+        ).toBeInTheDocument()
+      })
+
+      const submitButton = getByText('Submit Report', { selector: 'button' })
+      fireEvent.click(submitButton)
+
+      await waitFor(() => getByRole('alert'))
+      expect(dispatch).toHaveBeenCalledTimes(1)
+    })
+
+    it('Shows a message if search is clicked with an non-uploaded file', async () => {
+      const { getByText, container, getByLabelText, queryByText } =
+        await setup()
+
+      const uploadForm = container.querySelector('#fra-file-upload')
+      fireEvent.change(uploadForm, {
+        target: { files: [makeTestFile('report.txt')] },
+      })
+
+      await waitFor(() => {
+        expect(
+          getByText('You have selected the file: report.txt')
+        ).toBeInTheDocument()
+      })
+
+      const yearsDropdown = getByLabelText('Fiscal Year (October - September)')
+      fireEvent.change(yearsDropdown, { target: { value: '2024' } })
+
+      const quarterDropdown = getByLabelText('Quarter')
+      fireEvent.change(quarterDropdown, { target: { value: 'Q2' } })
+
+      await waitFor(() => {
+        expect(
+          getByText('Quarter 2 (January - March)', { selector: 'option' })
+            .selected
+        ).toBe(true)
+      })
+
+      fireEvent.click(getByText(/Search/, { selector: 'button' }))
+
+      await waitFor(() =>
+        expect(queryByText('Files Not Submitted')).toBeInTheDocument()
+      )
+    })
+
+    // it('Does not show a message if search is clicked after uploading a file', async () => {
+    //   const {
+    //     getByText,
+    //     container,
+    //     getByLabelText,
+    //     queryByText,
+    //     dispatch,
+    //     getByRole,
+    //   } = await setup()
+
+    //   const mockAxios = jest.genMockFromModule('axios')
+    //   mockAxios.create = jest.fn(() => mockAxios)
+    //   mockAxios.post.mockResolvedValue({ status: 201 })
+
+    //   const uploadForm = container.querySelector('#fra-file-upload')
+    //   fireEvent.change(uploadForm, {
+    //     target: { files: [makeTestFile('report.txt')] },
+    //   })
+
+    //   const submitButton = getByText('Submit Report')
+    //   fireEvent.click(submitButton)
+
+    //   // await waitFor(() => getByRole('alert'))
+    //   expect(dispatch).toHaveBeenCalledTimes(1)
+    //   expect(mockAxios.create).toHaveBeenCalledTimes(1)
+    //   expect(mockAxios.post).toHaveBeenCalledTimes(1)
+
+    //   const yearsDropdown = getByLabelText('Fiscal Year (October - September)')
+    //   fireEvent.change(yearsDropdown, { target: { value: '2024' } })
+
+    //   const quarterDropdown = getByLabelText('Quarter')
+    //   fireEvent.change(quarterDropdown, { target: { value: 'Q2' } })
+
+    //   await waitFor(() => {
+    //     expect(
+    //       getByText('Quarter 2 (January - March)', { selector: 'option' })
+    //         .selected
+    //     ).toBe(true)
+    //   })
+
+    //   fireEvent.click(getByText(/Search/, { selector: 'button' }))
+
+    //   await waitFor(() =>
+    //     expect(queryByText('Files Not Submitted')).not.toBeInTheDocument()
+    //   )
+    // })
   })
 })
