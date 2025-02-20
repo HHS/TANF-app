5aaf32f4164fef0694a221b5e5523d0a937596cc
Eric Lipe <elipe@teamraft.com>
2025-02-20 17:52:33 -0500

    - Update test to use RawRow

--- tdrs-backend/tdpservice/parsers/test/test_util.py
+++ tdrs-backend/tdpservice/parsers/test/test_util.py
@@ -122,15 +122,17 @@ def test_parse_line_parses_line_from_schema_to_dict():
     schema = TanfDataReportSchema(
         document=None,
         fields=[
-            Field(item=1, name='first', friendly_name='first', type='string', startIndex=0, endIndex=3),
-            Field(item=2, name='second', friendly_name='second', type='string', startIndex=3, endIndex=4),
-            Field(item=3, name='third', friendly_name='third', type='string', startIndex=4, endIndex=5),
-            Field(item=4, name='fourth', friendly_name='fourth', type='number', startIndex=5, endIndex=7),
-            Field(item=5, name='fifth', friendly_name='fifth', type='number', startIndex=7, endIndex=8),
+            Field(item=1, name='first', friendly_name='first', type=FieldType.ALPHA_NUMERIC, startIndex=0, endIndex=3),
+            Field(item=2, name='second', friendly_name='second', type=FieldType.ALPHA_NUMERIC, startIndex=3, endIndex=4),
+            Field(item=3, name='third', friendly_name='third', type=FieldType.ALPHA_NUMERIC, startIndex=4, endIndex=5),
+            Field(item=4, name='fourth', friendly_name='fourth', type=FieldType.NUMERIC, startIndex=5, endIndex=7),
+            Field(item=5, name='fifth', friendly_name='fifth', type=FieldType.NUMERIC, startIndex=7, endIndex=8),
         ]
     )
 
-    record = schema.parse_line(line)
+    length = len(line)
+    row = RawRow(data=line, raw_len=length, decoded_len=length, row_num=1, record_type="")
+    record = schema.parse_row(row)
 
     assert record['first'] == '123'
     assert record['second'] == '4'
