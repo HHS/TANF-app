87ddeb63d9cffcb5c05c55a41e4a159ef04604f0
Eric Lipe <elipe@teamraft.com>
2025-03-07 11:49:48 -0500

    - delete binaries - update script

--- scripts/cf-check.sh
+++ scripts/cf-check.sh
@@ -3,10 +3,16 @@ set -e
 if command -v cf /dev/null 2>&1; then
     echo The command cf is available
 else
-    mv ../bin/linux/jq /usr/local/bin/jq
-    mv ../bin/linux/yq /usr/local/bin/yq
-    mv ../bin/linux/cf8 /usr/local/bin/cf
-    jq --version
-    yq --version
+    if [[ -f /bin/terraform ]]; then
+        echo "This is our Terraform executor, Alpine Linux v3.13"
+        apk update
+        apk add curl jq
+    else
+        apt-get update
+        apt-get install curl wget gnupg2 apt-transport-https jq
+    fi
+    wget https://github.com/cloudfoundry/cli/releases/download/v8.10.0/cf8-cli_8.10.0_linux_x86-64.tgz
+    tar -xvf cf8-cli_8.10.0_linux_x86-64.tgz
+    mv cf8 /usr/local/bin/cf
     cf --version
 fi
