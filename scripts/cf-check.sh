108b9162aa5033f0f4969258b10f09ac1c062857
Eric Lipe <elipe@teamraft.com>
2025-03-07 11:13:19 -0500

    - Update script directory

--- scripts/cf-check.sh
+++ scripts/cf-check.sh
@@ -3,9 +3,9 @@ set -e
 if command -v cf /dev/null 2>&1; then
     echo The command cf is available
 else
-    mv ../bin/jq /usr/local/bin/jq
-    mv ../bin/yq /usr/local/bin/yq
-    mv ../bin/cf8 /usr/local/bin/cf
+    mv ../bin/linux/jq /usr/local/bin/jq
+    mv ../bin/linux/yq /usr/local/bin/yq
+    mv ../bin/linux/cf8 /usr/local/bin/cf
     jq --version
     yq --version
     cf --version
