34bd9fdf9dcbb924a7372c84ec705da7ef0bb593
Eric Lipe <elipe@teamraft.com>
2025-03-07 10:49:31 -0500

    - Remove sources.list - Add bin directory for common pipeline binaries - Update cf-check.sh to use new bin directory - remove sources.list ref from docker file - update loki to pull yq from github

--- scripts/cf-check.sh
+++ scripts/cf-check.sh
@@ -3,20 +3,10 @@ set -e
 if command -v cf /dev/null 2>&1; then
     echo The command cf is available
 else
-    if [[ -f /bin/terraform ]]; then
-        echo "This is our Terraform executor, Alpine Linux v3.13"
-        apk update
-        apk add curl jq
-
-    else
-        apt-get update
-        apt-get install curl wget gnupg2 apt-transport-https jq
-    fi
-
-    NEXUS_ARCHIVE="cf7-cli_7.7.13_linux_x86-64.tgz"
-    NEXUS_URL="https://tdp-nexus.dev.raftlabs.tech/repository/tdp-bin/cloudfoundry-cli/$NEXUS_ARCHIVE"
-    curl $NEXUS_URL -o $NEXUS_ARCHIVE  # prefers anonymous, use of -u failed.
-    tar xzf $NEXUS_ARCHIVE
-    mv ./cf7 /usr/local/bin/cf
+    mv ../bin/jq /usr/local/bin/jq
+    mv ../bin/yq /usr/local/bin/yq
+    mv ../bin/cf8 /usr/local/bin/cf
+    jq --version
+    yq --version
     cf --version
 fi
