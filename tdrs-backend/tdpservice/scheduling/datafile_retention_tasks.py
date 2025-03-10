38fe0d5afc55d366ce6449755b9e19956761bde7
Eric Lipe <elipe@teamraft.com>
2025-03-10 14:22:27 -0400

    - Updated to use raw deletes to save memory and go faster

--- tdrs-backend/tdpservice/scheduling/datafile_retention_tasks.py
+++ tdrs-backend/tdpservice/scheduling/datafile_retention_tasks.py
@@ -2,17 +2,20 @@
 
 from __future__ import absolute_import
 from celery import shared_task
-from django.db.models import Min, Max
+from datetime import datetime
 import logging
 from tdpservice.core.utils import log
 from tdpservice.data_files.models import DataFile
+from tdpservice.search_indexes.utils import delete_summaries, delete_errors, delete_records, get_log_context
 from tdpservice.stts.models import STT
+from tdpservice.users.models import User
 
 
 logger = logging.getLogger(__name__)
 
 
 RETRY_DELAY = 30
+system_user, created = User.objects.get_or_create(username='system')
 
 
 @shared_task(bind=True, max_retries=None)
@@ -36,9 +39,12 @@ def remove_old_versions(self, data_file_id=None, data_file_version=None):
 
         num_prev_versions = prev_versions.count()
         logger.info(f"Preparing to delete {num_prev_versions} old versions of file: {repr(data_file)}")
-        for prev_version in prev_versions:
-            prev_version.delete()
-            logger.info(f"Succesfully deleted old version: {repr(prev_version)}")
+        ids = prev_versions.values_list('id', flat=True)
+        log_context = get_log_context(system_user)
+        delete_summaries(ids, log_context)
+        delete_errors(ids, log_context)
+        delete_records(ids, True, log_context)
+        DataFile.objects.filter(id__in=ids).delete()
         logger.info(f"Successfully deleted {num_prev_versions} old versions of file: {repr(data_file)}")
     except Exception as e:
         if self.request.retries == data_file_version:
@@ -53,11 +59,18 @@ def remove_old_versions(self, data_file_id=None, data_file_version=None):
 def remove_all_old_versions():
     """Delete old versions for every file in the database."""
     stts = STT.objects.all()
-    min_year = DataFile.objects.aggregate(min_year=Min('year'))['min_year']
-    max_year = DataFile.objects.aggregate(max_year=Max('year'))['max_year']
+    min_year = 2019 # TDP didn't exist before this
+    max_year = datetime.now().year
     sections = DataFile.Section
     quarters = DataFile.Quarter
     num_exceptions = 0
+
+    num_out_of_range = DataFile.objects.exclude(year__range=(min_year, max_year)).count()
+    if num_out_of_range > 0:
+        log(f"Found {num_out_of_range} files with years outside of the range {min_year} to "
+            f"{max_year}. These will need manual cleanup!", level='warning')
+
+    log_context = get_log_context(system_user)
     for year in range(min_year, max_year + 1):
         for stt in stts:
             for section in sections:
@@ -67,7 +80,11 @@ def remove_all_old_versions():
                         if files.count() == 0:
                             continue
                         newest_file = files.latest('version')
-                        files.exclude(id=newest_file.id).delete()
+                        ids = files.exclude(id=newest_file.id).values_list('id', flat=True)
+                        delete_summaries(ids, log_context)
+                        delete_errors(ids, log_context)
+                        delete_records(ids, True, log_context)
+                        DataFile.objects.filter(id__in=ids).delete()
                     except Exception as e:
                         log(f"Failed to delete old versions of file for: Year:{year}, Quarter:{quarter}, "
                             f"Section:{section}, STT:{stt.name}", level='error')
