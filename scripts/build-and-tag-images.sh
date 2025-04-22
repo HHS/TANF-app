333d8582934fc08878088cb00cd99c6fa9c3b2ff
Matt Cole Anderson <manderson@teamraft.com>
2025-04-22 11:48:37 -0500

    update build-and-tag-images.sh to use ghcr.io as registry

--- scripts/build-and-tag-images.sh
+++ scripts/build-and-tag-images.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-if [ "$#" -ne 8  ]; then
-    echo "Error, this script expects 8 parameters."
-    echo "I.e: ./build-tag-images.sh BACKEND_APP_NAME FRONTEND_APP_NAME BACKEND_PATH FRONTEND_PATH BUILD_NUM COMMIT_HASH DOCKER_LOGIN DOCKER_USER"
+if [ "$#" -ne 9  ]; then
+    echo "Error, this script expects 9 parameters."
+    echo "I.e: ./build-tag-images.sh BACKEND_APP_NAME FRONTEND_APP_NAME BACKEND_PATH FRONTEND_PATH BUILD_NUM COMMIT_HASH REGISTRY_LOGIN REGISTRY_USER REGISTRY_DOMAIN"
     exit 1
 fi
 
@@ -12,21 +12,24 @@ BACKEND_PATH=$3
 FRONTEND_PATH=$4
 BUILD_NUM=$5
 COMMIT_HASH=$6
-DOCKER_LOGIN=$7
-DOCKER_USER=$8
+REGISTRY_LOGIN=$7
+REGISTRY_USER=$8
+REGISTRY_DOMAIN=$9
+
+STRIPPED_REGISTRY="${REGISTRY_DOMAIN#*://}" # removes the protocol from the domain for use in the image name
 BUILD_DATE=`date +%F`
 TAG="${BUILD_DATE}_build-${BUILD_NUM}_${COMMIT_HASH}"
 
 export DOCKER_CLI_EXPERIMENTAL=enabled
 
 build_and_tag() {
-    echo "$DOCKER_LOGIN" | docker login https://tdp-docker.dev.raftlabs.tech -u $DOCKER_USER --password-stdin
-    docker buildx build --load --platform linux/amd64 -t tdp-docker.dev.raftlabs.tech/$BACKEND_APP_NAME:$TAG -t tdp-docker.dev.raftlabs.tech/$BACKEND_APP_NAME:latest "$BACKEND_PATH"
-    docker buildx build --load --platform linux/arm64 -t tdp-docker.dev.raftlabs.tech/$BACKEND_APP_NAME:$TAG -t tdp-docker.dev.raftlabs.tech/$BACKEND_APP_NAME:latest "$BACKEND_PATH"
-    docker push --all-tags tdp-docker.dev.raftlabs.tech/$BACKEND_APP_NAME
-    docker buildx build --load --platform linux/amd64 -t tdp-docker.dev.raftlabs.tech/$FRONTEND_APP_NAME:$TAG -t tdp-docker.dev.raftlabs.tech/$FRONTEND_APP_NAME:latest "$FRONTEND_PATH"
-    docker buildx build --load --platform linux/arm64 -t tdp-docker.dev.raftlabs.tech/$FRONTEND_APP_NAME:$TAG -t tdp-docker.dev.raftlabs.tech/$FRONTEND_APP_NAME:latest "$FRONTEND_PATH"
-    docker push --all-tags tdp-docker.dev.raftlabs.tech/$FRONTEND_APP_NAME
+    echo "$REGISTRY_LOGIN" | docker login $REGISTRY_DOMAIN -u $REGISTRY_USER --password-stdin
+    docker buildx build --load --platform linux/amd64 -t $STRIPPED_REGISTRY/$BACKEND_APP_NAME:$TAG -t $STRIPPED_REGISTRY/$BACKEND_APP_NAME:latest "$BACKEND_PATH"
+    docker buildx build --load --platform linux/arm64 -t $STRIPPED_REGISTRY/$BACKEND_APP_NAME:$TAG -t $STRIPPED_REGISTRY/$BACKEND_APP_NAME:latest "$BACKEND_PATH"
+    docker push --all-tags $STRIPPED_REGISTRY/$BACKEND_APP_NAME
+    docker buildx build --load --platform linux/amd64 -t $STRIPPED_REGISTRY/$FRONTEND_APP_NAME:$TAG -t $STRIPPED_REGISTRY/$FRONTEND_APP_NAME:latest "$FRONTEND_PATH"
+    docker buildx build --load --platform linux/arm64 -t $STRIPPED_REGISTRY/$FRONTEND_APP_NAME:$TAG -t $STRIPPED_REGISTRY/$FRONTEND_APP_NAME:latest "$FRONTEND_PATH"
+    docker push --all-tags $STRIPPED_REGISTRY/$FRONTEND_APP_NAME
     docker logout
 }
 
