9d8202634d18757a402386a560dac03f8cfc2a12
Jan Timpe <jtimpe@goraft.tech>
2025-05-20 10:11:31 -0500

    separate celery locally

--- tdrs-backend/gunicorn_start.sh
+++ tdrs-backend/gunicorn_start.sh
@@ -19,13 +19,13 @@ else
     python manage.py collectstatic --noinput
 fi
 
-# Celery worker config can be found here: https://docs.celeryq.dev/en/stable/userguide/workers.html#:~:text=The-,hostname,-argument%20can%20expand
-celery -A tdpservice.settings worker --loglevel=INFO --concurrency=1 --max-tasks-per-child=1 -n worker1@%h &
-sleep 5
+# # Celery worker config can be found here: https://docs.celeryq.dev/en/stable/userguide/workers.html#:~:text=The-,hostname,-argument%20can%20expand
+# celery -A tdpservice.settings worker --loglevel=INFO --concurrency=1 --max-tasks-per-child=1 -n worker1@%h &
+# sleep 5
 
-# TODO: Uncomment the following line to add flower service when memory limitation is resolved
-celery -A tdpservice.settings --broker=$REDIS_URI flower &
-celery -A tdpservice.settings beat -l info --scheduler django_celery_beat.schedulers:DatabaseScheduler &
+# # TODO: Uncomment the following line to add flower service when memory limitation is resolved
+# celery -A tdpservice.settings --broker=$REDIS_URI flower &
+# celery -A tdpservice.settings beat -l info --scheduler django_celery_beat.schedulers:DatabaseScheduler &
 
 echo "Starting Gunicorn"
 if [[ "$DJANGO_CONFIGURATION" = "Development" || "$DJANGO_CONFIGURATION" = "Local" ]]; then
