f755ddce9dfb329fbcf0f4d0ea6f50db79594853
Jan Timpe <jtimpe@goraft.tech>
2025-05-22 15:26:47 -0500

    port syntax

--- tdrs-backend/celery_start.sh
+++ tdrs-backend/celery_start.sh
@@ -7,5 +7,5 @@ celery -A tdpservice.settings worker --loglevel=INFO --concurrency=1 --max-tasks
 sleep 5
 
 # TODO: Uncomment the following line to add flower service when memory limitation is resolved
-celery -A tdpservice.settings --broker=$REDIS_URI flower --port 8080 &
+celery -A tdpservice.settings --broker=$REDIS_URI flower --port=8080 &
 celery -A tdpservice.settings beat -l info --scheduler django_celery_beat.schedulers:DatabaseScheduler # &
