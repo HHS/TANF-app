48106d2f936b47c47b8fa04f854ee33b83a566a5
Matt Cole Anderson <manderson@teamraft.com>
2025-06-16 13:41:04 -0500

    change legend, add description, make text not wrap unless mobile screen size

--- tdrs-frontend/src/components/RequestAccessForm/FRASelector.jsx
+++ tdrs-frontend/src/components/RequestAccessForm/FRASelector.jsx
@@ -2,7 +2,15 @@ function FRASelector({ hasFRAData, setHasFRAData }) {
   return (
     <div className="usa-form-group">
       <fieldset className="usa-fieldset">
-        <legend className="usa-label text-bold">Has FRA Data</legend>
+        <legend className="usa-label text-bold text-no-wrap">
+          Will you be reporting Fiscal Responsibility Act (FRA) data files?*
+        </legend>
+        <p className="text-no-wrap margin-y-2px">
+          Unsure?{' '}
+          <a href="https://tdp-project-updates.app.cloud.gov/knowledge-center/submitting-fra-data.html">
+            Learn about FRA data file reporting
+          </a>
+        </p>
         <div className="usa-radio">
           <input
             className="usa-radio__input"
