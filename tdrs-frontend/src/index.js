fcb49df87a7b5289b543b603d1b85c31b4522dc9
Eric Lipe <elipe@teamraft.com>
2025-06-17 10:22:38 -0500

    - Disable rum by default - Only enable rum when deploying production - Add cors headers for custom header - Add conditionals to all useRUM functions to do nothing if faro isn't initialized - Added provider wrappers to use the correct components when faro is or isnt enabled - Fixed request access axios issue

--- tdrs-frontend/src/index.js
+++ tdrs-frontend/src/index.js
@@ -12,6 +12,7 @@ import {
   getWebInstrumentations,
   createReactRouterV6Options,
   FaroErrorBoundary,
+  faro,
 } from '@grafana/faro-react'
 
 import {
@@ -108,14 +109,23 @@ store.dispatch(fetchAuth())
 // }
 // Start the mirage server to stub some backend endpoints when running locally
 
+/* istanbul ignore next */
+const ErrorProvider = ({ children }) => {
+  return !faro || !faro.api ? (
+    <>{children}</>
+  ) : (
+    <FaroErrorBoundary>{children}</FaroErrorBoundary>
+  )
+}
+
 const container = document.getElementById('root')
 const root = createRoot(container)
 root.render(
   <Provider store={store}>
     <Router store={store} history={history}>
-      <FaroErrorBoundary>
+      <ErrorProvider>
         <App />
-      </FaroErrorBoundary>
+      </ErrorProvider>
     </Router>
   </Provider>
 )
