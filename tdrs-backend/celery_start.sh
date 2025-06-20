eb193c0a9958227cdbf5529f129ac7d496447df1
Jan Timpe <jtimpe@goraft.tech>
2025-06-20 09:43:37 -0500

    rm unneeded

--- tdrs-backend/celery_start.sh
+++ tdrs-backend/celery_start.sh
@@ -22,4 +22,4 @@ sleep 5
 
 # TODO: Uncomment the following line to add flower service when memory limitation is resolved
 celery -A tdpservice.settings --broker=$REDIS_URI flower --port=8080 &
-celery -A tdpservice.settings beat -l info --scheduler django_celery_beat.schedulers:DatabaseScheduler # &
+celery -A tdpservice.settings beat -l info --scheduler django_celery_beat.schedulers:DatabaseScheduler
