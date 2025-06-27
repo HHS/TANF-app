8b62e5bb2c42343aded7413862c08c56f1c2c7b4
Matt Cole Anderson <manderson@teamraft.com>
2025-06-27 13:02:55 -0500

    make changes to migrations

--- tdrs-backend/tdpservice/users/migrations/0046_add_fra_access_permission.py
+++ tdrs-backend/tdpservice/users/migrations/0046_add_fra_access_permission.py
@@ -6,27 +6,18 @@ from tdpservice.users.permissions import (
     view_permissions_q
 )
 
-def set_fra_submitters_group_and_permissions(apps, shcema_editor):
+def set_fra_permissions(apps, shcema_editor):
     Group = apps.get_model('auth', 'Group')
     Permission = apps.get_model('auth', 'Permission')
     ContentType = apps.get_model('contenttypes', 'ContentType')
 
     # Get or create groups that need FRA access
-    fra_submitter, _ = Group.objects.get_or_create(name='FRA Submitter')
     developer = Group.objects.get(name='Developer')
     ofa_admin = Group.objects.get(name='OFA Admin')
     ofa_regional_staff = Group.objects.get(name='OFA Regional Staff')
     ofa_system_admin = Group.objects.get(name='OFA System Admin')
 
-    # Get the same permissions as data analyst
-    datafile_permissions = get_permission_ids_for_model(
-        'data_files',
-        'datafile',
-        filters=[add_permissions_q, view_permissions_q]
-    )
-
-    # Creat Permission and assign to group
-    # TODO: 4972 - should i make use of the get_permission_ids_for_model function here?
+    # Creat Permission
     contentType = ContentType.objects.get(app_label='users', model='user')
     fra_permission, _ = Permission.objects.get_or_create(
         codename='has_fra_access',
@@ -34,32 +25,23 @@ def set_fra_submitters_group_and_permissions(apps, shcema_editor):
         content_type=contentType,
     )
 
-    # Clear old permissions and set the new one
-    fra_submitter.permissions.clear()
-    fra_submitter.permissions.add(*datafile_permissions, fra_permission)
-
     # add FRA permissions to other groups
     developer.permissions.add(fra_permission)
     ofa_admin.permissions.add(fra_permission)
     ofa_regional_staff.permissions.add(fra_permission)
     ofa_system_admin.permissions.add(fra_permission)
-    # TODO: 4972 - What other groups need fra access?
 
-def unset_fra_submitters_group_and_permissions(apps, schema_editor):
+def unset_fra_permissions(apps, schema_editor):
     Group = apps.get_model('auth', 'Group')
     Permission = apps.get_model('auth', 'Permission')
 
     # Groups to reove fra from
-    fra_submitter = Group.objects.get(name='FRA Submitter')
     developer = Group.objects.get(name='Developer')
     ofa_admin = Group.objects.get(name='OFA Admin')
     ofa_regional_staff = Group.objects.get(name='OFA Regional Staff')
     ofa_system_admin = Group.objects.get(name='OFA System Admin')
 
-    # Delete fra submitter group
-    fra_submitter.delete()
-
-    # Remove fra permissions from other groups
+    # Remove fra permissions from groups
     fra_access_permission = Permission.objects.get(codename='has_fra_access')
     developer.permissions.remove(fra_access_permission)
     ofa_admin.permissions.remove(fra_access_permission)
@@ -75,8 +57,8 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.RunPython(
-            set_fra_submitters_group_and_permissions,
-            reverse_code=unset_fra_submitters_group_and_permissions
+            set_fra_permissions,
+            reverse_code=unset_fra_permissions
         ),
     ]
 
