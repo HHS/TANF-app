fa759468469787ff574fb79e4ad88b3fad73786c
Eric Lipe <elipe@teamraft.com>
2025-07-07 09:35:34 -0500

    - Update deploy script

--- scripts/apply-database-config.sh
+++ scripts/apply-database-config.sh
@@ -59,6 +59,10 @@ python manage.py migrate
 status=$?
 echo "Done."
 
+echo "Generating Admin and User Views"
+python ./plg/grafana_views/generate_views.py --all
+echo "Done."
+
 echo "Applying SQL views"
 python manage.py runscript apply_grafana_views
 echo "Done."
