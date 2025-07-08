463fee848cca684367ec505fbe7c6ed0da92dd09
Eric Lipe <elipe@teamraft.com>
2025-07-08 15:43:33 -0500

    Revert "- remove console log"
    This reverts commit fc7c307b14a788a1a48019e0a1268b5b87d5f5f9.

--- tdrs-frontend/src/components/FileUpload/utils.jsx
+++ tdrs-frontend/src/components/FileUpload/utils.jsx
@@ -195,6 +195,8 @@ export const checkHeaderFile = async function (
   }
 
   const yearQuarterRegex = '[0-9]{4}[1-4]'
+  // TODO: Remove
+  console.log(header)
   const yearQuarter = header.match(yearQuarterRegex)
   const fileYear = yearQuarter?.[0]?.slice(0, 4)
   const fileQuarter = yearQuarter?.[0]?.slice(4, 5)
