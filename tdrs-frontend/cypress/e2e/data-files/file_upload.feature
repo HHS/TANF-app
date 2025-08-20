946920acc094fa8450449ec7ebf3799cb5adb4b1
Eric Lipe <elipe@teamraft.com>
2025-08-20 12:31:35 -0500

    - Parametrize file upload steps

--- tdrs-frontend/cypress/e2e/data-files/file_upload.feature
+++ tdrs-frontend/cypress/e2e/data-files/file_upload.feature
@@ -2,23 +2,23 @@ Feature: Data file submission
     Scenario: Admin Alex can submit TANF reports
         Given 'Admin Alex' logs in
         When Admin Alex submits the TANF Report
-        Then Admin Alex sees the upload in TANF Submission History
-        And Admin Alex can download the TANF error report
+        Then 'Admin Alex' sees the upload in 'TANF' Submission History
+        And 'Admin Alex' can download the 'TANF' error report
     Scenario: Admin Alex can submit SSP reports
         Given 'Admin Alex' logs in
         When Admin Alex submits the SSP Report
-        Then Admin Alex sees the upload in SSP Submission History
-        And Admin Alex can download the SSP error report
+        Then 'Admin Alex' sees the upload in 'SSP' Submission History
+        And 'Admin Alex' can download the 'SSP' error report
     Scenario: Admin Alex can submit FRA reports
         Given 'Admin Alex' logs in
         When Admin Alex submits the Work Outcomes Report
-        Then Admin Alex sees the upload in FRA Submission History
-        And Admin Alex can download the FRA error report
+        Then 'Admin Alex' sees the upload in 'FRA' Submission History
+        And 'Admin Alex' can download the 'FRA' error report
     Scenario: A FRA data analyst can submit a Work Outcomes of TANF Exiters data file
         Given 'FRA Data Analyst Fred' logs in
         When FRA Data Analyst Fred submits the Work Outcomes Report
-        Then FRA Data Analyst Fred sees the upload in Submission History
-        And FRA Data Analyst Fred can download the FRA error report
+        Then 'FRA Data Analyst Fred' sees the upload in 'FRA' Submission History
+        And 'FRA Data Analyst Fred' can download the 'FRA' error report
         # And Regional Randy gets an email (determine exact)
     Scenario: A FRA data analyst sees incorrect file type error
         Given 'FRA Data Analyst Fred' logs in
@@ -27,14 +27,14 @@ Feature: Data file submission
     Scenario: A FRA data analyst can submit a TANF data file
         Given 'FRA Data Analyst Fred' logs in
         When FRA Data Analyst Fred submits the TANF Report
-        Then FRA Data Analyst Fred sees the upload in TANF Submission History
-        And FRA Data Analyst Fred can download the TANF error report
+        Then 'FRA Data Analyst Fred' sees the upload in 'TANF' Submission History
+        And 'FRA Data Analyst Fred' can download the 'TANF' error report
         # And Regional Randy gets an email (determine exact)
     Scenario: A FRA data analyst can submit a SSP data file
         Given 'FRA Data Analyst Fred' logs in
         When FRA Data Analyst Fred submits the SSP Report
-        Then FRA Data Analyst Fred sees the upload in SSP Submission History
-        And FRA Data Analyst Fred can download the SSP error report
+        Then 'FRA Data Analyst Fred' sees the upload in 'SSP' Submission History
+        And 'FRA Data Analyst Fred' can download the 'SSP' error report
         # And Regional Randy gets an email (determine exact)
 
     Scenario Outline: <actor> can submit a <program> Section <section> data file
--- tdrs-frontend/cypress/e2e/data-files/file_upload.js
+++ tdrs-frontend/cypress/e2e/data-files/file_upload.js
@@ -2,6 +2,48 @@
 import { When, Then } from '@badeball/cypress-cucumber-preprocessor'
 import * as df from '../common-steps/data_files.js'
 
+Then(
+  '{string} sees the upload in {string} Submission History',
+  (actor, program) => {
+    // Wait for the API response and extract the file ID
+    cy.wait('@dataFileSubmit').then((interception) => {
+      // Check if we have a valid response with an ID
+      if (
+        interception.response &&
+        interception.response.body &&
+        interception.response.body.id
+      ) {
+        const fileId = interception.response.body.id
+
+        // Poll the API until the summary is populated
+        df.waitForDataFileSummary(fileId)
+        if (program === 'TANF') {
+          cy.get('button').contains('Submission History').click()
+          df.validateSmallCorrectFile()
+        } else if (program === 'SSP') {
+          cy.get('button').contains('Submission History').click()
+          df.validateSmallSSPFile()
+        } else if (program === 'FRA') {
+          cy.contains('Submission History').should('exist')
+          df.validateFraCsv()
+        }
+      }
+    })
+  }
+)
+
+Then('{string} can download the {string} error report', (actor, program) => {
+  if (program === 'TANF') {
+    df.downloadErrorReport('2021-Q1-Active Case Data Error Report.xlsx')
+  } else if (program === 'SSP') {
+    df.downloadErrorReport('2024-Q1-SSP Active Case Data Error Report.xlsx')
+  } else if (program === 'FRA') {
+    df.downloadErrorReport(
+      '2024-Q2-Work Outcomes of TANF Exiters Error Report.xlsx'
+    )
+  }
+})
+
 ///////////////////////// Admin Steps /////////////////////////
 
 // TANF steps
@@ -15,25 +57,6 @@ When('Admin Alex submits the TANF Report', () => {
   )
 
   cy.get('button').contains('Submit Data Files').should('exist').click()
-})
-
-Then('Admin Alex sees the upload in TANF Submission History', () => {
-  // Wait for the API response and extract the file ID
-  cy.wait('@dataFileSubmit').then((interception) => {
-    // Check if we have a valid response with an ID
-    if (
-      interception.response &&
-      interception.response.body &&
-      interception.response.body.id
-    ) {
-      const fileId = interception.response.body.id
-
-      // Poll the API until the summary is populated
-      df.waitForDataFileSummary(fileId)
-      cy.get('button').contains('Submission History').click()
-      df.validateSmallCorrectFile()
-    }
-  })
 })
 
 // Constants ----------
@@ -146,10 +169,6 @@ Then('{string} sees rejected status in submission history', (actor) => {
     })
 })
 
-Then('Admin Alex can download the TANF error report', () => {
-  df.downloadErrorReport('2021-Q1-Active Case Data Error Report.xlsx')
-})
-
 // SSP steps
 When('Admin Alex submits the SSP Report', () => {
   cy.visit('/data-files')
@@ -161,29 +180,6 @@ When('Admin Alex submits the SSP Report', () => {
   )
 
   cy.get('button').contains('Submit Data Files').should('exist').click()
-})
-
-Then('Admin Alex sees the upload in SSP Submission History', () => {
-  // Wait for the API response and extract the file ID
-  cy.wait('@dataFileSubmit').then((interception) => {
-    // Check if we have a valid response with an ID
-    if (
-      interception.response &&
-      interception.response.body &&
-      interception.response.body.id
-    ) {
-      const fileId = interception.response.body.id
-
-      // Poll the API until the summary is populated
-      df.waitForDataFileSummary(fileId)
-      cy.get('button').contains('Submission History').click()
-      df.validateSmallSSPFile()
-    }
-  })
-})
-
-Then('Admin Alex can download the SSP error report', () => {
-  df.downloadErrorReport('2024-Q1-SSP Active Case Data Error Report.xlsx')
 })
 
 // FRA steps
@@ -198,29 +194,6 @@ When('Admin Alex submits the Work Outcomes Report', () => {
   cy.get('button').contains('Submit Report').should('exist').click()
 })
 
-Then('Admin Alex sees the upload in FRA Submission History', () => {
-  cy.wait('@dataFileSubmit').then((interception) => {
-    // Check if we have a valid response with an ID
-    if (
-      interception.response &&
-      interception.response.body &&
-      interception.response.body.id
-    ) {
-      const fileId = interception.response.body.id
-
-      // Poll the API until the summary is populated
-      df.waitForDataFileSummary(fileId)
-      cy.contains('Submission History').should('exist')
-      df.validateFraCsv()
-    }
-  })
-})
-
-Then('Admin Alex can download the FRA error report', () => {
-  df.downloadErrorReport(
-    '2024-Q2-Work Outcomes of TANF Exiters Error Report.xlsx'
-  )
-})
 ///////////////////////////////////////////////////////////////
 
 /////////////////// FRA Data Analyst Steps ///////////////////
@@ -235,30 +208,6 @@ When('FRA Data Analyst Fred submits the Work Outcomes Report', () => {
     '../tdrs-backend/tdpservice/parsers/test/data/fra.csv'
   )
   cy.get('button').contains('Submit Report').should('exist').click()
-})
-
-Then('FRA Data Analyst Fred sees the upload in Submission History', () => {
-  cy.wait('@dataFileSubmit').then((interception) => {
-    // Check if we have a valid response with an ID
-    if (
-      interception.response &&
-      interception.response.body &&
-      interception.response.body.id
-    ) {
-      const fileId = interception.response.body.id
-
-      // Poll the API until the summary is populated
-      df.waitForDataFileSummary(fileId)
-      cy.contains('Submission History').should('exist')
-      df.validateFraCsv()
-    }
-  })
-})
-
-Then('FRA Data Analyst Fred can download the FRA error report', () => {
-  df.downloadErrorReport(
-    '2024-Q2-Work Outcomes of TANF Exiters Error Report.xlsx'
-  )
 })
 
 When('FRA Data Analyst Fred uploads incorrect file type', () => {
@@ -291,28 +240,6 @@ When('FRA Data Analyst Fred submits the TANF Report', () => {
   cy.get('button').contains('Submit Data Files').should('exist').click()
 })
 
-Then('FRA Data Analyst Fred sees the upload in TANF Submission History', () => {
-  cy.wait('@dataFileSubmit').then((interception) => {
-    // Check if we have a valid response with an ID
-    if (
-      interception.response &&
-      interception.response.body &&
-      interception.response.body.id
-    ) {
-      const fileId = interception.response.body.id
-
-      // Poll the API until the summary is populated
-      df.waitForDataFileSummary(fileId)
-      cy.get('button').contains('Submission History').click()
-      df.validateSmallCorrectFile()
-    }
-  })
-})
-
-Then('FRA Data Analyst Fred can download the TANF error report', () => {
-  df.downloadErrorReport('2021-Q1-Active Case Data Error Report.xlsx')
-})
-
 // SSP steps
 When('FRA Data Analyst Fred submits the SSP Report', () => {
   cy.visit('/data-files')
@@ -325,28 +252,6 @@ When('FRA Data Analyst Fred submits the SSP Report', () => {
   )
 
   cy.get('button').contains('Submit Data Files').should('exist').click()
-})
-
-Then('FRA Data Analyst Fred sees the upload in SSP Submission History', () => {
-  cy.wait('@dataFileSubmit').then((interception) => {
-    // Check if we have a valid response with an ID
-    if (
-      interception.response &&
-      interception.response.body &&
-      interception.response.body.id
-    ) {
-      const fileId = interception.response.body.id
-
-      // Poll the API until the summary is populated
-      df.waitForDataFileSummary(fileId)
-      cy.get('button').contains('Submission History').click()
-      df.validateSmallSSPFile()
-    }
-  })
-})
-
-Then('FRA Data Analyst Fred can download the SSP error report', () => {
-  df.downloadErrorReport('2024-Q1-SSP Active Case Data Error Report.xlsx')
 })
 
 ///////////////////////////////////////////////////////////////
