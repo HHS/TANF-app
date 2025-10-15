f296e28c1a52ade9dc302df2036dca26a2f59f67
Eric Lipe <elipe@teamraft.com>
2025-10-15 14:57:10 -0500

    - add debug statements

--- scripts/deploy-backend.sh
+++ scripts/deploy-backend.sh
@@ -156,6 +156,8 @@ update_backend()
         # Do a zero downtime deploy.  This requires enough memory for
         # two apps to exist in the org/space at one time.
         args+=("--strategy rolling")
+        echo cf push "${args[@]}"
+        ls -lha
         cf push "${args[@]}" || exit 1
     else
         cf push "${args[@]}"
