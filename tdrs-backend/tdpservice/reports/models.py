0e5c6c898a45cb25a1d9783ebc0c8fd50cb77f65
Matt Cole Anderson <manderson@teamraft.com>
2025-10-31 11:50:47 -0500

    refactor FileRecord abstract model to a common class for use in Reports and DataFiles

--- tdrs-backend/tdpservice/reports/models.py
+++ tdrs-backend/tdpservice/reports/models.py
@@ -6,6 +6,7 @@ from django.db import models
 from django.db.models import Max
 
 from tdpservice.backends import DataFilesS3Storage
+from tdpservice.common.models import StoredFileBase
 from tdpservice.stts.models import STT
 from tdpservice.users.models import User
 
@@ -18,30 +19,7 @@ def get_s3_upload_path(instance, filename):
     )
 
 
-# The Report File model was starting to explode, and I think that keeping this logic
-# in its own abstract class is better for documentation purposes.
-class File(models.Model):
-    """Abstract type representing a file stored in S3."""
-
-    class Meta:
-        """Metadata."""
-
-        abstract = True
-
-    # Keep the file name because it will be different in s3,
-    # but the interface will still want to present the file with its
-    # original name.
-    original_filename = models.CharField(max_length=256, blank=False, null=False)
-    # Slug is the name of the file in S3
-    # NOTE: Currently unused, may be removed with a later release
-    slug = models.CharField(max_length=256, blank=False, null=False)
-    # Not all files will have the correct extension,
-    # or even have one at all. The UI will provide this information
-    # separately
-    extension = models.CharField(max_length=8, default="txt")
-
-
-class ReportFile(File):
+class ReportFile(StoredFileBase):
     """Represents a version of a report file."""
 
     class Section(models.TextChoices):
