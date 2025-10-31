68710a5ef2cbbd485e72fc231d0b1c4aa33551e6
Eric Lipe <elipe@teamraft.com>
2025-10-31 12:01:45 -0500

    - Update based on suggestion

--- tdrs-backend/tdpservice/data_files/admin/filters.py
+++ tdrs-backend/tdpservice/data_files/admin/filters.py
@@ -52,40 +52,18 @@ class VersionFilter(MostRecentVersionFilter):
     parameter_name = "created_at"
 
     def queryset(self, request, queryset):
-        """
-        Sort queryset to show latest records.
-
-        Example of what the query looks like for reference:
-        SELECT df.*,
-            (SELECT MAX(version)
-                FROM data_files df2
-                WHERE df2.stt_code = df.stt_code
-                AND df2.year = df.year
-                AND df2.quarter = df.quarter
-                AND df2.program_type = df.program_type
-                AND df2.section = df.section
-            ) AS max_version
-        FROM data_files df
-        WHERE df.version = max_version
-        """
+        """Sort queryset to show latest records."""
         if self.value() is None and queryset.exists():
-            # Subquery to find the max version for each group
-            max_versions = (
-                queryset.filter(
-                    stt__stt_code=OuterRef("stt__stt_code"),
-                    year=OuterRef("year"),
-                    quarter=OuterRef("quarter"),
-                    program_type=OuterRef("program_type"),
-                    section=OuterRef("section"),
-                )
-                .values("stt__stt_code", "year", "quarter", "program_type", "section")
-                .annotate(max_version=Max("version"))
-                .values("max_version")
-            )
+            # Subquery to find the latest version for each group
+            versions = queryset.filter(
+                stt__stt_code=OuterRef("stt__stt_code"),
+                year=OuterRef("year"),
+                quarter=OuterRef("quarter"),
+                program_type=OuterRef("program_type"),
+                section=OuterRef("section"),
+            ).order_by("-version")
 
-            # Filter to only records with the max version
-            return queryset.annotate(max_version=Subquery(max_versions)).filter(
-                version=F("max_version")
-            )
+            # Filter to only records with the latest version
+            return queryset.filter(version=Subquery(versions.values("version")[:1]))
 
         return queryset
