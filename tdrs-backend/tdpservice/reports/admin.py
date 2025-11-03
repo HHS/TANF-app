5ce9fbe2be7794a4062227c649be8751d1bdfe8f
Matt Cole Anderson <manderson@teamraft.com>
2025-11-03 10:08:50 -0600

    add Admin class for ReportIngest

--- tdrs-backend/tdpservice/reports/admin.py
+++ tdrs-backend/tdpservice/reports/admin.py
@@ -4,12 +4,12 @@ from django.contrib import admin
 
 from tdpservice.core.utils import ReadOnlyAdminMixin
 
-from .models import ReportFile
+from .models import ReportFile, ReportIngest
 
 
 @admin.register(ReportFile)
 class ReportFileAdmin(ReadOnlyAdminMixin, admin.ModelAdmin):
-    """Read-only Admin class for Report models."""
+    """Read-only Admin class for Report File model."""
 
     list_display = [
         "id",
@@ -27,4 +27,27 @@ class ReportFileAdmin(ReadOnlyAdminMixin, admin.ModelAdmin):
     search_fields = [
         "original_filename",
         "slug",
+    ]
+
+@admin.register(ReportIngest)
+class ReportIngestAdmin(ReadOnlyAdminMixin, admin.ModelAdmin):
+    """Read-only Admin class for Report Ingest model."""
+
+    list_display = [
+        "id",
+        "original_filename",
+        "uploaded_by",
+        "status",
+        "created_at",
+        "num_reports_created",
+    ]
+    list_filter = [
+        "original_filename",
+        "uploaded_by__email",
+        "status",
+        "created_at",
+    ]
+    search_fields = [
+        "status",
+        "created_at",
     ]
