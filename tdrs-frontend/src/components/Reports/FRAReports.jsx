e9622dd2a64fba35290e791a3ea23445a5969ae7
jtimpe <111305129+jtimpe@users.noreply.github.com>
2025-11-25 13:56:10 -0600

    Update tdrs-frontend/src/components/Reports/FRAReports.jsx
    Co-authored-by: Eric Lipe <125676261+elipe17@users.noreply.github.com>

--- tdrs-frontend/src/components/Reports/FRAReports.jsx
+++ tdrs-frontend/src/components/Reports/FRAReports.jsx
@@ -653,7 +653,6 @@ const FRAReportsContent = () => {
       )
 
       const pollFraSubmissionHistory = (datafile) => {
-        console.log('starting fra polling', datafile.id)
         startPolling(
           `${datafile.id}`,
           () => getFraSubmissionStatus(datafile.id),
