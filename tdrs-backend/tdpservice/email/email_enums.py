156d397d166dc7880c4b243baf132ccbb774dfef
Jan Timpe <jtimpe@goraft.tech>
2025-12-03 17:31:21 -0600

    rm old templates

--- tdrs-backend/tdpservice/email/email_enums.py
+++ tdrs-backend/tdpservice/email/email_enums.py
@@ -2,25 +2,6 @@
 
 from enum import Enum
 
-# class EmailType(Enum):
-#     """Email type enum."""
-
-# ACCESS_REQUEST_COUNT = "access-request-count.html"
-# ACCESS_REQUEST_SUBMITTED = "access-request-submitted.html"
-# DATA_SUBMITTED = "data-submitted.html"
-# DATA_SUBMISSION_FAILED = "data-submitted-failed.html"
-# FRA_SUBMITTED = "fra-data-submitted.html"
-# REQUEST_APPROVED = "request-approved.html"
-# REQUEST_DENIED = "request-denied.html"
-# DEACTIVATION_WARNING = "account-deactivation-warning.html"
-# ACCOUNT_DEACTIVATED = "account-deactivated.html"
-# ACCOUNT_DEACTIVATED_ADMIN = "account-deactivated-admin.html"
-# UPCOMING_SUBMISSION_DEADLINE = "upcoming-submission-deadline.html"
-# STUCK_FILE_LIST = "stuck-file-list.html"
-# SYSTEM_ADMIN_ROLE_CHANGED = "system-admin-role-changed.html"
-# PROFILE_CHANGE_REQUEST_APPROVED = "profile-change-request-approved.html"
-# PROFILE_CHANGE_REQUEST_REJECTED = "profile-change-request-rejected.html"
-
 
 class UserAccountEmail(Enum):
     """Email templates related to user accounts."""
@@ -39,14 +20,14 @@ class UserAccountEmail(Enum):
 
 
 class DataFileEmail(Enum):
-    """Email templates related to data file submissions."""
+    """Email templates related to general data file submissions."""
 
-    # DATA_SUBMITTED = "data-submitted.html"
-    # FRA_SUBMITTED = "fra-data-submitted.html"
     UPCOMING_SUBMISSION_DEADLINE = "upcoming-submission-deadline.html"
 
 
 class TanfDataFileEmail(Enum):
+    """Email templates related to TANF data file submissions."""
+
     ACCEPTED = "tanf/accepted.html"
     ACCEPTED_WITH_ERRORS = "tanf/accepted_with_errors.html"
     PARTIALLY_ACCEPTED = "tanf/partially_accepted.html"
@@ -54,6 +35,8 @@ class TanfDataFileEmail(Enum):
 
 
 class FraDataFileEmail(Enum):
+    """Email templates related to FRA data file submissions."""
+
     ACCEPTED = "fra/accepted.html"
     ACCEPTED_WITH_ERRORS = "fra/accepted_with_errors.html"
     PARTIALLY_ACCEPTED = "fra/partially_accepted.html"
