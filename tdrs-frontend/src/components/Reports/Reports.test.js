63d8b172ccf778ae68b386f30270fa5484d30b1f
Mo S <97037188+raftmsohani@users.noreply.github.com>
2026-02-03 11:48:27 -0500

    5597 Fix showing tribal STT (#5606)
    * 5597 Fix showing tribal STT
    
    * Tech debt Linting

--- tdrs-frontend/src/components/Reports/Reports.test.js
+++ tdrs-frontend/src/components/Reports/Reports.test.js
@@ -1,12 +1,5 @@
 import React from 'react'
-import {
-  render,
-  screen,
-  fireEvent,
-  waitFor,
-  act,
-  queryAllByText,
-} from '@testing-library/react'
+import { render, screen, fireEvent, waitFor } from '@testing-library/react'
 
 import { Provider } from 'react-redux'
 import { MemoryRouter } from 'react-router-dom'
