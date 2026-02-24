6e1fe5caf30654842d3fd3c883d7c32e03916afb
Mo S <97037188+raftmsohani@users.noreply.github.com>
2026-02-24 15:29:58 -0500

    5525 Restart submission status polling page navigation (#5600)
    * 5525 restart submission status
    
    * only start polling effect when file is pending
    
    * fixed failing test
    
    * fixed frontend tests
    
    * fix linting
    
    * added frontend tests
    
    * changed alert message
    
    * added populate stt to fix e2e test failure
    
    * fixed failing test

--- tdrs-frontend/src/hooks/useFileUploadForm.js
+++ tdrs-frontend/src/hooks/useFileUploadForm.js
@@ -9,6 +9,7 @@ import {
 } from '../actions/reports'
 import { useEventLogger } from '../utils/eventLogger'
 import { useFormSubmission } from './useFormSubmission'
+import { POLLING_TIMEOUT_MESSAGE } from '../components/Reports/constants'
 import { useReportsContext } from '../components/Reports/ReportsContext'
 
 /**
@@ -86,8 +87,7 @@ export const useFileUploadForm = ({
           },
           (onError) => {
             onError({
-              message:
-                'Exceeded max number of tries to update submission status.',
+              message: POLLING_TIMEOUT_MESSAGE,
               type: 'warning',
             })
           }
