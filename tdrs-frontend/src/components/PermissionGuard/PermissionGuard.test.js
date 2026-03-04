75c30ce27b8884e0e73eed322d4a480b83efb2b9
Jan Timpe <jtimpe@goraft.tech>
2026-03-04 16:11:27 -0600

    tests

--- tdrs-frontend/src/components/PermissionGuard/PermissionGuard.test.js
+++ tdrs-frontend/src/components/PermissionGuard/PermissionGuard.test.js
@@ -29,174 +29,174 @@ describe('PermissionGuard.js', () => {
       </Provider>
     )
 
-  // describe('not allowed', () => {
-  //   it('shows not allowed if missing one permission', () => {
-  //     setup(
-  //       {
-  //         auth: {
-  //           authenticated: true,
-  //           user: {
-  //             roles: [{ permissions: [{ codename: 'some_stuff' }] }],
-  //           },
-  //         },
-  //       },
-  //       ['allowed']
-  //     )
+  describe('not allowed', () => {
+    it('shows not allowed if missing one permission', () => {
+      setup(
+        {
+          auth: {
+            authenticated: true,
+            user: {
+              roles: [{ permissions: [{ codename: 'some_stuff' }] }],
+            },
+          },
+        },
+        ['allowed']
+      )
 
-  //     expect(screen.queryByText('hello, world')).not.toBeInTheDocument()
-  //     expect(screen.queryByText('not allowed')).toBeInTheDocument()
-  //   })
+      expect(screen.queryByText('hello, world')).not.toBeInTheDocument()
+      expect(screen.queryByText('not allowed')).toBeInTheDocument()
+    })
 
-  //   it('shows not allowed if missing multiple permissions', () => {
-  //     setup(
-  //       {
-  //         auth: {
-  //           authenticated: true,
-  //           user: {
-  //             roles: [{ permissions: [{ codename: 'allowed' }] }],
-  //           },
-  //         },
-  //       },
-  //       ['allowed', 'super_allowed', 'super_duper_allowed']
-  //     )
+    it('shows not allowed if missing multiple permissions', () => {
+      setup(
+        {
+          auth: {
+            authenticated: true,
+            user: {
+              roles: [{ permissions: [{ codename: 'allowed' }] }],
+            },
+          },
+        },
+        ['allowed', 'super_allowed', 'super_duper_allowed']
+      )
 
-  //     expect(screen.queryByText('hello, world')).not.toBeInTheDocument()
-  //     expect(screen.queryByText('not allowed')).toBeInTheDocument()
-  //   })
+      expect(screen.queryByText('hello, world')).not.toBeInTheDocument()
+      expect(screen.queryByText('not allowed')).toBeInTheDocument()
+    })
 
-  //   it('shows not allowed if multiple roles missing permissions', () => {
-  //     setup(
-  //       {
-  //         auth: {
-  //           authenticated: true,
-  //           user: {
-  //             roles: [
-  //               { permissions: [{ codename: 'allowed' }] },
-  //               { permissions: [{ codename: 'super_allowed' }] },
-  //               { permissions: [{ codename: 'nothing' }] },
-  //             ],
-  //           },
-  //         },
-  //       },
-  //       ['allowed', 'super_allowed', 'super_duper_allowed']
-  //     )
+    it('shows not allowed if multiple roles missing permissions', () => {
+      setup(
+        {
+          auth: {
+            authenticated: true,
+            user: {
+              roles: [
+                { permissions: [{ codename: 'allowed' }] },
+                { permissions: [{ codename: 'super_allowed' }] },
+                { permissions: [{ codename: 'nothing' }] },
+              ],
+            },
+          },
+        },
+        ['allowed', 'super_allowed', 'super_duper_allowed']
+      )
 
-  //     expect(screen.queryByText('hello, world')).not.toBeInTheDocument()
-  //     expect(screen.queryByText('not allowed')).toBeInTheDocument()
-  //   })
+      expect(screen.queryByText('hello, world')).not.toBeInTheDocument()
+      expect(screen.queryByText('not allowed')).toBeInTheDocument()
+    })
 
-  //   it('shows not allowed if user has no roles', () => {
-  //     setup(
-  //       {
-  //         auth: {
-  //           authenticated: true,
-  //           user: {
-  //             roles: null,
-  //           },
-  //         },
-  //       },
-  //       ['anything']
-  //     )
+    it('shows not allowed if user has no roles', () => {
+      setup(
+        {
+          auth: {
+            authenticated: true,
+            user: {
+              roles: null,
+            },
+          },
+        },
+        ['anything']
+      )
 
-  //     expect(screen.queryByText('hello, world')).not.toBeInTheDocument()
-  //     expect(screen.queryByText('not allowed')).toBeInTheDocument()
-  //   })
+      expect(screen.queryByText('hello, world')).not.toBeInTheDocument()
+      expect(screen.queryByText('not allowed')).toBeInTheDocument()
+    })
 
-  //   it('shows not allowed if user has no permissions', () => {
-  //     setup(
-  //       {
-  //         auth: {
-  //           authenticated: true,
-  //           user: {
-  //             roles: [{ permissions: [] }],
-  //           },
-  //         },
-  //       },
-  //       ['anything']
-  //     )
+    it('shows not allowed if user has no permissions', () => {
+      setup(
+        {
+          auth: {
+            authenticated: true,
+            user: {
+              roles: [{ permissions: [] }],
+            },
+          },
+        },
+        ['anything']
+      )
 
-  //     expect(screen.queryByText('hello, world')).not.toBeInTheDocument()
-  //     expect(screen.queryByText('not allowed')).toBeInTheDocument()
-  //   })
+      expect(screen.queryByText('hello, world')).not.toBeInTheDocument()
+      expect(screen.queryByText('not allowed')).toBeInTheDocument()
+    })
 
-  //   it('shows not allowed if requiresApproval and not approved', () => {
-  //     setup(
-  //       {
-  //         auth: {
-  //           authenticated: true,
-  //           user: {
-  //             roles: [{ permissions: ['anything'] }],
-  //             account_approval_status: 'Pending',
-  //           },
-  //         },
-  //       },
-  //       [],
-  //       true
-  //     )
+    it('shows not allowed if requiresApproval and not approved', () => {
+      setup(
+        {
+          auth: {
+            authenticated: true,
+            user: {
+              roles: [{ permissions: ['anything'] }],
+              account_approval_status: 'Pending',
+            },
+          },
+        },
+        [],
+        true
+      )
 
-  //     expect(screen.queryByText('hello, world')).not.toBeInTheDocument()
-  //     expect(screen.queryByText('not allowed')).toBeInTheDocument()
-  //   })
-  // })
+      expect(screen.queryByText('hello, world')).not.toBeInTheDocument()
+      expect(screen.queryByText('not allowed')).toBeInTheDocument()
+    })
+  })
 
-  // describe('allowed', () => {
-  //   it('shows allowed if no required permissions given', () => {
-  //     setup(
-  //       {
-  //         auth: {
-  //           authenticated: true,
-  //           user: {
-  //             roles: null,
-  //           },
-  //         },
-  //       },
-  //       null
-  //     )
+  describe('allowed', () => {
+    it('shows allowed if no required permissions given', () => {
+      setup(
+        {
+          auth: {
+            authenticated: true,
+            user: {
+              roles: null,
+            },
+          },
+        },
+        null
+      )
 
-  //     expect(screen.queryByText('hello, world')).toBeInTheDocument()
-  //     expect(screen.queryByText('not allowed')).not.toBeInTheDocument()
-  //   })
+      expect(screen.queryByText('hello, world')).toBeInTheDocument()
+      expect(screen.queryByText('not allowed')).not.toBeInTheDocument()
+    })
 
-  //   it('shows allowed if user matches all permissions', () => {
-  //     setup(
-  //       {
-  //         auth: {
-  //           authenticated: true,
-  //           user: {
-  //             roles: [
-  //               { permissions: [{ codename: 'allowed' }] },
-  //               { permissions: [{ codename: 'super_allowed' }] },
-  //               { permissions: [{ codename: 'super_duper_allowed' }] },
-  //             ],
-  //           },
-  //         },
-  //       },
-  //       ['allowed', 'super_allowed', 'super_duper_allowed']
-  //     )
+    it('shows allowed if user matches all permissions', () => {
+      setup(
+        {
+          auth: {
+            authenticated: true,
+            user: {
+              roles: [
+                { permissions: [{ codename: 'allowed' }] },
+                { permissions: [{ codename: 'super_allowed' }] },
+                { permissions: [{ codename: 'super_duper_allowed' }] },
+              ],
+            },
+          },
+        },
+        ['allowed', 'super_allowed', 'super_duper_allowed']
+      )
 
-  //     expect(screen.queryByText('hello, world')).toBeInTheDocument()
-  //     expect(screen.queryByText('not allowed')).not.toBeInTheDocument()
-  //   })
+      expect(screen.queryByText('hello, world')).toBeInTheDocument()
+      expect(screen.queryByText('not allowed')).not.toBeInTheDocument()
+    })
 
-  //   it('shows allowed if requiresApproval and approved', () => {
-  //     setup(
-  //       {
-  //         auth: {
-  //           authenticated: true,
-  //           user: {
-  //             roles: [{ permissions: ['anything'] }],
-  //             account_approval_status: 'Approved',
-  //           },
-  //         },
-  //       },
-  //       [],
-  //       true
-  //     )
+    it('shows allowed if requiresApproval and approved', () => {
+      setup(
+        {
+          auth: {
+            authenticated: true,
+            user: {
+              roles: [{ permissions: ['anything'] }],
+              account_approval_status: 'Approved',
+            },
+          },
+        },
+        [],
+        true
+      )
 
-  //     expect(screen.queryByText('hello, world')).toBeInTheDocument()
-  //     expect(screen.queryByText('not allowed')).not.toBeInTheDocument()
-  //   })
-  // })
+      expect(screen.queryByText('hello, world')).toBeInTheDocument()
+      expect(screen.queryByText('not allowed')).not.toBeInTheDocument()
+    })
+  })
 
   describe('feature flags', () => {
     it.each([
