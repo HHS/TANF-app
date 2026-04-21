a04f8ec1ea6f00c7b11b141d2abb562426978df4
Matt Cole Anderson <manderson@teamraft.com>
2026-04-21 15:00:45 -0500

    remove subtitle from STT feedback reports view

--- tdrs-frontend/src/components/Routes/Routes.js
+++ tdrs-frontend/src/components/Routes/Routes.js
@@ -57,10 +57,10 @@ const AppRoutes = () => {
   const feedbackReportsTitle = userCanUploadFeedbackReports
     ? 'Upload Feedback Reports'
     : 'Feedback Reports'
-  // TODO: Change the subtitle to include wording for FRA as well TANF/SSP
+  // TODO: Change the subtitle to include wording for FRA as well as TANF/SSP
   const feedbackReportsSubtitle = userCanUploadFeedbackReports
     ? 'TANF WPR, SSP WPR, TANF & SSP Combined, and Time Limit Reports'
-    : 'Work Participation Rate and Time Limit Reports'
+    : null
 
   const setEditState = (isEditing) => {
     setIsInEditMode(isEditing)
