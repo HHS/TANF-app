70411e0e83da01b4b28436b3c1e68a548ac792a4
Jan Timpe <jtimpe@goraft.tech>
2026-05-06 10:11:50 -0500

    shadow df model

--- tdrs-services/parser/internal/db/models.go
+++ tdrs-services/parser/internal/db/models.go
@@ -8,7 +8,7 @@ import (
 	"github.com/jackc/pgx/v5/pgtype"
 )
 
-type DataFilesDatafile struct {
+type ShadowDataFilesDatafile struct {
 	ID               int32
 	OriginalFilename string
 	Slug             string
